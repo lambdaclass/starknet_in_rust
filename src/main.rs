21936b799b8468a5a167381ba4f69fb2e103fb2a
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2022-12-07 23:12:42 +0100

    Add `PatriciaMerkleTree` interface and a few tests.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod patricia_merkle_tree;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/patricia_merkle_tree.rs
@@ -0,0 +1,237 @@
+/// Patricia Merkle Tree implementation.
+///
+/// For now, keys are always `[u8; 32]`, which represent `KECCAK256` hashes.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct PatriciaMerkleTree<V> {
+    root_node: Option<Node<V>>,
+}
+
+impl<V> PatriciaMerkleTree<V> {
+    /// Create an empty tree.
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    /// Check if the tree is empty.
+    pub fn is_empty(&self) -> bool {
+        self.root_node.is_none()
+    }
+
+    /// Insert a key-value into the tree.
+    ///
+    /// Overwrites and returns the previous value.
+    pub fn insert(&mut self, key: &[u8; 32], value: V) -> Option<V> {
+        todo!()
+    }
+
+    /// Remove a value given its key.
+    ///
+    /// Returns the removed value.
+    pub fn remove(&mut self, key: &[u8; 32]) -> Option<V> {
+        todo!()
+    }
+
+    /// Retrieves a value given its key.
+    pub fn get(&self, key: &[u8; 32]) -> Option<V> {
+        todo!()
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Node<V> {
+    Branch(BranchNode<V>),
+    Extension(ExtensionNode<V>),
+    Leaf(LeafNode<V>),
+}
+
+impl<V> From<BranchNode<V>> for Node<V> {
+    fn from(value: BranchNode<V>) -> Self {
+        Self::Branch(value)
+    }
+}
+
+impl<V> From<ExtensionNode<V>> for Node<V> {
+    fn from(value: ExtensionNode<V>) -> Self {
+        Self::Extension(value)
+    }
+}
+
+impl<V> From<LeafNode<V>> for Node<V> {
+    fn from(value: LeafNode<V>) -> Self {
+        Self::Leaf(value)
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct BranchNode<V> {
+    choices: [Option<Box<Node<V>>>; 16],
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct ExtensionNode<V> {
+    // Boolean flag is true if last value in prefix is a nibble (not a byte).
+    prefix: (Vec<u8>, bool),
+    child: Box<Node<V>>,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct LeafNode<V> {
+    key: Vec<u8>,
+    value: V,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! pm_tree {
+        () => {
+            PatriciaMerkleTree {
+                root_node: None,
+            }
+        };
+        ( < $t:ty > ) => {
+            PatriciaMerkleTree::<$t> {
+                root_node: None,
+            }
+        };
+        ( $type:ident { $( $root_node:tt )* } ) => {
+            PatriciaMerkleTree {
+                root_node: Some(pm_tree!(@parse $type { $( $root_node )* }).into()),
+            }
+        };
+
+        ( @parse branch { $( $key:literal => $type:ident { $( $node:tt )* } ),* } ) => {
+            BranchNode {
+                choices: {
+                    let mut choices: [Option<Box<Node<_>>>; 16] = Default::default();
+                    $( choices[$key] = Some(Box::new(pm_tree!(@parse $type { $( $node )* }).into())); )*
+                    choices
+                },
+            }
+        };
+        ( @parse extension { $prefix:expr, $type:ident { $( $node:tt )* } } ) => {
+            ExtensionNode {
+                prefix: $prefix,
+                child: Box::new(pm_tree!(@parse $type { $( $node )* }).into()),
+            }
+        };
+        ( @parse leaf { $key:expr => $value:expr } ) => {
+            LeafNode {
+                key: $key,
+                value: $value,
+            }
+        };
+    }
+
+    /// Test that `PatriciaMerkleTree` can be initialized.
+    #[test]
+    fn patricia_tree_new() {
+        assert_eq!(PatriciaMerkleTree::<()>::new(), pm_tree!());
+    }
+
+    /// Test that `PatriciaMerkleTree::is_empty()` works as intended.
+    #[test]
+    fn patricia_tree_is_empty() {
+        assert!(pm_tree!(<()>).is_empty());
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works when the tree is empty.
+    #[test]
+    fn patricia_tree_insert_empty() {
+        let mut pm_tree = pm_tree!();
+        let key = Default::default();
+
+        assert_eq!(pm_tree.insert(&key, ()), None);
+        assert_eq!(
+            pm_tree,
+            pm_tree! {
+                leaf { key.to_vec() => () }
+            },
+        );
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works when combining on a leaf node, when the first
+    /// character is differing.
+    #[test]
+    fn patricia_tree_insert_leaf_beginning() {
+        let key_a: [u8; 32] = Default::default();
+        let mut key_b: [u8; 32] = Default::default();
+        key_b[0] += 0x10;
+
+        let mut pm_tree = pm_tree! {
+            leaf { key_a.to_vec() => () }
+        };
+
+        assert_eq!(pm_tree.insert(&key_b, ()), None);
+        assert_eq!(
+            pm_tree,
+            pm_tree! {
+                branch {
+                    0 => leaf { key_a.to_vec() => () },
+                    1 => leaf { key_b.to_vec() => () }
+                }
+            }
+        );
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works when combining on a leaf node, when the first
+    /// differing character is in the middle.
+    #[test]
+    fn patricia_tree_insert_leaf_middle() {
+        let key_a: [u8; 32] = Default::default();
+        let mut key_b: [u8; 32] = Default::default();
+        key_b[4] += 0x01;
+
+        let mut pm_tree = pm_tree! {
+            leaf { key_a.to_vec() => () }
+        };
+
+        assert_eq!(pm_tree.insert(&key_b, ()), None);
+        assert_eq!(pm_tree, pm_tree! {
+            extension { (vec![0], true), branch {
+                0 => leaf { key_a.to_vec() => () },
+                1 => leaf { key_b.to_vec() => () }
+            } }
+        });
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` overwrites a leaf value.
+    #[test]
+    fn patricia_tree_insert_leaf_overwrite() {
+        todo!()
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works by inserting into an existing branch.
+    #[test]
+    fn patricia_tree_insert_branch() {
+        todo!()
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works by overwriting an existing branch's child.
+    #[test]
+    fn patricia_tree_insert_branch_overwrite() {
+        todo!()
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works by splitting an extension in the beginning.
+    #[test]
+    fn patricia_tree_insert_extension_beginning() {
+        todo!()
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works by splitting an extension in the middle.
+    #[test]
+    fn patricia_tree_insert_extension_middle() {
+        todo!()
+    }
+
+    /// Test that `PatriciaMerkleTree::insert()` works by splitting an extension in the end.
+    #[test]
+    fn patricia_tree_insert_extension_end() {
+        todo!()
+    }
+
+    // TODO: Design and implement tests for `PatriciaMerkleTree::remove()`.
+    // TODO: Design and implement tests for `PatriciaMerkleTree::get()`.
+}
