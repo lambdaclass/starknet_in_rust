3a42822202993a003ed03c2323fd46b047aea076
Juan Bono <juanbono94@gmail.com>
2022-12-07 18:13:20 -0300

    Add SyscallHintProcessor (#1)
    * add tests and processor scaffolding
    
    * apply cargo fmt
    
    * Remove access to private fields
    
    * Relocate macros to another mod
    
    * Move vm macro to utils mod
    
    * merge and clean a bit the test
    
    * Update dependencies with cairo-rs link instead of local
    
    * cargo fmt
    
    * Syscall trait (#6)
    
    * Trait definitions
    
    * Fix format
    
    Co-authored-by: SantiagoPittella <pittellasantiago@gmail.com>
    Co-authored-by: Milton <milton.scuderi@lambdaclass.com>
    Co-authored-by: mmsc2 <88055861+mmsc2@users.noreply.github.com>

---
+++ src/core/syscall_handler.rs
@@ -0,0 +1,251 @@
+use std::any::Any;
+use std::collections::HashMap;
+
+use cairo_rs::any_box;
+use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::{
+    BuiltinHintProcessor, HintProcessorData,
+};
+use cairo_rs::hint_processor::hint_processor_definition::{HintProcessor, HintReference};
+use cairo_rs::types::exec_scope::ExecutionScopes;
+use cairo_rs::types::relocatable::{MaybeRelocatable, Relocatable};
+use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
+use cairo_rs::vm::vm_core::VirtualMachine;
+use num_bigint::BigInt;
+
+const DEPLOY_SYSCALL_CODE: &str =
+    "syscall_handler.deploy(segments=segments, syscall_ptr=ids.syscall_ptr)";
+
+pub struct CairoStructProxy;
+
+pub struct SyscallHintProcessor<H: SyscallHandler> {
+    builtin_hint_processor: BuiltinHintProcessor,
+    syscall_handler: H,
+}
+
+impl SyscallHintProcessor<BusinessLogicSyscallHandler> {
+    pub fn new_empty() -> SyscallHintProcessor<BusinessLogicSyscallHandler> {
+        SyscallHintProcessor {
+            builtin_hint_processor: BuiltinHintProcessor::new_empty(),
+            syscall_handler: BusinessLogicSyscallHandler,
+        }
+    }
+}
+impl<H: SyscallHandler> SyscallHintProcessor<H> {
+    pub fn should_run_syscall_hint(
+        &self,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        hint_data: &Box<dyn Any>,
+        constants: &HashMap<String, BigInt>,
+    ) -> Result<bool, VirtualMachineError> {
+        match self
+            .builtin_hint_processor
+            .execute_hint(vm, exec_scopes, hint_data, constants)
+        {
+            Ok(()) => Ok(false),
+            Err(VirtualMachineError::UnknownHint(_)) => Ok(true),
+            Err(e) => Err(e),
+        }
+    }
+
+    fn execute_syscall_hint(
+        &self,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        hint_data: &Box<dyn Any>,
+        constants: &HashMap<String, BigInt>,
+    ) -> Result<(), VirtualMachineError> {
+        let hint_data = hint_data
+            .downcast_ref::<HintProcessorData>()
+            .ok_or(VirtualMachineError::WrongHintData)?;
+
+        match &*hint_data.code {
+            DEPLOY_SYSCALL_CODE => {
+                println!("Running deploy syscall.");
+                Err(VirtualMachineError::NotImplemented)
+            }
+            _ => Err(VirtualMachineError::NotImplemented),
+        }
+    }
+}
+
+impl<H: SyscallHandler> HintProcessor for SyscallHintProcessor<H> {
+    fn execute_hint(
+        &self,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        hint_data: &Box<dyn Any>,
+        constants: &HashMap<String, BigInt>,
+    ) -> Result<(), VirtualMachineError> {
+        if self.should_run_syscall_hint(vm, exec_scopes, hint_data, constants)? {
+            self.execute_syscall_hint(vm, exec_scopes, hint_data, constants)?
+        }
+        Ok(())
+    }
+
+    fn compile_hint(
+        &self,
+        hint_code: &str,
+        ap_tracking_data: &cairo_rs::serde::deserialize_program::ApTracking,
+        reference_ids: &std::collections::HashMap<String, usize>,
+        references: &std::collections::HashMap<
+            usize,
+            cairo_rs::hint_processor::hint_processor_definition::HintReference,
+        >,
+    ) -> Result<Box<dyn Any>, VirtualMachineError> {
+        Ok(any_box!(HintProcessorData {
+            code: hint_code.to_string(),
+            ap_tracking: ap_tracking_data.clone(),
+            ids_data: get_ids_data(reference_ids, references)?,
+        }))
+    }
+}
+
+fn get_ids_data(
+    reference_ids: &HashMap<String, usize>,
+    references: &HashMap<usize, HintReference>,
+) -> Result<HashMap<String, HintReference>, VirtualMachineError> {
+    let mut ids_data = HashMap::<String, HintReference>::new();
+    for (path, ref_id) in reference_ids {
+        let name = path
+            .rsplit('.')
+            .next()
+            .ok_or(VirtualMachineError::FailedToGetIds)?;
+        ids_data.insert(
+            name.to_string(),
+            references
+                .get(ref_id)
+                .ok_or(VirtualMachineError::FailedToGetIds)?
+                .clone(),
+        );
+    }
+    Ok(ids_data)
+}
+
+pub trait SyscallHandler {
+    fn emit_message(&self, vm: VirtualMachine, syscall_ptr: Relocatable);
+    fn send_message_to_l1(&self, vm: VirtualMachine, syscall_ptr: Relocatable);
+    fn _get_tx_info_ptr(&self, vm: VirtualMachine);
+    fn _deploy(&self, vm: VirtualMachine, syscall_ptr: Relocatable) -> i32;
+    fn _read_and_validate_syscall_request(
+        &self,
+        syscall_name: &str,
+        vm: VirtualMachine,
+        syscall_ptr: Relocatable,
+    ) -> CairoStructProxy;
+    fn _call_contract(
+        &self,
+        syscall_name: &str,
+        vm: VirtualMachine,
+        syscall_ptr: Relocatable,
+    ) -> Vec<i32>;
+    fn _get_caller_address(&self, vm: VirtualMachine, syscall_ptr: Relocatable) -> i32;
+    fn _get_contract_address(&self, vm: VirtualMachine, syscall_ptr: Relocatable) -> i32;
+    fn _storage_read(&self, address: i32) -> i32;
+    fn _storage_write(&self, address: i32, value: i32);
+    fn _allocate_segment(&self, vm: VirtualMachine, data: Vec<MaybeRelocatable>) -> Relocatable;
+}
+
+struct OsSyscallHandler {}
+
+pub struct BusinessLogicSyscallHandler;
+
+impl SyscallHandler for BusinessLogicSyscallHandler {
+    fn emit_message(&self, vm: VirtualMachine, syscall_ptr: Relocatable) {
+        todo!()
+    }
+    fn send_message_to_l1(&self, vm: VirtualMachine, syscall_ptr: Relocatable) {
+        todo!()
+    }
+
+    fn _get_tx_info_ptr(&self, vm: VirtualMachine) {
+        todo!()
+    }
+    fn _deploy(&self, vm: VirtualMachine, syscall_ptr: Relocatable) -> i32 {
+        todo!()
+    }
+    fn _read_and_validate_syscall_request(
+        &self,
+        syscall_name: &str,
+        vm: VirtualMachine,
+        syscall_ptr: Relocatable,
+    ) -> CairoStructProxy {
+        todo!()
+    }
+    fn _call_contract(
+        &self,
+        syscall_name: &str,
+        vm: VirtualMachine,
+        syscall_ptr: Relocatable,
+    ) -> Vec<i32> {
+        todo!()
+    }
+    fn _get_caller_address(&self, vm: VirtualMachine, syscall_ptr: Relocatable) -> i32 {
+        todo!()
+    }
+    fn _get_contract_address(&self, vm: VirtualMachine, syscall_ptr: Relocatable) -> i32 {
+        todo!()
+    }
+    fn _storage_read(&self, address: i32) -> i32 {
+        todo!()
+    }
+    fn _storage_write(&self, address: i32, value: i32) {
+        todo!()
+    }
+    fn _allocate_segment(&self, vm: VirtualMachine, data: Vec<MaybeRelocatable>) -> Relocatable {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::syscall_handler::{SyscallHintProcessor, DEPLOY_SYSCALL_CODE};
+    use crate::utils::test_utils::*;
+    use cairo_rs::any_box;
+    use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::{
+        BuiltinHintProcessor, HintProcessorData,
+    };
+    use cairo_rs::hint_processor::hint_processor_definition::HintProcessor;
+    use cairo_rs::types::exec_scope::ExecutionScopes;
+    use cairo_rs::types::relocatable::{MaybeRelocatable, Relocatable};
+    use cairo_rs::vm::errors::memory_errors::MemoryError;
+    use cairo_rs::vm::errors::vm_errors::VirtualMachineError;
+    use cairo_rs::vm::vm_core::VirtualMachine;
+    use num_bigint::{BigInt, Sign};
+    use std::any::Any;
+    use std::collections::HashMap;
+
+    #[test]
+    fn run_alloc_hint_ap_is_not_empty() {
+        let hint_code = "memory[ap] = segments.add()";
+        let mut vm = vm!();
+        //Add 3 segments to the memory
+        add_segments!(vm, 3);
+        vm.set_ap(6);
+        //Insert something into ap
+        let key = Relocatable::from((1, 6));
+        vm.insert_value(&key, (1, 6)).unwrap();
+        //ids and references are not needed for this test
+        assert_eq!(
+            run_hint!(vm, HashMap::new(), hint_code),
+            Err(VirtualMachineError::MemoryError(
+                MemoryError::InconsistentMemory(
+                    MaybeRelocatable::from((1, 6)),
+                    MaybeRelocatable::from((1, 6)),
+                    MaybeRelocatable::from((3, 0))
+                )
+            ))
+        );
+    }
+
+    // tests that we are executing correctly our syscall hint processor.
+    #[test]
+    fn cannot_run_syscall_hints() {
+        let hint_code = DEPLOY_SYSCALL_CODE;
+        let mut vm = vm!();
+        assert_eq!(
+            run_syscall_hint!(vm, HashMap::new(), hint_code),
+            Err(VirtualMachineError::NotImplemented)
+        );
+    }
+}
---
+++ src/utils.rs
@@ -0,0 +1,101 @@
+#[cfg(test)]
+#[macro_use]
+pub mod test_utils {
+
+    macro_rules! vm {
+        () => {{
+            VirtualMachine::new(
+                BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
+                false,
+                Vec::new(),
+            )
+        }};
+
+        ($use_trace:expr) => {{
+            VirtualMachine::new(
+                BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
+                $use_trace,
+                Vec::new(),
+            )
+        }};
+    }
+    pub(crate) use vm;
+
+    #[macro_export]
+    macro_rules! add_segments {
+        ($vm:expr, $n:expr) => {
+            for _ in 0..$n {
+                $vm.add_memory_segment();
+            }
+        };
+    }
+    pub(crate) use add_segments;
+
+    #[macro_export]
+    macro_rules! exec_scopes_ref {
+        () => {
+            &mut ExecutionScopes::new()
+        };
+    }
+    pub(crate) use exec_scopes_ref;
+
+    #[macro_export]
+    macro_rules! run_hint {
+        ($vm:expr, $ids_data:expr, $hint_code:expr, $exec_scopes:expr, $constants:expr) => {{
+            let hint_data = HintProcessorData::new_default($hint_code.to_string(), $ids_data);
+            let hint_processor = BuiltinHintProcessor::new_empty();
+            hint_processor.execute_hint(&mut $vm, $exec_scopes, &any_box!(hint_data), $constants)
+        }};
+        ($vm:expr, $ids_data:expr, $hint_code:expr, $exec_scopes:expr) => {{
+            let hint_data = HintProcessorData::new_default($hint_code.to_string(), $ids_data);
+            let hint_processor = BuiltinHintProcessor::new_empty();
+            hint_processor.execute_hint(
+                &mut $vm,
+                $exec_scopes,
+                &any_box!(hint_data),
+                &HashMap::new(),
+            )
+        }};
+        ($vm:expr, $ids_data:expr, $hint_code:expr) => {{
+            let hint_data = HintProcessorData::new_default($hint_code.to_string(), $ids_data);
+            let hint_processor = BuiltinHintProcessor::new_empty();
+            hint_processor.execute_hint(
+                &mut $vm,
+                exec_scopes_ref!(),
+                &any_box!(hint_data),
+                &HashMap::new(),
+            )
+        }};
+    }
+    pub(crate) use run_hint;
+
+    #[macro_export]
+    macro_rules! run_syscall_hint {
+        ($vm:expr, $ids_data:expr, $hint_code:expr, $exec_scopes:expr, $constants:expr) => {{
+            let hint_data = HintProcessorData::new_default($hint_code.to_string(), $ids_data);
+            let hint_processor = BuiltinHintProcessor::new_empty();
+            hint_processor.execute_hint(&mut $vm, $exec_scopes, &any_box!(hint_data), $constants)
+        }};
+        ($vm:expr, $ids_data:expr, $hint_code:expr, $exec_scopes:expr) => {{
+            let hint_data = HintProcessorData::new_default($hint_code.to_string(), $ids_data);
+            let hint_processor = BuiltinHintProcessor::new_empty();
+            hint_processor.execute_hint(
+                &mut $vm,
+                $exec_scopes,
+                &any_box!(hint_data),
+                &HashMap::new(),
+            )
+        }};
+        ($vm:expr, $ids_data:expr, $hint_code:expr) => {{
+            let hint_data = HintProcessorData::new_default($hint_code.to_string(), $ids_data);
+            let hint_processor = SyscallHintProcessor::new_empty();
+            hint_processor.execute_hint(
+                &mut $vm,
+                exec_scopes_ref!(),
+                &any_box!(hint_data),
+                &HashMap::new(),
+            )
+        }};
+    }
+    pub(crate) use run_syscall_hint;
+}
