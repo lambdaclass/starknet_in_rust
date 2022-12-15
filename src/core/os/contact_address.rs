e8eba34046949d97cace0d20fdd9bee9ced7b11d
Pedro Fontana <pedro.fontana@lamdaclass.com>
2022-12-15 19:51:08 -0300

    first implementation of fn compute_hash_on_elements + unit tests

--- src/core/os/contact_address.rs
+++ src/core/os/contact_address.rs
@@ -1,14 +1,66 @@
-use num_bigint::BigInt;
+use num_bigint::{BigInt, Sign};
+use starknet_crypto::{pedersen_hash, FieldElement};
 
 use crate::core::errors::syscall_handler_errors::SyscallHandlerError;
 
 pub fn calculate_contract_address_from_hash(
     salt: &BigInt,
     class_hash: &BigInt,
-    constructor_calldata: Vec<&BigInt>,
-    deployer_address: &BigInt,
+    constructor_calldata: Vec<BigInt>,
+    deployer_address: u64,
 ) -> Result<BigInt, SyscallHandlerError> {
+    // pedersen_hash(x, y)
     todo!()
+}
+
+fn compute_hash_on_elements(vec: Vec<BigInt>) -> Result<BigInt, SyscallHandlerError> {
+    let mut felt_vec: Vec<FieldElement> = vec
+        .into_iter()
+        .map(|num| FieldElement::from_dec_str(&num.to_str_radix(10)).unwrap())
+        .collect();
+    felt_vec.push(FieldElement::from(felt_vec.len()));
+    felt_vec.insert(0, FieldElement::from(0_u16));
+    let felt_result = felt_vec
+        .into_iter()
+        .reduce(|x, y| pedersen_hash(&x, &y))
+        .unwrap();
+    let result = BigInt::from_bytes_be(Sign::Plus, &felt_result.to_bytes_be());
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compute_hash_on_elements() {
+        let v1 = vec![BigInt::from(1)];
+        let result1 = compute_hash_on_elements(v1).unwrap();
+        let v2 = vec![
+            BigInt::from(1),
+            BigInt::from(2),
+            BigInt::from(3),
+            BigInt::from(4),
+        ];
+        let result2 = compute_hash_on_elements(v2).unwrap();
+
+        assert_eq!(
+            result1,
+            BigInt::parse_bytes(
+                b"3416122613774376552656914666405609308365843021349846777564025639164215424932",
+                10
+            )
+            .unwrap()
+        );
+        assert_eq!(
+            result2,
+            BigInt::parse_bytes(
+                b"2904394281987469213428308031512088126582033652660815761074595741628288213124",
+                10
+            )
+            .unwrap()
+        );
+    }
 }
 
 /*
