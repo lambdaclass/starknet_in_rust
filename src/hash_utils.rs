7546af0234a454084bd0fadf5f6ef08d64bc70dd
Pedro Fontana <pedro.fontana@lamdaclass.com>
2022-12-19 17:46:40 -0300

    Add test_calculate_contract_address_from_hash

--- src/hash_utils.rs
+++ src/hash_utils.rs
@@ -57,8 +57,6 @@ mod tests {
     fn test_compute_hash_on_elements() {
         let v1 = vec![bigint!(1)];
         let result1 = compute_hash_on_elements(&v1);
-        let v2 = vec![bigint!(1), bigint!(2), bigint!(3), bigint!(4)];
-        let result2 = compute_hash_on_elements(&v2);
 
         assert_eq!(
             result1,
@@ -66,10 +64,61 @@ mod tests {
                 b"3416122613774376552656914666405609308365843021349846777564025639164215424932"
             ))
         );
+
+        let v2 = vec![bigint!(1), bigint!(2), bigint!(3), bigint!(4)];
+        let result2 = compute_hash_on_elements(&v2);
+
         assert_eq!(
             result2,
             Ok(bigint_str!(
                 b"2904394281987469213428308031512088126582033652660815761074595741628288213124"
+            ))
+        );
+
+        let v3 = vec![
+            bigint!(0),
+            bigint!(15),
+            bigint!(1232),
+            bigint_str!(b"8918274123"),
+            bigint!(46534),
+        ];
+        let result3 = compute_hash_on_elements(&v3);
+
+        assert_eq!(
+            result3,
+            Ok(bigint_str!(
+                b"183592112522859067029852736072730560878910822643949684307130835577741550985"
+            ))
+        );
+    }
+
+    #[test]
+    fn test_calculate_contract_address_from_hash() {
+        let result_1 = calculate_contract_address_from_hash(
+            &bigint!(1),
+            &bigint!(2),
+            &vec![bigint!(3), bigint!(4)],
+            5,
+        );
+
+        assert_eq!(
+            result_1,
+            Ok(bigint_str!(
+                b"1885555033409779003200115284723341705041371741573881252130189632266543809788"
+            ))
+        );
+
+        let result_2 = calculate_contract_address_from_hash(
+            &bigint!(756),
+            &bigint!(543),
+            &vec![bigint!(124543), bigint!(5345345), bigint!(89)],
+            87123,
+        );
+
+        assert_eq!(
+            result_2,
+            Ok(bigint_str!(
+                b"2864535578326518086698404810362457605993575745991923092043914398137702365865"
             ))
         );
     }
