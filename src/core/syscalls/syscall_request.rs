a5bc23570d703eecb64a04c595970d36c8cae4af
Pedro Fontana <pedro.fontana@lamdaclass.com>
2022-12-29 14:14:24 -0300

    Add GetTxInfoRequest

--- src/core/syscalls/syscall_request.rs
+++ src/core/syscalls/syscall_request.rs
@@ -7,7 +7,7 @@ use num_bigint::BigInt;
 #[derive(Debug, PartialEq)]
 pub(crate) enum SyscallRequest {
     EmitEvent(EmitEventStruct),
-    GetTxInfo(TxInfoStruct),
+    GetTxInfo(GetTxInfoRequest),
     Deploy(DeployRequestStruct),
     SendMessageToL1(SendMessageToL1SysCall),
     LibraryCall(LibraryCallStruct),
@@ -74,6 +74,11 @@ pub(crate) struct GetCallerAddressRequest {
     pub(crate) _selector: BigInt,
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub(crate) struct GetTxInfoRequest {
+    pub(crate) selector: BigInt,
+}
+
 pub(crate) trait FromPtr {
     fn from_ptr(
         vm: &VirtualMachine,
@@ -127,6 +132,12 @@ impl From<GetBlockTimestampRequest> for SyscallRequest {
     }
 }
 
+impl From<GetTxInfoRequest> for SyscallRequest {
+    fn from(get_tx_info_request: GetTxInfoRequest) -> SyscallRequest {
+        SyscallRequest::GetTxInfo(get_tx_info_request)
+    }
+}
+
 impl FromPtr for EmitEventStruct {
     fn from_ptr(
         vm: &VirtualMachine,
@@ -162,37 +173,14 @@ pub(crate) struct TxInfoStruct {
     pub(crate) nonce: BigInt,
 }
 
-impl From<TxInfoStruct> for SyscallRequest {
-    fn from(tx_info_struct: TxInfoStruct) -> SyscallRequest {
-        SyscallRequest::GetTxInfo(tx_info_struct)
-    }
-}
-
-impl FromPtr for TxInfoStruct {
+impl FromPtr for GetTxInfoRequest {
     fn from_ptr(
         vm: &VirtualMachine,
         syscall_ptr: Relocatable,
     ) -> Result<SyscallRequest, SyscallHandlerError> {
-        let version = get_integer(vm, &(syscall_ptr))?;
-        let account_contract_address = get_big_int(vm, &(&syscall_ptr + 1))?;
-        let max_fee = get_big_int(vm, &(&syscall_ptr + 2))?;
-        let signature_len = get_integer(vm, &(&syscall_ptr + 3))?;
-        let signature = get_relocatable(vm, &(&syscall_ptr + 4))?;
-        let transaction_hash = get_big_int(vm, &(&syscall_ptr + 5))?;
-        let chain_id = get_integer(vm, &(&syscall_ptr + 6))?;
-        let nonce = get_big_int(vm, &(&syscall_ptr + 7))?;
+        let selector = get_big_int(vm, &(syscall_ptr))?;
 
-        Ok(TxInfoStruct {
-            version,
-            account_contract_address,
-            max_fee,
-            signature_len,
-            signature,
-            transaction_hash,
-            chain_id,
-            nonce,
-        }
-        .into())
+        Ok(GetTxInfoRequest { selector }.into())
     }
 }
 
@@ -306,6 +294,12 @@ impl FromPtr for GetBlockTimestampRequest {
     }
 }
 impl CountFields for GetBlockTimestampRequest {
+    fn count_fields() -> usize {
+        1
+    }
+}
+
+impl CountFields for GetTxInfoRequest {
     fn count_fields() -> usize {
         1
     }
