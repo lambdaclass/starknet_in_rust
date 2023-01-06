e667b13fbfbdf25719adb4c0faf2bd45d02fa370
Pedro Fontana <pedro.fontana@lambdaclass.com>
2023-01-06 14:37:14 -0300

    Implement StateReader trait for CachedState (#62)
    * implement StateCache
    
    * Modify StateReader trait
    
    * Implement StatedReader for CachedStated
    
    * Use reference in funtions
    
    * Add StateCache unit tests
    
    * Rename src/state/state_chache.rs -> src/state/cached_state.rs
    
    * Revert "Rename src/state/state_chache.rs -> src/state/cached_state.rs"
    
    This reverts commit d34a0ea4c64c70dea3f894b6c964e7b1ffcce255.
    
    * Create mod cached_state.rs
    
    * Remove unwraps
    
    * Modify StateReader methods to return references
    
    * Reorder CachedState methods
    
    * Implement State trait for CachedState (#65)
    
    * Add State:Errors
    
    * Implement State for CachedState
    
    * Solve merge conflicts
    
    * cargo fmt
    
    Co-authored-by: Pedro Fontana <pedro.fontana@lamdaclass.com>
    
    Co-authored-by: Pedro Fontana <pedro.fontana@lamdaclass.com>

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,3 +1,4 @@
+pub(crate) mod cached_state;
 pub mod state_api;
 pub mod state_api_objects;
 pub(crate) mod state_chache;
--- src/state/state_api.rs
+++ src/state/state_api.rs
@@ -1,25 +1,29 @@
 use num_bigint::BigInt;
 
-use crate::services::api::contract_class::ContractClass;
+use crate::{core::errors::state_errors::StateError, services::api::contract_class::ContractClass};
 
-use super::state_api_objects::BlockInfo;
+use super::{state_api_objects::BlockInfo, state_chache::StorageEntry};
 
 pub(crate) trait StateReader {
     /// Returns the contract class of the given class hash.
-    fn get_contract_class(&self, class_hash: &[u8]) -> ContractClass;
+    fn get_contract_class(&mut self, class_hash: &[u8]) -> Result<&ContractClass, StateError>;
     /// Returns the class hash of the contract class at the given address.
-    fn get_class_hash_at(&self, contract_address: &BigInt) -> Vec<u8>;
+    fn get_class_hash_at(&mut self, contract_address: &BigInt) -> Result<&Vec<u8>, StateError>;
     /// Returns the nonce of the given contract instance.
-    fn get_nonce_at(&self, contract_address: &BigInt) -> BigInt;
+    fn get_nonce_at(&mut self, contract_address: &BigInt) -> Result<&BigInt, StateError>;
     /// Returns the storage value under the given key in the given contract instance.
-    fn get_storage_at(&self, contract_address: &BigInt, key: &BigInt) -> BigInt;
+    fn get_storage_at(&mut self, storage_entry: &StorageEntry) -> Result<&BigInt, StateError>;
 }
 
 pub(crate) trait State {
-    fn block_info(&self) -> BlockInfo;
+    fn get_block_info(&self) -> &BlockInfo;
     fn set_contract_class(&mut self, class_hash: &[u8], contract_class: &ContractClass);
-    fn deploy_contract(&self, contract_address: &BigInt, class_hash: &[u8]);
-    fn increment_nonce(&mut self, contract_address: &BigInt);
-    fn update_block_info(&mut self, block_info: &BlockInfo);
-    fn set_storage_at(&mut self, contract_address: &BigInt, key: &BigInt, value: BigInt);
+    fn deploy_contract(
+        &mut self,
+        contract_address: BigInt,
+        class_hash: Vec<u8>,
+    ) -> Result<(), StateError>;
+    fn increment_nonce(&mut self, contract_address: &BigInt) -> Result<(), StateError>;
+    fn update_block_info(&mut self, block_info: BlockInfo);
+    fn set_storage_at(&mut self, storage_entry: &StorageEntry, value: BigInt);
 }
