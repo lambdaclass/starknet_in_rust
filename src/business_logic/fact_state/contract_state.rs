4a290201df48119f980a43c5526fa4121d90f83b
Pedro Fontana <pedro.fontana@lamdaclass.com>
2023-01-13 13:38:58 -0300

    Implement struct ContractState

---
+++ src/business_logic/fact_state/contract_state.rs
@@ -0,0 +1,32 @@
+use felt::Felt;
+
+use crate::business_logic::state::cached_state::UNINITIALIZED_CLASS_HASH;
+
+pub(crate) struct ContractState {
+    contract_hash: Vec<u8>,
+    nonce: Felt,
+}
+
+impl ContractState {
+    fn create(contract_hash: Vec<u8>, nonce: Felt) -> Self {
+        Self {
+            contract_hash,
+            nonce,
+        }
+    }
+
+    pub(crate) fn empty() -> Self {
+        Self {
+            contract_hash: UNINITIALIZED_CLASS_HASH.to_vec(),
+            nonce: 0.into(),
+        }
+    }
+
+    fn initialized(&self) -> bool {
+        !(self.contract_hash == UNINITIALIZED_CLASS_HASH)
+    }
+
+    fn is_empty(&self) -> bool {
+        !self.initialized()
+    }
+}
--- src/business_logic/fact_state/mod.rs
+++ src/business_logic/fact_state/mod.rs
@@ -1,1 +1,2 @@
+pub(crate) mod contract_state;
 pub mod state;
