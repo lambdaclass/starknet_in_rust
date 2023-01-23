5101ff9916f3aad10e9d1f9bbf07139cadf8a05f
mmsc2 <88055861+mmsc2@users.noreply.github.com>
2023-01-23 17:05:40 -0300

    Implement StateDiff (#82)
    * Implement StateDiff
    
    * complete functions for state diff and block info
    
    * Cargo
    
    * Fix get_keys function
    
    * Add tests for utils.rs
    
    * Add test for cache_storage_mapping
    
    * Delete unnecessary merge function

--- src/business_logic/state/state_api_objects.rs
+++ src/business_logic/state/state_api_objects.rs
@@ -1,4 +1,4 @@
-use crate::utils::Address;
+use crate::{starkware_utils::starkware_errors::StarkwareError, utils::Address};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct BlockInfo {
@@ -16,6 +16,33 @@ pub struct BlockInfo {
 
     // The version of StarkNet system (e.g. "0.10.3").
     starknet_version: String,
+}
+
+impl BlockInfo {
+    pub fn empty(sequencer_address: Address) -> Self {
+        BlockInfo {
+            block_number: 1,
+            block_timestamp: 0,
+            gas_price: 0,
+            sequencer_address,
+            starknet_version: "0.0.0".to_string(),
+        }
+    }
+
+    pub fn validate_legal_progress(
+        &self,
+        next_block_info: BlockInfo,
+    ) -> Result<(), StarkwareError> {
+        if self.block_number + 1 != next_block_info.block_number {
+            return Err(StarkwareError::InvalidBlockNumber);
+        }
+
+        if self.block_timestamp >= next_block_info.block_timestamp {
+            return Err(StarkwareError::InvalidBlockTimestamp);
+        }
+
+        Ok(())
+    }
 }
 
 impl Default for BlockInfo {
