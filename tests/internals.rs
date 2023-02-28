45a8e8222605bdca73701bdb26d6d3c4af6f7b69
Esteve Soler Arderiu <soler.arderiu@gmail.com>
2023-02-28 20:05:24 +0100

    Fix `CachedState` insertion.

--- tests/internals.rs
+++ tests/internals.rs
@@ -8,33 +8,30 @@ use starknet_rs::{
     },
     definitions::general_config::StarknetGeneralConfig,
     services::api::contract_class::ContractClass,
-    starknet_storage::{
-        dict_storage::{DictStorage, Prefix},
-        storage::Storage,
-    },
+    starknet_storage::dict_storage::DictStorage,
     utils::{felt_to_hash, Address},
 };
 use std::{collections::HashMap, path::PathBuf};
 
-const ACCOUNT_CONTRACT_PATH: &str = "starknet_programs/account_without_validations.json";
+const ACCOUNT_CONTRACT_PATH: &str = "starknet_programs/account_without_validation.json";
 const ERC20_CONTRACT_PATH: &str = "starknet_programs/erc20_contract_without_some_syscalls.json";
 const TEST_CONTRACT_PATH: &str = "starknet_programs/test_contract.json";
 
 lazy_static! {
     // Addresses.
-    static ref TEST_ACCOUNT_CONTRACT_ADDRESS: Address = Address(felt_str!("0x101"));
-    static ref TEST_CONTRACT_ADDRESS: Address = Address(felt_str!("0x100"));
+    static ref TEST_ACCOUNT_CONTRACT_ADDRESS: Address = Address(felt_str!("257"));
+    static ref TEST_CONTRACT_ADDRESS: Address = Address(felt_str!("256"));
 
     // Class hashes.
-    static ref TEST_ACCOUNT_CONTRACT_CLASS_HASH: Felt = felt_str!("0x111");
-    static ref TEST_CLASS_HASH: Felt = felt_str!("0x110");
-    static ref TEST_ERC20_CONTRACT_CLASS_HASH: Felt = felt_str!("0x1010");
+    static ref TEST_ACCOUNT_CONTRACT_CLASS_HASH: Felt = felt_str!("273");
+    static ref TEST_CLASS_HASH: Felt = felt_str!("272");
+    static ref TEST_ERC20_CONTRACT_CLASS_HASH: Felt = felt_str!("4112");
 
     // Storage keys.
     static ref TEST_ERC20_ACCOUNT_BALANCE_KEY: Felt =
-        felt_str!("0x2a2c49c4dba0d91b34f2ade85d41d09561f9a77884c15ba2ab0f2241b080deb");
+        felt_str!("1192211877881866289306604115402199097887041303917861778777990838480655617515");
     static ref TEST_ERC20_SEQUENCER_BALANCE_KEY: Felt =
-        felt_str!("0x723973208639b7839ce298f7ffea61e3f9533872defd7abdb91023db4658812");
+        felt_str!("3229073099929281304021185011369329892856197542079132996799046100564060768274");
 
     // Others.
     static ref ACTUAL_FEE: Felt = 2.into();
@@ -49,7 +46,7 @@ where
 
 #[allow(dead_code)]
 fn create_account_tx_test_state(
-) -> Result<CachedState<InMemoryStateReader>, Box<dyn std::error::Error>> {
+) -> Result<(StarknetGeneralConfig, CachedState<InMemoryStateReader>), Box<dyn std::error::Error>> {
     let general_config = StarknetGeneralConfig::default();
 
     let test_contract_class_hash = TEST_CLASS_HASH.clone();
@@ -95,38 +92,33 @@ fn create_account_tx_test_state(
         ),
     ]);
 
-    Ok(CachedState::new(
-        InMemoryStateReader::new(
-            {
-                let mut storage = DictStorage::new();
+    let cached_state = CachedState::new(
+        {
+            let mut state_reader = InMemoryStateReader::new(DictStorage::new(), DictStorage::new());
 
-                for (contract_address, class_hash) in address_to_class_hash {
-                    let storage_keys = storage_view
-                        .iter()
-                        .filter_map(|((k0, k1), v)| {
-                            (k0 == &contract_address).then_some((felt_to_hash(k1), v.clone()))
-                        })
-                        .collect();
+            for (contract_address, class_hash) in address_to_class_hash {
+                let storage_keys = storage_view
+                    .iter()
+                    .filter_map(|((k0, k1), v)| {
+                        (k0 == &contract_address).then_some((k1.clone(), v.clone()))
+                    })
+                    .collect();
 
-                    storage.set_value(
-                        &(Prefix::ContractState, felt_to_hash(&contract_address.0)),
-                        serde_json::to_vec(&ContractState::new(
-                            felt_to_hash(&class_hash),
-                            Felt::zero(),
-                            storage_keys,
-                        ))?,
-                    )?;
-                }
+                state_reader.contract_states_mut().insert(
+                    contract_address,
+                    ContractState::new(felt_to_hash(&class_hash), Felt::zero(), storage_keys),
+                );
+            }
 
-                storage
-            },
-            DictStorage::new(),
-        ),
+            state_reader
+        },
         Some(
             class_hash_to_class
                 .into_iter()
                 .map(|(k, v)| (felt_to_hash(&k), v))
                 .collect(),
         ),
-    ))
+    );
+
+    Ok((general_config, cached_state))
 }
