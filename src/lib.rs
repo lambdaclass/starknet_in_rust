48228e98509b3799e72fe8798fceab620b4bb102
juanbono <juanbono94@gmail.com>
2023-03-03 19:30:40 -0300

    fix the implementation of call_contract

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 use business_logic::{
     execution::{
         execution_entry_point::ExecutionEntryPoint,
-        objects::{CallType, TransactionExecutionContext, TransactionExecutionInfo},
+        objects::{CallInfo, CallType, TransactionExecutionContext, TransactionExecutionInfo},
     },
     fact_state::state::ExecutionResourcesManager,
     state::{
@@ -35,44 +35,30 @@ pub mod utils;
 
 type TransactionResult<T> = Result<T, TransactionError>;
 
-#[derive(Debug)]
-pub struct ContractCall {
-    contract_address: Address,
-    entry_point_selector: Felt,
-    calldata: Vec<Felt>,
-    caller_address: Address,
-    entrypoint_type: EntryPointType,
-    call_type: CallType,
-    class_hash: ClassHash,
-}
+pub struct SimulationFlags;
 
-pub struct Starknet {}
+pub struct Starknet;
 
 impl Starknet {
     pub fn call_contract<T>(
         state: &mut CachedState<T>,
-        call: ContractCall,
-        resources_manager: &mut ExecutionResourcesManager,
+        tx: Transaction,
         config: &StarknetGeneralConfig,
-        tx_execution_context: &TransactionExecutionContext,
     ) -> TransactionResult<Vec<Felt>>
     where
         T: State + StateReader + Clone + Default,
     {
-        let entrypoint = ExecutionEntryPoint::new(
-            call.contract_address,
-            call.calldata,
-            call.entry_point_selector,
-            call.caller_address,
-            call.entrypoint_type,
-            Some(call.call_type),
-            Some(call.class_hash),
-        );
-
-        let call_info =
-            entrypoint.execute(state, config, resources_manager, tx_execution_context)?;
-
-        Ok(call_info.retdata)
+        let mut state_copy = state.clone();
+        tx.execute(&mut state_copy, config)
+            .and_then(|tx_exec| {
+                tx_exec
+                    .call_info
+                    .ok_or(TransactionError::StarknetError(
+                        "Empty CallInfo.".to_string(),
+                    ))
+                    .map(|r| r.retdata)
+            })
+            .map_err(Into::into)
     }
 
     pub fn estimate_fee<T>(
@@ -84,6 +70,7 @@ impl Starknet {
         T: State + StateReader + Clone + Default,
     {
         let mut state_copy = state.clone();
+        // TODO: check if the estimate_fee is the actual_fee.
         tx.execute(&mut state_copy, config)
             .map(|tx_exec| tx_exec.actual_fee)
             .map_err(Into::into)
@@ -99,5 +86,21 @@ impl Starknet {
         T: State + StateReader + Clone + Default,
     {
         tx.execute(state, config).map_err(Into::into)
+    }
+
+    pub fn simulate_tx<T>(
+        state: &CachedState<T>,
+        tx: Transaction,
+        config: &StarknetGeneralConfig,
+        _options: Option<SimulationFlags>,
+    ) -> TransactionResult<(TransactionExecutionInfo, u64)>
+    where
+        T: State + StateReader + Clone + Default,
+    {
+        let mut state_copy = state.clone();
+        // TODO: check if the estimate_fee is the actual_fee.
+        tx.execute(&mut state_copy, config)
+            .map(|tx_exec| (tx_exec.clone(), tx_exec.actual_fee))
+            .map_err(Into::into)
     }
 }
