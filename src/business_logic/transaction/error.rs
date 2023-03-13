c96381cadee7f07b4fa48f1d29a2e4e83515b49f
Federica <fedemoletta@hotmail.com>
2023-03-13 17:29:46 -0300

    Fix some typos +

--- src/business_logic/transaction/error.rs
+++ src/business_logic/transaction/error.rs
@@ -45,7 +45,7 @@ pub enum TransactionError {
     ClassAlreadyDeclared([u8; 32]),
     #[error(transparent)]
     Starkware(#[from] StarkwareError),
-    #[error("Expected a relocatable value but get wrong data type")]
+    #[error("Expected a relocatable value but got an integer")]
     NotARelocatableValue,
     #[error("Error converting from {0} to {1}")]
     ErrorInDataConversion(String, String),
@@ -55,21 +55,19 @@ pub enum TransactionError {
     UnexpectedHolesL2toL1Messages,
     #[error("Attemp to return class hash with incorrect call type")]
     CallTypeIsNotDelegate,
-    #[error("Attemp to return code address when is None")]
+    #[error("Attemp to return code address when it is None")]
     AttempToUseNoneCodeAddress,
-    #[error("error recovering class hash from storage")]
+    #[error("Error recovering class hash from storage")]
     FailToReadClassHash,
     #[error("Missing contract class after fetching")]
     MissigContractClass,
-    #[error("contract address {0:?} not deployed")]
+    #[error("Contract address {0:?} is not deployed")]
     NotDeployedContract([u8; 32]),
-    #[error("error allocating memory segment")]
-    ErrorAllocatingSegment,
     #[error("Non-unique entry points are not possible in a ContractClass object")]
     NonUniqueEntryPoint,
     #[error("Requested entry point was not found")]
     EntryPointNotFound,
-    #[error("Ptr result diverges after calculate final stacks")]
+    #[error("Ptr result diverges after calculating final stacks")]
     OsContextPtrNotEqual,
     #[error("Illegal OS ptr offset")]
     IllegalOsPtrOffset,
@@ -83,7 +81,7 @@ pub enum TransactionError {
     InvalidStopPointer(Relocatable, Relocatable),
     #[error("Invalid entry point types")]
     InvalidEntryPoints,
-    #[error("Expected and int value got a Relocatable")]
+    #[error("Expected an int value got a Relocatable")]
     NotAnInt,
     #[error("Out of bounds write to a read-only segment.")]
     OutOfBound,
@@ -93,8 +91,8 @@ pub enum TransactionError {
     TraceException(#[from] TraceError),
     #[error(transparent)]
     MemoryException(#[from] MemoryError),
-    #[error("Expected Relocatable; found None")]
-    InvalidInitialFp,
+    #[error("Missing initial_fp")]
+    MissingInitialFp,
     #[error("Transaction context is invalid")]
     InvalidTxContext,
     #[error("{0}")]
