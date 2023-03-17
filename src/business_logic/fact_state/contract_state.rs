5f35882129d7238e71dc2d38fff9f20ce4bc118f
Martina <martina@martina>
2023-03-17 17:28:37 -0300

    delete unused code: ContractState and Storage

--- src/business_logic/fact_state/contract_state.rs
+++ src/business_logic/fact_state/contract_state.rs
@@ -1,47 +1,4 @@
-use crate::{
-    business_logic::state::cached_state::UNINITIALIZED_CLASS_HASH,
-    utils::{Address, ClassHash},
-};
-use felt::Felt;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-#[derive(Deserialize, Serialize, PartialEq, Eq, Debug, Clone)]
-pub struct ContractState {
-    pub(crate) class_hash: ClassHash,
-    pub(crate) nonce: Felt,
-    pub(crate) storage_keys: HashMap<Felt, Felt>,
-}
-
-impl ContractState {
-    pub fn new(class_hash: ClassHash, nonce: Felt, storage_keys: HashMap<Felt, Felt>) -> Self {
-        Self {
-            class_hash,
-            nonce,
-            storage_keys,
-        }
-    }
-
-    // TODO: Remove warning inhibitor when finally used.
-    #[allow(dead_code)]
-    pub(crate) fn empty() -> Self {
-        Self {
-            class_hash: *UNINITIALIZED_CLASS_HASH,
-            nonce: 0.into(),
-            storage_keys: HashMap::new(),
-        }
-    }
-
-    fn initialized(&self) -> bool {
-        &self.class_hash != UNINITIALIZED_CLASS_HASH
-    }
-
-    // TODO: Remove warning inhibitor when finally used.
-    #[allow(dead_code)]
-    fn is_empty(&self) -> bool {
-        !self.initialized()
-    }
-}
+use crate::utils::Address;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct StateSelector {
