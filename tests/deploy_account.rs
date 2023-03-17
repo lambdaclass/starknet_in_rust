d0c70976230658dc88e4808d96065f126d158b09
Tomás <tomas.gruner@lambdaclass.com>
2023-03-17 20:49:57 -0300

    Use correct class_hash in test

--- tests/deploy_account.rs
+++ tests/deploy_account.rs
@@ -68,8 +68,10 @@ fn internal_deploy_account() {
             None,
             Some(CallInfo {
                 call_type: Some(CallType::Call),
-                contract_address: Address(felt_str!("1351769743764599227746416364615306404319526869558988948822078481252102329345")),
-                class_hash: Some(*b"\x06\xf5\x00\xf5'5]\xfd\xb8\t<\x7f\xe4nos\xc9j\x86s\x92\xb4\x9f\xa4\x15zuu8\x92\x859"),
+                contract_address: Address(felt_str!(
+                    "3577223136242220508961486249701638158054969090851914040041358274796489907314"
+                )),
+                class_hash: Some(class_hash),
                 entry_point_selector: Some(CONSTRUCTOR_ENTRY_POINT_SELECTOR.clone()),
                 entry_point_type: Some(EntryPointType::Constructor),
                 ..Default::default()
