b5ce8a88567bc02dce8ac2293736fa5d2e1fe3ad
Roberto Catalan <rcatalan@itba.edu.ar>
2023-03-20 15:17:52 -0300

    Add test for balanceOf

--- tests/nft/erc721.rs
+++ tests/nft/erc721.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use cairo_rs::vm::runners::cairo_runner::ExecutionResources;
 use felt::Felt;
 use num_traits::Zero;
@@ -41,6 +43,13 @@ fn name(calldata: &[Felt], call_config: &mut CallConfig) -> Result<CallInfo, Tra
 
 fn symbol(calldata: &[Felt], call_config: &mut CallConfig) -> Result<CallInfo, TransactionError> {
     execute_entry_point("symbol", calldata, call_config)
+}
+
+fn balanceOf(
+    calldata: &[Felt],
+    call_config: &mut CallConfig,
+) -> Result<CallInfo, TransactionError> {
+    execute_entry_point("balanceOf", calldata, call_config)
 }
 
 #[test]
@@ -242,4 +251,78 @@ fn erc721_symbol_test() {
     };
 
     assert_eq!(symbol(&[], &mut call_config).unwrap(), expected_call_info);
+}
+
+#[test]
+fn erc721_balanceOf() {
+    let address = Address(1111.into());
+    let class_hash = [1; 32];
+    let mut state = setup_contract("starknet_programs/ERC721.json", &address, class_hash);
+    let entry_points_by_type = state
+        .get_contract_class(&class_hash)
+        .unwrap()
+        .entry_points_by_type()
+        .clone();
+
+    let caller_address = Address(666.into());
+    let general_config = StarknetGeneralConfig::default();
+    let mut resources_manager = ExecutionResourcesManager::default();
+    let entry_point_type = EntryPointType::External;
+
+    let collection_name = Felt::from_bytes_be("some-nft".as_bytes());
+    let collection_symbol = Felt::from(555);
+    let to = Felt::from(666);
+    let calldata = [collection_name, collection_symbol, to].to_vec();
+
+    let entry_point_type_constructor = EntryPointType::Constructor;
+    let mut call_config = CallConfig {
+        state: &mut state,
+        caller_address: &caller_address,
+        address: &address,
+        class_hash: &class_hash,
+        entry_points_by_type: &entry_points_by_type,
+        entry_point_type: &entry_point_type_constructor,
+        general_config: &general_config,
+        resources_manager: &mut resources_manager,
+    };
+
+    contructor(&calldata, &mut call_config).unwrap();
+
+    //owner to check balance
+    let calldata = [Felt::from(666)].to_vec();
+
+    call_config.entry_point_type = &entry_point_type;
+
+    let entrypoint_selector = Felt::from_bytes_be(&calculate_sn_keccak(b"balanceOf"));
+
+    //expected result should be 1 in uint256. So in Felt it should be [1,0]
+    let expected_read_result = vec![Felt::from(1_u8), Felt::from(0_u8)];
+
+    let mut accessed_storage_keys = HashSet::new();
+    let mut balance = get_accessed_keys("ERC721_balances", vec![vec![666_u32.into()]])
+        .drain()
+        .collect::<Vec<[u8; 32]>>()[0];
+    accessed_storage_keys.insert(balance);
+    balance[31] += 1;
+    accessed_storage_keys.insert(balance);
+
+    let expected_call_info = CallInfo {
+        caller_address: Address(666.into()),
+        call_type: Some(CallType::Delegate),
+        contract_address: Address(1111.into()),
+        entry_point_selector: Some(entrypoint_selector),
+        entry_point_type: Some(EntryPointType::External),
+        calldata: calldata.clone(),
+        retdata: expected_read_result.clone(),
+        execution_resources: ExecutionResources::default(),
+        class_hash: Some(class_hash),
+        accessed_storage_keys: accessed_storage_keys,
+        storage_read_values: expected_read_result,
+        ..Default::default()
+    };
+
+    assert_eq!(
+        balanceOf(&calldata, &mut call_config).unwrap(),
+        expected_call_info
+    );
 }
