afdeca8dce73272baa6313b0d9b0f835f6dcc756
Milton <milton.scuderi@lambdaclass.com>
2023-03-21 12:37:34 -0300

    Implement methods of starknet state

--- crates/starknet-rs-py/src/starknet_state.rs
+++ crates/starknet-rs-py/src/starknet_state.rs
@@ -2,9 +2,15 @@ use cairo_felt::Felt;
 use num_bigint::BigUint;
 use pyo3::{exceptions::PyRuntimeError, prelude::*};
 use starknet_rs::{
-    business_logic::execution::objects::TransactionExecutionInfo,
+    business_logic::transaction::transactions::Transaction,
     services::api::contract_class::ContractClass,
-    testing::starknet_state::StarknetState as InnerStarknetState, utils::Address,
+    testing::{starknet_state::StarknetState as InnerStarknetState, type_utils::ExecutionInfo},
+    utils::Address,
+};
+
+use crate::types::{
+    call_info::PyCallInfo, contract_class::PyContractClass,
+    transaction_execution_info::PyTransactionExecutionInfo,
 };
 #[pyclass]
 #[pyo3(name = "StarknetState")]
@@ -16,11 +22,13 @@ impl PyStarknetState {
     pub fn declare(
         &mut self,
         contract_class: ContractClass,
-    ) -> PyResult<([u8; 32], TransactionExecutionInfo)> {
-        Ok(self
+    ) -> PyResult<([u8; 32], PyTransactionExecutionInfo)> {
+        let (hash, exec_info) = self
             .inner
             .declare(contract_class)
-            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+
+        Ok((hash, PyTransactionExecutionInfo::from(exec_info)))
     }
 
     pub fn invoke_raw(
@@ -31,7 +39,7 @@ impl PyStarknetState {
         max_fee: u64,
         signature: Option<Vec<BigUint>>,
         nonce: Option<BigUint>,
-    ) -> PyResult<TransactionExecutionInfo> {
+    ) -> PyResult<PyTransactionExecutionInfo> {
         let address = Address(contract_address.into());
         let selector = selector.into();
 
@@ -48,14 +56,87 @@ impl PyStarknetState {
             ),
             None => None,
         };
+
         let nonce = match nonce {
             Some(n) => Some(Felt::from(n)),
             None => None,
         };
 
-        Ok(self
+        let exec_info = self
             .inner
             .invoke_raw(address, selector, calldata, max_fee, signature, nonce)
-            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+
+        Ok(PyTransactionExecutionInfo::from(exec_info))
+    }
+
+    pub fn execute_entry_point_raw(
+        &mut self,
+        contract_address: BigUint,
+        entry_point_selector: BigUint,
+        calldata: Vec<BigUint>,
+        caller_address: BigUint,
+    ) -> PyResult<PyCallInfo> {
+        let calldata = calldata
+            .into_iter()
+            .map(|v| Felt::from(v))
+            .collect::<Vec<Felt>>();
+        let entry_point_selector = Felt::from(entry_point_selector);
+
+        let call_info = self
+            .inner
+            .execute_entry_point_raw(
+                Address(Felt::from(contract_address)),
+                entry_point_selector,
+                calldata,
+                Address(Felt::from(caller_address)),
+            )
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+
+        Ok(PyCallInfo::from(call_info))
+    }
+
+    pub fn deploy(
+        &mut self,
+        contract_class: PyContractClass,
+        constructor_calldata: Vec<BigUint>,
+        contract_address_salt: BigUint,
+    ) -> PyResult<(BigUint, PyTransactionExecutionInfo)> {
+        let contract_class = contract_class.inner;
+        let constructor_calldata = constructor_calldata
+            .into_iter()
+            .map(|v| Felt::from(v))
+            .collect::<Vec<Felt>>();
+        let contract_address_salt = Address(Felt::from(contract_address_salt));
+
+        let (address, exec_info) = self
+            .inner
+            .deploy(contract_class, constructor_calldata, contract_address_salt)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+        Ok((
+            address.0.to_biguint(),
+            PyTransactionExecutionInfo::from(exec_info),
+        ))
+    }
+
+    pub fn execute_tx(&mut self, tx: &mut Transaction) -> PyResult<PyTransactionExecutionInfo> {
+        let tx_info = self
+            .inner
+            .execute_tx(tx)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+        Ok(PyTransactionExecutionInfo::from(tx_info))
+    }
+    pub fn add_messages_and_events(&mut self, exec_info: &ExecutionInfo) -> PyResult<()> {
+        self.inner
+            .add_messages_and_events(exec_info)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+        Ok(())
+    }
+
+    pub fn consume_message_hash(&mut self, message_hash: Vec<u8>) -> PyResult<()> {
+        self.inner
+            .consume_message_hash(message_hash)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+        Ok(())
     }
 }
