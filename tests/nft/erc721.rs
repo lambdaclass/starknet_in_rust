8d15369fa31c8f8b0581dad4603ad24eda3c7826
matias-gonz <maigonzalez@fi.uba.ar>
2023-03-21 16:57:47 -0300

    Test erc721_calling_constructor_twice_should_fail_test

--- tests/nft/erc721.rs
+++ tests/nft/erc721.rs
@@ -1004,3 +1004,39 @@ fn erc721_safe_transfer_from_should_panic() {
     // The contract will fail because the receiver address is not a IERC721Receiver contract
     safe_transfer_from(&calldata, &mut call_config).unwrap();
 }
+
+#[test]
+fn erc721_calling_constructor_twice_should_fail_test() {
+    let address = Address(1111.into());
+    let class_hash = [1; 32];
+    let mut state = setup_contract("starknet_programs/ERC721.json", &address, class_hash);
+    let entry_points_by_type = state
+        .get_contract_class(&class_hash)
+        .unwrap()
+        .entry_points_by_type()
+        .clone();
+
+    let name = Felt::from_bytes_be("some-nft".as_bytes());
+    let symbol = Felt::from(555);
+    let to = Felt::from(666);
+    let calldata = [name, symbol, to].to_vec();
+    let caller_address = Address(666.into());
+    let general_config = StarknetGeneralConfig::default();
+    let mut resources_manager = ExecutionResourcesManager::default();
+    let entry_point_type = EntryPointType::Constructor;
+
+    let mut call_config = CallConfig {
+        state: &mut state,
+        caller_address: &caller_address,
+        address: &address,
+        class_hash: &class_hash,
+        entry_points_by_type: &entry_points_by_type,
+        entry_point_type: &entry_point_type,
+        general_config: &general_config,
+        resources_manager: &mut resources_manager,
+    };
+
+    contructor(&calldata, &mut call_config).unwrap();
+
+    assert!(contructor(&calldata, &mut call_config).is_err());
+}
