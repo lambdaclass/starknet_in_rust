4e3043cd2f4020dff363d6f9cf1acccfd54b6e18
Tomás <tomas.gruner@lambdaclass.com>
2023-03-21 12:39:35 -0300

    Add compile error when using incompatible features
    Also add a smoke test for when module is not working

--- crates/starknet-rs-py/src/lib.rs
+++ crates/starknet-rs-py/src/lib.rs
@@ -1,5 +1,9 @@
 #![deny(warnings)]
 
+mod cached_state;
+mod starknet_state;
+mod types;
+
 use self::{
     cached_state::PyCachedState,
     types::{
@@ -11,12 +15,11 @@ use self::{
 use pyo3::prelude::*;
 use types::general_config::{PyStarknetChainId, PyStarknetGeneralConfig, PyStarknetOsConfig};
 
-mod cached_state;
-mod starknet_state;
-mod types;
+#[cfg(all(feature = "extension-module", feature = "embedded-python"))]
+compile_error!("\"extension-module\" is incompatible with \"embedded-python\" as it inhibits linking with cpython");
 
 #[pymodule]
-pub fn starknet_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+pub fn starknet_rs_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyStarknetGeneralConfig>()?;
     m.add_class::<PyStarknetOsConfig>()?;
     m.add_class::<PyStarknetChainId>()?;
@@ -30,4 +33,17 @@ pub fn starknet_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyOrderedL2ToL1Message>()?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use pyo3::prelude::*;
+
+    #[test]
+    fn starknet_rs_py_test() {
+        Python::with_gil(|py| {
+            let module = PyModule::new(py, "My Module");
+            assert!(crate::starknet_rs_py(py, module.unwrap()).is_ok());
+        });
+    }
 }
