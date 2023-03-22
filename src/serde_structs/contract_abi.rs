b9d16b08d2aae39da2520dd869c4816f2b8e84d6
Martina <martina@martina>
2023-03-22 16:28:40 -0300

    add test for read_abi

--- src/serde_structs/contract_abi.rs
+++ src/serde_structs/contract_abi.rs
@@ -38,3 +38,24 @@ pub fn read_abi(abi_name: &PathBuf) -> HashMap<String, (usize, EntryPointType)> 
 
     result_hash_map
 }
+#[test]
+fn test_read_abi() {
+    let path_a = PathBuf::from(r"contract_execution_examples/abi.json");
+    // using the function to read an abi
+    let result = read_abi(&path_a);
+
+    // this is the expected result of the function above
+    let expected_result: HashMap<String, (usize, EntryPointType)> = HashMap::from([
+        (
+            String::from("increase_balance"),
+            (0 as usize, EntryPointType::External),
+        ),
+        (
+            String::from("get_balance"),
+            (1 as usize, EntryPointType::External),
+        ),
+    ]);
+
+    // final check
+    assert_eq!(result, expected_result)
+}
