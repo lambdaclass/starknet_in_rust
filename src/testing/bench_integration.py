1a0180e9c92e375703ddf2e1d546c2e66e75a3c4
SantiagoPittella <pittellasantiago@gmail.com>
2023-03-27 14:45:08 -0300

    change test to use first_contract.cairo

--- src/testing/bench_integration.py
+++ src/testing/bench_integration.py
@@ -6,8 +6,8 @@ import pytest_asyncio
 from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.testing.starknet import Starknet
 
-ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), "../../starknet_programs/account_without_validation.cairo")
-CONTRACT_FILE = os.path.join(os.path.dirname(__file__), "../../starknet_programs/fibonacci.cairo")
+# ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), "../../starknet_programs/account_without_validation.cairo")
+CONTRACT_FILE = os.path.join(os.path.dirname(__file__), "../../starknet_programs/first_contract.cairo")
 
 @pytest_asyncio.fixture
 async def starknet() -> Starknet:
@@ -17,13 +17,15 @@ async def starknet() -> Starknet:
 async def contract(starknet: Starknet) -> StarknetContract:
     return await starknet.deploy(source=CONTRACT_FILE)
 
-@pytest_asyncio.fixture
-async def account(starknet: Starknet) -> StarknetContract:
-    return await starknet.deploy(source=ACCOUNT_FILE)
+# @pytest_asyncio.fixture
+# async def account(starknet: Starknet) -> StarknetContract:
+#     return await starknet.deploy(source=ACCOUNT_FILE)
 
 @pytest.mark.asyncio
-async def test_basic(contract: StarknetContract, account: StarknetContract, starknet: Starknet):
-    print(starknet.__dir__)
+async def test_basic(contract: StarknetContract):
     # contract_address, selector: felt, calldata_len: felt, calldata: felt*
-    call_info = await account.__execute__(contract_address=contract.contract_address, selector=0x112e35f48499939272000bd72eb840e502ca4c3aefa8800992e8defb746e0c9, calldata=[1, 1, 10]).execute()
-    assert call_info.result == (144,)
+    # call_info = await account.__execute__(contract_address=contract.contract_address, selector=0x112e35f48499939272000bd72eb840e502ca4c3aefa8800992e8defb746e0c9, calldata=[1, 1, 10]).execute()
+    for i in range(1, 101):
+        await contract.increase_balance(amount=10).execute()
+        call_info = await contract.get_balance().execute()
+        assert call_info.result == (10 * i,)
