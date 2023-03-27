e021c05d42179ccfa785bbd236e64821e4659cf0
Roberto Catalan <rcatalan@itba.edu.ar>
2023-03-27 11:00:06 -0300

    Refactor swap test fail not enough funds

--- tests/amm_contracts/amm_deploy.rs
+++ tests/amm_contracts/amm_deploy.rs
@@ -346,3 +346,30 @@ fn amm_swap_should_fail_with_amount_out_of_bounds() {
 
     assert!(swap(&calldata, &mut call_config).is_err());
 }
+
+#[test]
+fn amm_swap_should_fail_when_user_does_not_have_enough_funds() {
+    let general_config = StarknetGeneralConfig::default();
+    let mut state = CachedState::new(InMemoryStateReader::default(), Some(Default::default()));
+    // Deploy contract
+    let (contract_address, class_hash) =
+        deploy(&mut state, "starknet_programs/amm.json", &general_config);
+
+    let calldata = [Felt::new(1), Felt::new(100)].to_vec();
+    let caller_address = Address(0000.into());
+    let general_config = StarknetGeneralConfig::default();
+    let mut resources_manager = ExecutionResourcesManager::default();
+    let mut call_config = CallConfig {
+        state: &mut state,
+        caller_address: &caller_address,
+        address: &contract_address,
+        class_hash: &class_hash,
+        general_config: &general_config,
+        resources_manager: &mut resources_manager,
+    };
+
+    init_pool(&[Felt::new(1000), Felt::new(1000)], &mut call_config).unwrap();
+    add_demo_token(&[Felt::new(10), Felt::new(10)], &mut call_config).unwrap();
+
+    assert!(swap(&calldata, &mut call_config).is_err());
+}
