3a1c0574c9d38414e85c2a501219689a979a0abe
Tomás <tomas.gruner@lambdaclass.com>
2023-03-27 16:32:14 -0300

    Fix: change Felt -> Felt252

--- crates/starknet-rs-py/src/utils/mod.rs
+++ crates/starknet-rs-py/src/utils/mod.rs
@@ -1,6 +1,6 @@
 use crate::types::general_config::PyStarknetGeneralConfig;
 use crate::{cached_state::PyCachedState, types::contract_class::PyContractClass};
-use cairo_felt::Felt;
+use cairo_felt::Felt252;
 use num_bigint::BigUint;
 use pyo3::{exceptions::PyValueError, pyfunction, PyResult};
 use starknet_rs::business_logic::fact_state::in_memory_state_reader::InMemoryStateReader;
@@ -48,7 +48,7 @@ pub(crate) fn py_validate_contract_deployed(
     contract_address: BigUint,
 ) -> PyResult<[u8; 32]> {
     let s: &mut CachedState<InMemoryStateReader> = state.into();
-    match validate_contract_deployed(s, &Address(Felt::from(contract_address))) {
+    match validate_contract_deployed(s, &Address(Felt252::from(contract_address))) {
         Ok(res) => Ok(res),
         Err(err) => Err(PyValueError::new_err(err.to_string())),
     }
@@ -63,7 +63,7 @@ pub(crate) fn py_calculate_event_hash(
 ) -> PyResult<BigUint> {
     let felt_keys = keys.into_iter().map(Into::into).collect();
     let felt_data = data.into_iter().map(Into::into).collect();
-    match calculate_event_hash(Felt::from(from_address), felt_keys, felt_data) {
+    match calculate_event_hash(Felt252::from(from_address), felt_keys, felt_data) {
         Ok(res) => Ok(res.to_biguint()),
         Err(err) => Err(PyValueError::new_err(err.to_string())),
     }
@@ -77,10 +77,10 @@ pub(crate) fn py_calculate_contract_address(
     constructor_calldata: Vec<BigUint>,
     deployer_address: BigUint,
 ) -> PyResult<BigUint> {
-    let salt = Address(Felt::from(salt));
-    let class_hash = Felt::from(py_compute_class_hash(contract_class)?);
+    let salt = Address(Felt252::from(salt));
+    let class_hash = Felt252::from(py_compute_class_hash(contract_class)?);
     let constructor_calldata: Vec<_> = constructor_calldata.into_iter().map(Into::into).collect();
-    let deployer_address = Address(Felt::from(deployer_address));
+    let deployer_address = Address(Felt252::from(deployer_address));
     match calculate_contract_address(&salt, &class_hash, &constructor_calldata, deployer_address) {
         Ok(res) => Ok(res.to_biguint()),
         Err(err) => Err(PyValueError::new_err(err.to_string())),
@@ -95,10 +95,10 @@ pub(crate) fn py_calculate_contract_address_from_hash(
     constructor_calldata: Vec<BigUint>,
     deployer_address: BigUint,
 ) -> PyResult<BigUint> {
-    let salt = Address(Felt::from(salt));
-    let class_hash = Felt::from(class_hash);
+    let salt = Address(Felt252::from(salt));
+    let class_hash = Felt252::from(class_hash);
     let constructor_calldata: Vec<_> = constructor_calldata.into_iter().map(Into::into).collect();
-    let deployer_address = Address(Felt::from(deployer_address));
+    let deployer_address = Address(Felt252::from(deployer_address));
     match calculate_contract_address(&salt, &class_hash, &constructor_calldata, deployer_address) {
         Ok(res) => Ok(res.to_biguint()),
         Err(err) => Err(PyValueError::new_err(err.to_string())),
@@ -113,9 +113,9 @@ pub(crate) fn py_calculate_deploy_transaction_hash(
     constructor_calldata: Vec<BigUint>,
     chain_id: BigUint,
 ) -> PyResult<BigUint> {
-    let contract_address = Address(Felt::from(contract_address));
+    let contract_address = Address(Felt252::from(contract_address));
     let constructor_calldata: Vec<_> = constructor_calldata.into_iter().map(Into::into).collect();
-    let chain_id = Felt::from(chain_id);
+    let chain_id = Felt252::from(chain_id);
     match calculate_deploy_transaction_hash(
         version,
         &contract_address,
@@ -137,9 +137,9 @@ pub(crate) fn py_calculate_declare_transaction_hash(
     version: u64,
     nonce: BigUint,
 ) -> PyResult<BigUint> {
-    let chain_id = Felt::from(chain_id);
-    let sender_address = Address(Felt::from(sender_address));
-    let nonce = Felt::from(nonce);
+    let chain_id = Felt252::from(chain_id);
+    let sender_address = Address(Felt252::from(sender_address));
+    let nonce = Felt252::from(nonce);
     match calculate_declare_transaction_hash(
         contract_class.into(),
         chain_id,
