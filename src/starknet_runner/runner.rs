a4fca8497d4dcfdb696950e00a3c5785d23d1642
Milton <milton.scuderi@lambdaclass.com>
2023-03-28 16:12:36 -0300

    Change layout

--- src/starknet_runner/runner.rs
+++ src/starknet_runner/runner.rs
@@ -244,7 +244,7 @@ mod test {
     #[test]
     fn prepare_os_context_test() {
         let program = cairo_rs::types::program::Program::default();
-        let cairo_runner = CairoRunner::new(&program, "all", false).unwrap();
+        let cairo_runner = CairoRunner::new(&program, "all_cairo", false).unwrap();
         let vm = VirtualMachine::new(true);
 
         let mut state = CachedState::<InMemoryStateReader>::default();
