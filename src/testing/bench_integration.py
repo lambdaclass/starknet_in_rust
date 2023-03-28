89ff0f3fd1d0c4e6ba7deddba13fba3060b1541d
SantiagoPittella <pittellasantiago@gmail.com>
2023-03-28 09:50:10 -0300

    update names

--- src/testing/bench_integration.py
+++ src/testing/bench_integration.py
@@ -6,7 +6,6 @@ import pytest_asyncio
 from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.testing.starknet import Starknet
 
-# ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), "../../starknet_programs/account_without_validation.cairo")
 CONTRACT_FILE = os.path.join(os.path.dirname(__file__), "test.cairo")
 
 @pytest_asyncio.fixture
@@ -17,13 +16,7 @@ async def starknet() -> Starknet:
 async def contract(starknet: Starknet) -> StarknetContract:
     return await starknet.deploy(source=CONTRACT_FILE)
 
-# @pytest_asyncio.fixture
-# async def account(starknet: Starknet) -> StarknetContract:
-#     return await starknet.deploy(source=ACCOUNT_FILE)
-
 @pytest.mark.asyncio
-async def test_invoke1000(contract: StarknetContract):
-    # contract_address, selector: felt, calldata_len: felt, calldata: felt*
-    # call_info = await account.__execute__(contract_address=contract.contract_address, selector=0x112e35f48499939272000bd72eb840e502ca4c3aefa8800992e8defb746e0c9, calldata=[1, 1, 10]).execute()
+async def test_invoke(contract: StarknetContract):
     for i in range(0, 1):
         await contract.fib(first_element=1, second_element=1, n=15000).execute()
--- src/testing/run_bench.sh
+++ src/testing/run_bench.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 hyperfine \
-	    -n "cairo-lang 1k" "pytest bench_integration.py::test_invoke1000" \
-	    -n "starknet_in_rust 1k" "cargo test testing::bench_integration::test_invoke --release"
+	    -n "cairo-lang fib 15k" "pytest bench_integration.py::test_invoke" \
+	    -n "starknet_in_rust fib 15k" "cargo test testing::bench_integration::test_invoke --release"
