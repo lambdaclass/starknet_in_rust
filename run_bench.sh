59605094d23b96477a094753243067ad265fbaef
SantiagoPittella <pittellasantiago@gmail.com>
2023-03-31 13:59:37 -0300

    add bench scripts

---
+++ run_bench.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+hyperfine -w 3 -r 5 \
+	    -n "cairo-lang (CPython) read/write storage 10k" "pytest bench_integration.py::test_invoke" \
+	    -n "starknet_in_rust read/write storage 10k" "cargo run --bin bench_bin --release"
---
+++ run_bench_deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+hyperfine -w 3 -r 5 \
+	    -n "cairo-lang (CPython) deploy 10k" "pytest bench_integration.py::test_deploy" \
+	    -n "starknet_in_rust deploy 10k" "cargo run --bin bench_deploy --release"
