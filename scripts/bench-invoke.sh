043c08d68d9998214c30704bca0b4480c81df528
SantiagoPittella <pittellasantiago@gmail.com>
2023-04-03 16:51:12 -0300

    add -N flag to hyperfine

--- scripts/bench-invoke.sh
+++ scripts/bench-invoke.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-hyperfine -w 3 -r 5 \
+hyperfine -N -w 3 -r 5 \
 	    -n "cairo-lang (CPython) read/write storage 10k" "pytest bench_integration.py::test_invoke" \
 	    -n "starknet_in_rust read/write storage 10k without deploy" "./target/release/invoke"
