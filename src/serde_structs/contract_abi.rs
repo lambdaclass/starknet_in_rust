9e2b7ed9d93af72e2c27a1827676d56790a07fda
Roberto Catalan <rcatalan@itba.edu.ar>
2023-04-04 16:10:38 -0300

    Add warning msg regarding missing types for ABI

--- src/serde_structs/contract_abi.rs
+++ src/serde_structs/contract_abi.rs
@@ -15,6 +15,9 @@ struct Signature {
     type_name: String,
 }
 
+// We should should consider reading all the information from the abi in the future. Right now we are not considering:
+// - type: "event"
+// - type: "struct"
 pub fn read_abi(abi_name: &PathBuf) -> HashMap<String, (usize, EntryPointType)> {
     let abi: Vec<Signature> = serde_json::from_reader(&File::open(abi_name).unwrap()).unwrap();
     let mut func_type_counter: HashMap<String, usize> = HashMap::new();
