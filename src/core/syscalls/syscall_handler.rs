42c59d4b5b03976478abbafab2b6b2bbb7bad976
Roberto Catalan <rcatalan@itba.edu.ar>
2023-04-12 17:08:47 -0300

    Add new syscall to SyscallHandler

--- src/core/syscalls/syscall_handler.rs
+++ src/core/syscalls/syscall_handler.rs
@@ -34,7 +34,34 @@ use cairo_rs::{
 use felt::Felt252;
 use std::{any::Any, collections::HashMap};
 
-pub trait SyscallHandler {}
+pub(crate) trait SyscallHandler {
+    fn send_message_to_l1(
+        &mut self,
+        vm: &VirtualMachine,
+        syscall_ptr: Relocatable,
+    ) -> Result<(), SyscallHandlerError>;
+
+    fn read_and_validate_syscall_request(
+        &mut self,
+        syscall_name: &str,
+        vm: &VirtualMachine,
+        syscall_ptr: Relocatable,
+    ) -> Result<SyscallRequest, SyscallHandlerError>;
+
+    fn read_syscall_request(
+        &self,
+        syscall_name: &str,
+        vm: &VirtualMachine,
+        syscall_ptr: Relocatable,
+    ) -> Result<SyscallRequest, SyscallHandlerError> {
+        match syscall_name {
+            "send_message_to_l1" => SendMessageToL1SysCall::from_ptr(vm, syscall_ptr),
+            _ => Err(SyscallHandlerError::UnknownSyscall(
+                syscall_name.to_string(),
+            )),
+        }
+    }
+}
 
 //* ---------------------
 //* DeprecatedSyscallHandler Trait
