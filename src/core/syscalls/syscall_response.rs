fc8e742531a90609ad0c6e179fbf255284a7e87f
Roberto Catalan <rcatalan@itba.edu.ar>
2023-04-13 15:47:30 -0300

    Refactor for optionality in field ResponseBody

--- src/core/syscalls/syscall_response.rs
+++ src/core/syscalls/syscall_response.rs
@@ -5,5 +5,5 @@ pub struct SyscallResponse {
     /// The amount of gas left after the syscall execution.
     gas: u64,
     /// Syscall specific response fields.
-    body: ResponseBody,
+    body: Option<ResponseBody>,
 }
