b8fc0a176c0f4483802a3d03c623bd06df04a7bc
Tomás <tomas.gruner@lambdaclass.com>
2023-04-13 10:36:16 -0300

    Add SyscallResponse common fields

--- src/core/syscalls/syscall_response.rs
+++ src/core/syscalls/syscall_response.rs
@@ -1,7 +1,11 @@
-#[allow(unused)]
-pub(crate) enum ResponseBody {}
+pub enum ResponseBody {}
 
 #[allow(unused)]
-pub(crate) struct SyscallResponse {
+pub struct SyscallResponse {
+    /// The amount of gas left after the syscall execution.
+    gas: u64,
+    /// If the syscall succeeded.
+    failure_flag: bool,
+    /// Syscall specific response fields.
     syscall_response: ResponseBody,
 }
