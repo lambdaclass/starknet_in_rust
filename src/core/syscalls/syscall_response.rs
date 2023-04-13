9afcba156cd9afc35478de39b4b27c2bb84db1d7
Roberto Catalan <rcatalan@itba.edu.ar>
2023-04-13 15:49:35 -0300

    Change visibility level to crate

--- src/core/syscalls/syscall_response.rs
+++ src/core/syscalls/syscall_response.rs
@@ -1,7 +1,7 @@
-pub enum ResponseBody {}
+pub(crate) enum ResponseBody {}
 
 #[allow(unused)]
-pub struct SyscallResponse {
+pub(crate) struct SyscallResponse {
     /// The amount of gas left after the syscall execution.
     gas: u64,
     /// Syscall specific response fields.
