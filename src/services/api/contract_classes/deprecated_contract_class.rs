2944daf4d3f4c8035bce152e328d4e64f601d431
Esteban Dimitroff Hódi <esteban.dimitroff@entropy1729.com>
2023-04-19 12:15:19 -0300

    Made to_cairo_runner_program function public

--- src/services/api/contract_classes/deprecated_contract_class.rs
+++ src/services/api/contract_classes/deprecated_contract_class.rs
@@ -183,7 +183,7 @@ fn convert_entry_points(
     converted_entries
 }
 
-fn to_cairo_runner_program(
+pub fn to_cairo_runner_program(
     program: &starknet_api::deprecated_contract_class::Program,
 ) -> Result<Program, ProgramError> {
     let program = program.clone();
