bcba3bea6b923bc07dbf77fbe3c7b85a5131c061
matias-gonz <maigonzalez@fi.uba.ar>
2023-04-24 18:29:45 -0300

    Add Cairo1HintProcessor.test_less_than

--- crates/cairo-1-hint-processor/src/hint_processor.rs
+++ crates/cairo-1-hint-processor/src/hint_processor.rs
@@ -1,12 +1,15 @@
 use cairo_lang_casm::{
     hints::Hint,
-    operand::{CellRef, Register},
+    operand::{CellRef, DerefOrImmediate, Operation, Register, ResOperand},
 };
 use cairo_rs::{
     hint_processor::hint_processor_definition::HintProcessor,
     types::exec_scope::ExecutionScopes,
-    types::relocatable::Relocatable,
-    vm::{errors::hint_errors::HintError, vm_core::VirtualMachine},
+    types::relocatable::{MaybeRelocatable, Relocatable},
+    vm::{
+        errors::{hint_errors::HintError, vm_errors::VirtualMachineError},
+        vm_core::VirtualMachine,
+    },
 };
 use felt::Felt252;
 use std::collections::HashMap;
@@ -22,11 +25,78 @@ fn cell_ref_to_relocatable(cell_ref: &CellRef, vm: &VirtualMachine) -> Relocatab
     (base + (cell_ref.offset as i32)).unwrap()
 }
 
+fn get_cell_val(vm: &VirtualMachine, cell: &CellRef) -> Result<Felt252, VirtualMachineError> {
+    Ok(vm
+        .get_integer(cell_ref_to_relocatable(cell, vm))?
+        .as_ref()
+        .clone())
+}
+
+fn get_ptr(
+    vm: &VirtualMachine,
+    cell: &CellRef,
+    offset: &Felt252,
+) -> Result<Relocatable, VirtualMachineError> {
+    Ok((vm.get_relocatable(cell_ref_to_relocatable(cell, vm))? + offset)?)
+}
+
+fn get_double_deref_val(
+    vm: &VirtualMachine,
+    cell: &CellRef,
+    offset: &Felt252,
+) -> Result<Felt252, VirtualMachineError> {
+    Ok(vm.get_integer(get_ptr(vm, cell, offset)?)?.as_ref().clone())
+}
+
+fn res_operand_get_val(
+    vm: &VirtualMachine,
+    res_operand: &ResOperand,
+) -> Result<Felt252, VirtualMachineError> {
+    match res_operand {
+        ResOperand::Deref(cell) => get_cell_val(vm, cell),
+        ResOperand::DoubleDeref(cell, offset) => get_double_deref_val(vm, cell, &(*offset).into()),
+        ResOperand::Immediate(x) => Ok(Felt252::from(x.value.clone())),
+        ResOperand::BinOp(op) => {
+            let a = get_cell_val(vm, &op.a)?;
+            let b = match &op.b {
+                DerefOrImmediate::Deref(cell) => get_cell_val(vm, cell)?,
+                DerefOrImmediate::Immediate(x) => Felt252::from(x.value.clone()),
+            };
+            match op.op {
+                Operation::Add => Ok(a + b),
+                Operation::Mul => Ok(a * b),
+            }
+        }
+    }
+}
+
 impl Cairo1HintProcessor {
     fn alloc_segment(&mut self, vm: &mut VirtualMachine, dst: &CellRef) -> Result<(), HintError> {
         let segment = vm.add_memory_segment();
         vm.insert_value(cell_ref_to_relocatable(dst, vm), segment)
             .map_err(HintError::from)
+    }
+
+    fn test_less_than(
+        &self,
+        vm: &mut VirtualMachine,
+        lhs: &ResOperand,
+        rhs: &ResOperand,
+        dst: &CellRef,
+    ) -> Result<(), HintError> {
+        let lhs_value = res_operand_get_val(vm, lhs)?;
+        let rhs_value = res_operand_get_val(vm, rhs)?;
+        let result = if lhs_value < rhs_value {
+            Felt252::from(1)
+        } else {
+            Felt252::from(0)
+        };
+
+        vm.insert_value(
+            cell_ref_to_relocatable(dst, vm),
+            MaybeRelocatable::from(result),
+        )
+        .map_err(HintError::from)
     }
 }
 
@@ -47,6 +117,7 @@ impl HintProcessor for Cairo1HintProcessor {
         let hint = hint_data.downcast_ref::<Hint>().unwrap();
         match hint {
             Hint::AllocSegment { dst } => self.alloc_segment(vm, dst),
+            Hint::TestLessThan { lhs, rhs, dst } => self.test_less_than(vm, lhs, rhs, dst),
             _ => todo!(),
         }
     }
