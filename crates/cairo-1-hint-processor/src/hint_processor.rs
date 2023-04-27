d90180cf6d0309658c8a1a18df4cc02d4749f6b4
Milton <milton.scuderi@lambdaclass.com>
2023-04-27 14:47:29 -0300

    GetCurrentAccessIndex

--- crates/cairo-1-hint-processor/src/hint_processor.rs
+++ crates/cairo-1-hint-processor/src/hint_processor.rs
@@ -1,3 +1,5 @@
+use crate::dict_manager::DictSquashExecScope;
+
 use super::dict_manager::DictManagerExecScope;
 use cairo_lang_casm::{
     hints::{CoreHint, Hint},
@@ -442,6 +444,21 @@ impl Cairo1HintProcessor {
         println!();
         Ok(())
     }
+
+    fn get_current_access_index(
+        &self,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        range_check_ptr: &ResOperand,
+    ) -> Result<(), HintError> {
+        let dict_squash_exec_scope: &mut DictSquashExecScope =
+            exec_scopes.get_mut_ref("dict_squash_exec_scope")?;
+        let (range_check_base, range_check_offset) = extract_buffer(range_check_ptr)?;
+        let range_check_ptr = get_ptr(vm, range_check_base, &range_check_offset)?;
+        let current_access_index = dict_squash_exec_scope.current_access_index().unwrap();
+        vm.insert_value(range_check_ptr, current_access_index)?;
+        Ok(())
+    }
 }
 
 impl HintProcessor for Cairo1HintProcessor {
@@ -538,6 +555,11 @@ impl HintProcessor for Cairo1HintProcessor {
                 a,
                 b,
             }) => self.assert_le_find_small_arcs(vm, exec_scopes, range_check_ptr, a, b),
+
+            Hint::Core(CoreHint::GetCurrentAccessIndex { range_check_ptr }) => {
+                self.get_current_access_index(vm, exec_scopes, range_check_ptr)
+            }
+
             _ => todo!(),
         }
     }
