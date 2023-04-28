b14e038564ef65ad74289eaea6356144a8818f50
mmsc2 <88055861+mmsc2@users.noreply.github.com>
2023-04-28 15:43:03 -0300

    Init squash data (#446)
    * Init squash data
    
    * change expect for hint error

--- crates/cairo-1-hint-processor/src/hint_processor.rs
+++ crates/cairo-1-hint-processor/src/hint_processor.rs
@@ -1,5 +1,6 @@
-use super::dict_manager::DictManagerExecScope;
 use crate::dict_manager::DictSquashExecScope;
+
+use super::dict_manager::DictManagerExecScope;
 use ark_ff::fields::{Fp256, MontBackend, MontConfig};
 use ark_ff::{Field, PrimeField};
 use ark_std::UniformRand;
@@ -623,6 +624,72 @@ impl Cairo1HintProcessor {
         Ok(())
     }
 
+    fn init_squash_data(
+        &self,
+        vm: &mut VirtualMachine,
+        exec_scopes: &mut ExecutionScopes,
+        dict_accesses: &ResOperand,
+        n_accesses: &ResOperand,
+        big_keys: &CellRef,
+    ) -> Result<(), HintError> {
+        let dict_access_size = 3;
+        let rangecheck_bound = Felt252::from(u128::MAX) + 1u32;
+
+        exec_scopes.assign_or_update_variable(
+            "dict_squash_exec_scope",
+            Box::<DictSquashExecScope>::default(),
+        );
+        let dict_squash_exec_scope =
+            exec_scopes.get_mut_ref::<DictSquashExecScope>("dict_squash_exec_scope")?;
+        let (dict_accesses_base, dict_accesses_offset) = extract_buffer(dict_accesses)?;
+        let dict_accesses_address = get_ptr(vm, dict_accesses_base, &dict_accesses_offset)?;
+        let n_accesses =
+            res_operand_get_val(vm, n_accesses)?
+                .to_usize()
+                .ok_or(HintError::CustomHint(
+                    "Number of accesses is too large or negative.".to_string(),
+                ))?;
+
+        for i in 0..n_accesses {
+            let current_key = vm.get_integer((dict_accesses_address + i * dict_access_size)?)?;
+            dict_squash_exec_scope
+                .access_indices
+                .entry(current_key.into_owned())
+                .and_modify(|indices| indices.push(Felt252::from(i)))
+                .or_insert_with(|| vec![Felt252::from(i)]);
+        }
+        // Reverse the accesses in order to pop them in order later.
+        for (_, accesses) in dict_squash_exec_scope.access_indices.iter_mut() {
+            accesses.reverse();
+        }
+
+        dict_squash_exec_scope.keys = dict_squash_exec_scope
+            .access_indices
+            .keys()
+            .cloned()
+            .collect();
+        dict_squash_exec_scope.keys.sort_by(|a, b| b.cmp(a));
+        // big_keys indicates if the keys are greater than rangecheck_bound. If they are not
+        // a simple range check is used instead of assert_le_felt252.
+
+        let val = if dict_squash_exec_scope.keys[0] < rangecheck_bound {
+            Felt252::from(0)
+        } else {
+            Felt252::from(1)
+        };
+
+        vm.insert_value(cell_ref_to_relocatable(big_keys, vm), val)?;
+
+        vm.insert_value(
+            cell_ref_to_relocatable(big_keys, vm),
+            dict_squash_exec_scope
+                .current_key()
+                .ok_or(HintError::CustomHint("No current key".to_string()))?,
+        )?;
+
+        Ok(())
+    }
+
     fn get_current_access_delta(
         &self,
         vm: &mut VirtualMachine,
@@ -696,7 +763,6 @@ impl Cairo1HintProcessor {
         } else {
             Felt252::from(1)
         };
-
         vm.insert_value(cell_ref_to_relocatable(should_skip_loop, vm), val)?;
 
         Ok(())
@@ -831,10 +897,17 @@ impl HintProcessor for Cairo1HintProcessor {
                 b,
             }) => self.assert_le_find_small_arcs(vm, exec_scopes, range_check_ptr, a, b),
 
+            Hint::Core(CoreHint::InitSquashData {
+                dict_accesses,
+                n_accesses,
+                big_keys,
+                ..
+            }) => self.init_squash_data(vm, exec_scopes, dict_accesses, n_accesses, big_keys),
 
             Hint::Core(CoreHint::Felt252DictEntryInit { dict_ptr, key }) => {
                 self.dict_entry_init(vm, exec_scopes, dict_ptr, key)
             }
+
             Hint::Core(CoreHint::GetCurrentAccessDelta { index_delta_minus1 }) => {
                 self.get_current_access_delta(vm, exec_scopes, index_delta_minus1)
             }
