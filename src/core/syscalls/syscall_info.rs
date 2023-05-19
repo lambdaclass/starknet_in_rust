eb4a7bc9099da595374d5dafe390ac567cc577cc
Federica <fedemoletta@hotmail.com>
2023-05-19 17:23:11 -0300

    Add size for library_call syscall

--- src/core/syscalls/syscall_info.rs
+++ src/core/syscalls/syscall_info.rs
@@ -2,6 +2,7 @@ pub fn get_syscall_size_from_name(syscall_name: &str) -> usize {
     match syscall_name {
         "emit_event" => 4,
         "deploy" => 9,
+        "library_call" => 5,
         _ => unreachable!(),
     }
 }
