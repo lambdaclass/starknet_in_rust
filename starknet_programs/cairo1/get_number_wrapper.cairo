a04a43c138ef2fb9b2d9cd2c9620b3d02cbb15ed
Federica <fedemoletta@hotmail.com>
2023-05-31 17:50:35 -0300

    Add integration test withh call_contract

---
+++ starknet_programs/cairo1/get_number_wrapper.cairo
@@ -0,0 +1,37 @@
+use starknet::ClassHash;
+
+#[abi]
+trait GetNumber {
+
+    #[view]
+    fn get_number() -> felt252;
+
+    #[external]
+    fn upgrade(new_class_hash: ClassHash);
+}
+
+#[contract]
+mod GetNumberWrapper {
+    use super::GetNumberDispatcherTrait; 
+    use super::GetNumberDispatcher;
+    use starknet::ClassHash;
+    use starknet::ContractAddress;
+    use starknet::contract_address_try_from_felt252;
+    use option::OptionTrait; 
+
+    // We use a constant for the contract_address to test the replace_class functionality
+    const get_number_contract_address: felt252 = 1;
+
+    #[view]
+    fn get_number() -> felt252 {
+        let address = contract_address_try_from_felt252(get_number_contract_address).unwrap();
+        GetNumberDispatcher {contract_address: address}.get_number()
+    }
+
+    #[external]
+    fn upgrade(new_class_hash: ClassHash) {
+        let address = contract_address_try_from_felt252(get_number_contract_address).unwrap();
+        GetNumberDispatcher {contract_address: address}.upgrade(new_class_hash);
+    }
+}
+      
--- tests/cairo_1_syscalls.rs
+++ tests/cairo_1_syscalls.rs
@@ -9,7 +9,7 @@ use cairo_vm::{
     vm::runners::{builtin_runner::RANGE_CHECK_BUILTIN_NAME, cairo_runner::ExecutionResources},
 };
 use num_bigint::BigUint;
-use num_traits::{Num, Zero};
+use num_traits::{Num, One, Zero};
 use starknet_rs::{
     business_logic::{
         execution::{
@@ -574,7 +574,7 @@ fn emit_event() {
 
 #[test]
 fn replace_class_internal() {
-    // This test only checks that the contract is updated in the storage, see `replace_class_library_call`
+    // This test only checks that the contract is updated in the storage, see `replace_class_contract_call`
     //  Create program and entry point types for contract class
     let program_data_a = include_bytes!("../starknet_programs/cairo1/get_number_a.casm");
     let contract_class_a: CasmContractClass = serde_json::from_slice(program_data_a).unwrap();
@@ -655,4 +655,161 @@ fn replace_class_internal() {
         state.get_compiled_class(&class_hash_b).unwrap(),
         CompiledClass::Casm(Box::new(contract_class_b))
     );
+}
+
+#[test]
+fn replace_class_contract_call() {
+    /* Test Outline:
+       - Add `get_number_a.cairo` contract at address 2 and `get_number_b.cairo` contract without an address
+       - Call `get_number` function of `get_number_wrapper.cairo` and expect to get an answer from `get_number_a` (25)
+       - Call `upgrade` function of `get_number_wrapper.cairo` with `get_number_b.cairo`'s class_hash
+       - Call `get_number` function of `get_number_wrapper.cairo` and expect to get an answer from `get_number_b` (17)
+    */
+
+    // SET GET_NUMBER_A
+    // Add get_number_a.cairo to storage
+    let program_data = include_bytes!("../starknet_programs/cairo1/get_number_a.casm");
+    let contract_class_a: CasmContractClass = serde_json::from_slice(program_data).unwrap();
+
+    // Create state reader with class hash data
+    let mut contract_class_cache = HashMap::new();
+
+    let address = Address(Felt252::one());
+    let class_hash_a: ClassHash = [1; 32];
+    let nonce = Felt252::zero();
+
+    contract_class_cache.insert(class_hash_a, contract_class_a);
+    let mut state_reader = InMemoryStateReader::default();
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(address.clone(), class_hash_a);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(address.clone(), nonce.clone());
+
+    // SET GET_NUMBER_B
+
+    // Add get_number_b contract to the state (only its contract_class)
+
+    let program_data = include_bytes!("../starknet_programs/cairo1/get_number_b.casm");
+    let contract_class_b: CasmContractClass = serde_json::from_slice(program_data).unwrap();
+
+    let class_hash_b: ClassHash = [2; 32];
+
+    contract_class_cache.insert(class_hash_b, contract_class_b.clone());
+
+    // SET GET_NUMBER_WRAPPER
+
+    //  Create program and entry point types for contract class
+    let program_data = include_bytes!("../starknet_programs/cairo1/get_number_wrapper.casm");
+    let wrapper_contract_class: CasmContractClass = serde_json::from_slice(program_data).unwrap();
+    let entrypoints = wrapper_contract_class.clone().entry_points_by_type;
+    let get_number_entrypoint_selector = &entrypoints.external.get(1).unwrap().selector;
+    let upgrade_entrypoint_selector = &entrypoints.external.get(0).unwrap().selector;
+
+    let wrapper_address = Address(Felt252::from(2));
+    let wrapper_class_hash: ClassHash = [3; 32];
+
+    contract_class_cache.insert(wrapper_class_hash, wrapper_contract_class);
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(wrapper_address.clone(), wrapper_class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(wrapper_address.clone(), nonce);
+
+    // Create state from the state_reader and contract cache.
+    let mut state = CachedState::new(state_reader, None, Some(contract_class_cache));
+
+    // INITIALIZE STARKNET CONFIG
+    let general_config = StarknetGeneralConfig::default();
+    let tx_execution_context = TransactionExecutionContext::new(
+        Address(0.into()),
+        Felt252::zero(),
+        Vec::new(),
+        0,
+        10.into(),
+        general_config.invoke_tx_max_n_steps(),
+        TRANSACTION_VERSION,
+    );
+    let mut resources_manager = ExecutionResourcesManager::default();
+
+    // CALL GET_NUMBER BEFORE REPLACE_CLASS
+
+    let calldata = [].to_vec();
+    let caller_address = Address(0000.into());
+    let entry_point_type = EntryPointType::External;
+
+    let exec_entry_point = ExecutionEntryPoint::new(
+        address.clone(),
+        calldata.clone(),
+        Felt252::new(get_number_entrypoint_selector.clone()),
+        caller_address.clone(),
+        entry_point_type,
+        Some(CallType::Delegate),
+        Some(wrapper_class_hash),
+        100000,
+    );
+
+    let result = exec_entry_point
+        .execute(
+            &mut state,
+            &general_config,
+            &mut resources_manager,
+            &tx_execution_context,
+            false,
+        )
+        .unwrap();
+    assert_eq!(result.retdata, vec![25.into()]);
+
+    // REPLACE_CLASS
+
+    let calldata = [Felt252::from_bytes_be(&class_hash_b)].to_vec();
+
+    let exec_entry_point = ExecutionEntryPoint::new(
+        address.clone(),
+        calldata.clone(),
+        Felt252::new(upgrade_entrypoint_selector.clone()),
+        caller_address.clone(),
+        entry_point_type,
+        Some(CallType::Delegate),
+        Some(wrapper_class_hash),
+        100000,
+    );
+
+    exec_entry_point
+        .execute(
+            &mut state,
+            &general_config,
+            &mut resources_manager,
+            &tx_execution_context,
+            false,
+        )
+        .unwrap();
+
+    // CALL GET_NUMBER AFTER REPLACE_CLASS
+
+    let calldata = [].to_vec();
+
+    let exec_entry_point = ExecutionEntryPoint::new(
+        address.clone(),
+        calldata.clone(),
+        Felt252::new(get_number_entrypoint_selector.clone()),
+        caller_address.clone(),
+        entry_point_type,
+        Some(CallType::Delegate),
+        Some(wrapper_class_hash),
+        100000,
+    );
+
+    let result = exec_entry_point
+        .execute(
+            &mut state,
+            &general_config,
+            &mut resources_manager,
+            &tx_execution_context,
+            false,
+        )
+        .unwrap();
+    assert_eq!(result.retdata, vec![17.into()]);
 }
