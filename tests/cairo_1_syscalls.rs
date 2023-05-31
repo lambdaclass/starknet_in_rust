636e836d4170623671f56b44d40813dbe9c35d8f
Federica <fedemoletta@hotmail.com>
2023-05-31 10:41:21 -0300

    fmt

--- tests/cairo_1_syscalls.rs
+++ tests/cairo_1_syscalls.rs
@@ -498,6 +498,7 @@ fn emit_event() {
 
     // Create state from the state_reader and contract cache.
     let mut state = CachedState::new(state_reader, None, Some(contract_class_cache));
+
     // Create an execution entry point
     let calldata = [].to_vec();
     let caller_address = Address(0000.into());
