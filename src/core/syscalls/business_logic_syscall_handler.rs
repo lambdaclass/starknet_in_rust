c24cf7c7840525bc754de06f8122a469d8850eb6
Santiago Pittella <87827390+SantiagoPittella@users.noreply.github.com>
2023-06-05 18:48:45 -0300

    Refactor modules state api objects (#537)
    * Remove objets.rs and add everything in mod.rs
    
    * Remove state_api_objets.rs and add everything in mod.rs
    
    * import only used structs
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: Juan Bono <juanbono94@gmail.com>

--- src/core/syscalls/business_logic_syscall_handler.rs
+++ src/core/syscalls/business_logic_syscall_handler.rs
@@ -18,7 +18,7 @@ use super::{
     },
     syscall_response::{CallContractResponse, FailureReason, ResponseBody},
 };
-use crate::business_logic::state::state_api_objects::BlockInfo;
+use crate::business_logic::state::BlockInfo;
 use crate::business_logic::transaction::error::TransactionError;
 use crate::utils::calculate_sn_keccak;
 use crate::{
