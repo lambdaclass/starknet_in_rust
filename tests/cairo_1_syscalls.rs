335f3afc0da171f57a3dfad5b0c41734b8e93620
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-06-08 11:43:47 -0300

    Test get execution info (#575)
    * Add SyscallRequest::GetExecutionInfo
    
    * Add get_execution_info
    
    * Add get_execution_info.cairo
    
    * Fix merge
    
    * Update get_execution_info.cairo
    
    * Add test_get_execution_info
    
    * Add get_execution_info to read_syscall_request and get_syscall_size_from_name
    
    * Fix get_execution_info_syscall size
    
    * Fix pointers in get_execution_info
    
    * Add TODO get_selector
    
    * Test test_get_execution_info
    
    * Test signature
    
    ---------
    
    Co-authored-by: Juan Bono <juanbono94@gmail.com>

--- tests/cairo_1_syscalls.rs
+++ tests/cairo_1_syscalls.rs
@@ -762,6 +762,102 @@ fn test_send_message_to_l1_syscall() {
 }
 
 #[test]
+fn test_get_execution_info() {
+    //  Create program and entry point types for contract class
+    let program_data = include_bytes!("../starknet_programs/cairo1/get_execution_info.casm");
+    let contract_class: CasmContractClass = serde_json::from_slice(program_data).unwrap();
+    let entrypoints = contract_class.clone().entry_points_by_type;
+    let external_entrypoint_selector = &entrypoints.external.get(0).unwrap().selector;
+
+    // Create state reader with class hash data
+    let mut contract_class_cache = HashMap::new();
+
+    let address = Address(1111.into());
+    let class_hash: ClassHash = [1; 32];
+    let nonce = Felt252::zero();
+
+    contract_class_cache.insert(class_hash, contract_class);
+    let mut state_reader = InMemoryStateReader::default();
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(address.clone(), class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(address.clone(), nonce);
+
+    // Create state from the state_reader and contract cache.
+    let mut state = CachedState::new(state_reader, None, Some(contract_class_cache));
+
+    let general_config = StarknetGeneralConfig::default();
+    let tx_execution_context = TransactionExecutionContext::new(
+        Address(0.into()),
+        Felt252::zero(),
+        vec![22.into(), 33.into()],
+        0,
+        10.into(),
+        general_config.invoke_tx_max_n_steps(),
+        TRANSACTION_VERSION,
+    );
+
+    let mut resources_manager = ExecutionResourcesManager::default();
+
+    let create_execute_extrypoint = |selector: &BigUint,
+                                     calldata: Vec<Felt252>,
+                                     entry_point_type: EntryPointType|
+     -> ExecutionEntryPoint {
+        ExecutionEntryPoint::new(
+            address.clone(),
+            calldata,
+            Felt252::new(selector.clone()),
+            Address(0000.into()),
+            entry_point_type,
+            Some(CallType::Delegate),
+            Some(class_hash),
+            100000,
+        )
+    };
+
+    // RUN GET_INFO
+    // Create an execution entry point
+    let get_info_exec_entry_point = create_execute_extrypoint(
+        external_entrypoint_selector,
+        vec![],
+        EntryPointType::External,
+    );
+
+    // Run send_msg entrypoint
+    let call_info = get_info_exec_entry_point
+        .execute(
+            &mut state,
+            &general_config,
+            &mut resources_manager,
+            &tx_execution_context,
+            false,
+        )
+        .unwrap();
+
+    let expected_ret_data = vec![
+        general_config.block_info().sequencer_address.0.clone(),
+        0.into(),
+        0.into(),
+        address.0.clone(),
+    ];
+
+    let expected_call_info = CallInfo {
+        caller_address: Address(0.into()),
+        call_type: Some(CallType::Delegate),
+        contract_address: address.clone(),
+        class_hash: Some(class_hash),
+        entry_point_selector: Some(external_entrypoint_selector.into()),
+        entry_point_type: Some(EntryPointType::External),
+        retdata: expected_ret_data,
+        ..Default::default()
+    };
+
+    assert_eq!(call_info, expected_call_info);
+}
+
+#[test]
 fn replace_class_internal() {
     // This test only checks that the contract is updated in the storage, see `replace_class_contract_call`
     //  Create program and entry point types for contract class
