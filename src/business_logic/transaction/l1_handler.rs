1e50fc97790f1e972593dfb0b526edbf10a94a3d
SantiagoPittella <pittellasantiago@gmail.com>
2023-06-09 15:58:21 -0300

    add l1 handler to os usage, add test

--- src/business_logic/transaction/l1_handler.rs
+++ src/business_logic/transaction/l1_handler.rs
@@ -159,3 +159,138 @@ impl L1Handler {
         ))
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{
+        collections::{HashMap, HashSet},
+        path::PathBuf,
+    };
+
+    use cairo_vm::{
+        felt::{felt_str, Felt252},
+        vm::runners::cairo_runner::ExecutionResources,
+    };
+    use num_traits::{Num, Zero};
+    use starknet_contract_class::EntryPointType;
+
+    use crate::{
+        business_logic::{
+            execution::{CallInfo, TransactionExecutionInfo},
+            fact_state::in_memory_state_reader::InMemoryStateReader,
+            state::{cached_state::CachedState, state_api::State},
+            transaction::l1_handler::L1Handler,
+        },
+        definitions::{general_config::StarknetGeneralConfig, transaction_type::TransactionType},
+        services::api::contract_classes::deprecated_contract_class::ContractClass,
+        utils::Address,
+    };
+
+    #[test]
+    fn test_execute_l1_handler() {
+        let l1_handler = L1Handler::new(
+            Address(0.into()),
+            Felt252::from_str_radix(
+                "c73f681176fc7b3f9693986fd7b14581e8d540519e27400e88b8713932be01",
+                16,
+            )
+            .unwrap(),
+            vec![
+                Felt252::from_str_radix("8359E4B0152ed5A731162D3c7B0D8D56edB165A0", 16).unwrap(),
+                1.into(),
+                10.into(),
+            ],
+            0.into(),
+            0.into(),
+            Some(10000.into()),
+        );
+
+        // Instantiate CachedState
+        let mut state_reader = InMemoryStateReader::default();
+        // Set contract_class
+        let class_hash = [1; 32];
+        let contract_class =
+            ContractClass::try_from(PathBuf::from("starknet_programs/l1l2.json")).unwrap();
+        // Set contact_state
+        let contract_address = Address(0.into());
+        let nonce = Felt252::zero();
+
+        state_reader
+            .address_to_class_hash_mut()
+            .insert(contract_address.clone(), class_hash);
+        state_reader
+            .address_to_nonce
+            .insert(contract_address, nonce);
+
+        let mut state = CachedState::new(state_reader.clone(), None, None);
+
+        // Initialize state.contract_classes
+        state.set_contract_classes(HashMap::new()).unwrap();
+
+        state
+            .set_contract_class(&class_hash, &contract_class)
+            .unwrap();
+
+        let mut config = StarknetGeneralConfig::default();
+        config.cairo_resource_fee_weights = HashMap::from([
+            (String::from("l1_gas_usage"), 0.into()),
+            (String::from("pedersen_builtin"), 16.into()),
+            (String::from("range_check_builtin"), 70.into()),
+        ]);
+        config.starknet_os_config.gas_price = 1;
+
+        let tx_exec = l1_handler.execute(&mut state, &config, 100000).unwrap();
+
+        let expected_tx_exec = expected_tx_exec_info();
+        assert_eq!(tx_exec, expected_tx_exec)
+    }
+
+    fn expected_tx_exec_info() -> TransactionExecutionInfo {
+        TransactionExecutionInfo {
+            validate_info: None,
+            call_info: Some(CallInfo {
+                caller_address: Address(0.into()),
+                call_type: Some(crate::business_logic::execution::CallType::Call),
+                contract_address: Address(0.into()),
+                code_address: None,
+                class_hash: Some([
+                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1, 1,
+                ]),
+                entry_point_selector: Some(felt_str!(
+                    "352040181584456735608515580760888541466059565068553383579463728554843487745"
+                )),
+                entry_point_type: Some(EntryPointType::L1Handler),
+                calldata: vec![
+                    felt_str!("749882478819638189522059655282096373471980381600"),
+                    1.into(),
+                    10.into(),
+                ],
+                retdata: vec![],
+                execution_resources: ExecutionResources {
+                    n_steps: 0,
+                    n_memory_holes: 0,
+                    builtin_instance_counter: HashMap::new(),
+                },
+                events: vec![],
+                l2_to_l1_messages: vec![],
+                storage_read_values: vec![0.into()],
+                accessed_storage_keys: HashSet::from([[
+                    4, 40, 11, 247, 0, 35, 63, 18, 141, 159, 101, 81, 182, 2, 213, 216, 100, 110,
+                    5, 5, 101, 122, 13, 252, 204, 72, 77, 8, 58, 226, 194, 24,
+                ]]),
+                internal_calls: vec![],
+                gas_consumed: 0,
+                failure_flag: false,
+            }),
+            fee_transfer_info: None,
+            actual_fee: 0,
+            actual_resources: HashMap::from([
+                ("pedersen_builtin".to_string(), 13),
+                ("range_check_builtin".to_string(), 23),
+                ("l1_gas_usage".to_string(), 18471),
+            ]),
+            tx_type: Some(TransactionType::L1Handler),
+        }
+    }
+}
