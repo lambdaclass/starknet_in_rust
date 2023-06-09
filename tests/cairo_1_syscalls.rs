98b229afe5486199148709437e662407a51b898f
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-06-09 18:25:54 +0300

    bugfix: Fix how `CallInfo` is built at the end of `ExecuteEntryPoint::_execute` (#614)
    * Fix CallInfo for cairo 1
    
    * Remove TODO
    
    * Add gas values to tests
    
    * Fix test
    
    * Clippy
    
    * Fix gas calculation in get_call_result
    
    * Fix gas values in tests
    
    * Fix test value
    
    * use staurating sub
    
    * Fix failure flag value
    
    * Fix
    
    * Fix CallInfo::result()

--- tests/cairo_1_syscalls.rs
+++ tests/cairo_1_syscalls.rs
@@ -277,6 +277,7 @@ fn library_call() {
             retdata: [5.into()].to_vec(),
             execution_resources: ExecutionResources::default(),
             class_hash: Some(lib_class_hash),
+            gas_consumed: 0,
             ..Default::default()
         }],
         code_address: None,
@@ -284,6 +285,7 @@ fn library_call() {
         l2_to_l1_messages: vec![],
         storage_read_values: vec![],
         accessed_storage_keys: HashSet::new(),
+        gas_consumed: 78980,
         ..Default::default()
     };
 
@@ -756,6 +758,7 @@ fn test_send_message_to_l1_syscall() {
         entry_point_selector: Some(external_entrypoint_selector.into()),
         entry_point_type: Some(EntryPointType::External),
         l2_to_l1_messages,
+        gas_consumed: 10040,
         ..Default::default()
     };
 
@@ -852,6 +855,7 @@ fn test_get_execution_info() {
         entry_point_selector: Some(external_entrypoint_selector.into()),
         entry_point_type: Some(EntryPointType::External),
         retdata: expected_ret_data,
+        gas_consumed: 38180,
         ..Default::default()
     };
 
@@ -1597,7 +1601,8 @@ fn test_out_of_gas_failure() {
     assert_eq!(
         call_info.retdata,
         vec![Felt252::from_bytes_be("Out of gas".as_bytes())]
-    )
+    );
+    assert!(call_info.failure_flag)
 }
 
 #[test]
