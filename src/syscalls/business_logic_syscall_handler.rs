2925c97df7181668f71099f8492b88edecbd9319
Federica <fedemoletta@hotmail.com>
2023-06-15 16:32:01 -0300

    Remove swaps

--- src/syscalls/business_logic_syscall_handler.rs
+++ src/syscalls/business_logic_syscall_handler.rs
@@ -648,15 +648,11 @@ where
         remaining_gas: u128,
     ) -> Result<SyscallResponse, SyscallHandlerError> {
         let calldata = get_felt_range(vm, request.calldata_start, request.calldata_end)?;
-        // Change own contract_address to the called one in order to interact with its storage
-        let previous_caller_address = std::mem::take(&mut self.caller_address);
-        self.caller_address = std::mem::take(&mut self.contract_address);
-        self.contract_address = request.contract_address;
         let execution_entry_point = ExecutionEntryPoint::new(
-            self.contract_address.clone(),
+            request.contract_address,
             calldata,
             request.selector,
-            self.caller_address.clone(),
+            self.contract_address.clone(),
             EntryPointType::External,
             Some(CallType::Call),
             None,
@@ -664,9 +660,6 @@ where
         );
 
         let response = self.call_contract_helper(vm, remaining_gas, execution_entry_point);
-        // Restore contract address after call is finished
-        self.contract_address = std::mem::take(&mut self.caller_address);
-        self.caller_address = previous_caller_address;
         response
     }
 
