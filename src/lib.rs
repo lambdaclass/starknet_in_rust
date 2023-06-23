809952118b3667c100c7ea3322d34a0575f5c6d3
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-06-23 18:20:28 -0300

    Add estimate_message_fee (#679)
    * Add estimate_message_fee
    
    * Test estimate_message_fee
    
    * Fix clippy

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ use crate::{
 use definitions::block_context::BlockContext;
 use state::cached_state::CachedState;
 use transaction::InvokeFunction;
+use transaction::L1Handler;
 use utils::Address;
 
 #[cfg(test)]
@@ -61,7 +62,7 @@ pub fn simulate_transaction<S: StateReader>(
 pub fn estimate_fee<T>(
     transaction: &Transaction,
     state: T,
-    transaction_context: &BlockContext,
+    block_context: &BlockContext,
 ) -> Result<(u128, usize), TransactionError>
 where
     T: StateReader,
@@ -73,8 +74,7 @@ where
     cached_state.get_class_hash_at(&transaction.contract_address())?;
 
     // execute the transaction with the fake state.
-    let transaction_result =
-        transaction.execute(&mut cached_state, transaction_context, 1_000_000)?;
+    let transaction_result = transaction.execute(&mut cached_state, block_context, 1_000_000)?;
     if let Some(gas_usage) = transaction_result.actual_resources.get("l1_gas_usage") {
         let actual_fee = transaction_result.actual_fee;
         Ok((actual_fee, *gas_usage))
@@ -134,6 +134,31 @@ pub fn call_contract<T: State + StateReader>(
     Ok(call_info.retdata)
 }
 
+/// Estimate the fee associated with L1Handler
+pub fn estimate_message_fee<T>(
+    l1_handler: &L1Handler,
+    state: T,
+    block_context: &BlockContext,
+) -> Result<(u128, usize), TransactionError>
+where
+    T: StateReader,
+{
+    // This is used as a copy of the original state, we can update this cached state freely.
+    let mut cached_state = CachedState::<T>::new(state, None, None);
+
+    // Check if the contract is deployed.
+    cached_state.get_class_hash_at(l1_handler.contract_address())?;
+
+    // execute the transaction with the fake state.
+    let transaction_result = l1_handler.execute(&mut cached_state, block_context, 1_000_000)?;
+    if let Some(gas_usage) = transaction_result.actual_resources.get("l1_gas_usage") {
+        let actual_fee = transaction_result.actual_fee;
+        Ok((actual_fee, *gas_usage))
+    } else {
+        Err(TransactionError::ResourcesError)
+    }
+}
+
 pub fn execute_transaction<T: State + StateReader>(
     tx: Transaction,
     state: &mut T,
@@ -149,20 +174,22 @@ mod test {
     use std::path::PathBuf;
 
     use crate::core::contract_address::compute_deprecated_class_hash;
+    use crate::definitions::block_context::StarknetChainId;
     use crate::definitions::constants::EXECUTE_ENTRY_POINT_SELECTOR;
     use crate::estimate_fee;
+    use crate::estimate_message_fee;
     use crate::services::api::contract_classes::deprecated_contract_class::ContractClass;
     use crate::testing::{create_account_tx_test_state, TEST_CONTRACT_ADDRESS, TEST_CONTRACT_PATH};
-    use crate::transaction::{InvokeFunction, Transaction};
+    use crate::transaction::{InvokeFunction, L1Handler, Transaction};
     use crate::utils::felt_to_hash;
     use cairo_lang_starknet::casm_contract_class::CasmContractClass;
     use cairo_vm::felt::{felt_str, Felt252};
-    use num_traits::{One, Zero};
+    use num_traits::{Num, One, Zero};
     use starknet_contract_class::EntryPointType;
 
     use crate::{
         call_contract,
-        definitions::block_context::{BlockContext, StarknetChainId},
+        definitions::block_context::BlockContext,
         simulate_transaction,
         state::{
             cached_state::CachedState, in_memory_state_reader::InMemoryStateReader,
@@ -235,6 +262,61 @@ mod test {
         .unwrap();
 
         assert_eq!(retdata, vec![89.into()]);
+    }
+
+    #[test]
+    fn test_estimate_message_fee() {
+        let l1_handler = L1Handler::new(
+            Address(0.into()),
+            Felt252::from_str_radix(
+                "c73f681176fc7b3f9693986fd7b14581e8d540519e27400e88b8713932be01",
+                16,
+            )
+            .unwrap(),
+            vec![
+                Felt252::from_str_radix("8359E4B0152ed5A731162D3c7B0D8D56edB165A0", 16).unwrap(),
+                1.into(),
+                10.into(),
+            ],
+            0.into(),
+            0.into(),
+            Some(10000.into()),
+        )
+        .unwrap();
+
+        // Instantiate CachedState
+        let mut state_reader = InMemoryStateReader::default();
+        // Set contract_class
+        let class_hash = [1; 32];
+        let contract_class =
+            ContractClass::try_from(PathBuf::from("starknet_programs/l1l2.json")).unwrap();
+        // Set contact_state
+        let contract_address = Address(0.into());
+        let nonce = Felt252::zero();
+
+        state_reader
+            .address_to_class_hash_mut()
+            .insert(contract_address.clone(), class_hash);
+        state_reader
+            .address_to_nonce
+            .insert(contract_address, nonce);
+
+        let mut state = CachedState::new(state_reader.clone(), None, None);
+
+        // Initialize state.contract_classes
+        let contract_classes = HashMap::from([(class_hash, contract_class)]);
+        state.set_contract_classes(contract_classes).unwrap();
+
+        let mut block_context = BlockContext::default();
+        block_context.cairo_resource_fee_weights = HashMap::from([
+            (String::from("l1_gas_usage"), 0.into()),
+            (String::from("pedersen_builtin"), 16.into()),
+            (String::from("range_check_builtin"), 70.into()),
+        ]);
+        block_context.starknet_os_config.gas_price = 1;
+
+        let estimated_fee = estimate_message_fee(&l1_handler, state, &block_context).unwrap();
+        assert_eq!(estimated_fee, (0, 18471));
     }
 
     #[test]
