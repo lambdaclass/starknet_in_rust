81e39351b4f9741ca732b4efa77fbb51921af461
SantiagoPittella <pittellasantiago@gmail.com>
2023-06-26 13:06:49 -0300

    update amm tests

--- tests/complex_contracts/amm_contracts/amm_proxy.rs
+++ tests/complex_contracts/amm_contracts/amm_proxy.rs
@@ -301,8 +301,8 @@ fn amm_proxy_add_demo_token_test() {
         calldata: calldata.clone()[1..].to_vec(),
         storage_read_values: vec![0.into(), 0.into()],
         execution_resources: ExecutionResources {
-            n_steps: 401,
-            n_memory_holes: 40,
+            n_steps: 397,
+            n_memory_holes: 42,
             builtin_instance_counter: HashMap::from([
                 ("pedersen_builtin".to_string(), 8),
                 ("range_check_builtin".to_string(), 20),
@@ -321,8 +321,8 @@ fn amm_proxy_add_demo_token_test() {
         entry_point_type: Some(EntryPointType::External),
         calldata: calldata.clone(),
         execution_resources: ExecutionResources {
-            n_steps: 449,
-            n_memory_holes: 40,
+            n_steps: 445,
+            n_memory_holes: 42,
             builtin_instance_counter: HashMap::from([
                 ("pedersen_builtin".to_string(), 8),
                 ("range_check_builtin".to_string(), 20),
@@ -427,8 +427,8 @@ fn amm_proxy_get_account_token_balance() {
         retdata: [200.into()].to_vec(),
         storage_read_values: [200.into()].to_vec(),
         execution_resources: ExecutionResources {
-            n_steps: 94,
-            n_memory_holes: 10,
+            n_steps: 92,
+            n_memory_holes: 11,
             builtin_instance_counter: HashMap::from([
                 ("pedersen_builtin".to_string(), 2),
                 ("range_check_builtin".to_string(), 3),
@@ -448,8 +448,8 @@ fn amm_proxy_get_account_token_balance() {
         calldata: calldata.clone(),
         retdata: [200.into()].to_vec(),
         execution_resources: ExecutionResources {
-            n_steps: 153,
-            n_memory_holes: 10,
+            n_steps: 151,
+            n_memory_holes: 11,
             builtin_instance_counter: HashMap::from([
                 ("pedersen_builtin".to_string(), 2),
                 ("range_check_builtin".to_string(), 3),
@@ -561,8 +561,8 @@ fn amm_proxy_swap() {
         storage_read_values: [100.into(), 1000.into(), 1000.into(), 100.into(), 200.into()]
             .to_vec(),
         execution_resources: ExecutionResources {
-            n_steps: 830,
-            n_memory_holes: 90,
+            n_steps: 826,
+            n_memory_holes: 92,
             builtin_instance_counter: HashMap::from([
                 ("pedersen_builtin".to_string(), 14),
                 ("range_check_builtin".to_string(), 41),
@@ -582,8 +582,8 @@ fn amm_proxy_swap() {
         calldata: calldata.clone(),
         retdata: expected_result,
         execution_resources: ExecutionResources {
-            n_steps: 889,
-            n_memory_holes: 90,
+            n_steps: 885,
+            n_memory_holes: 92,
             builtin_instance_counter: HashMap::from([
                 ("pedersen_builtin".to_string(), 14),
                 ("range_check_builtin".to_string(), 41),
