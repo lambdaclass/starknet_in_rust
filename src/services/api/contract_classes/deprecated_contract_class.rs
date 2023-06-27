b43cb95b577748e857948f33af0cb4b7a9b23877
Juan Bono <juanbono94@gmail.com>
2023-06-27 08:30:50 -0300

    add lambda_starknet_api (#692)

--- src/services/api/contract_classes/deprecated_contract_class.rs
+++ src/services/api/contract_classes/deprecated_contract_class.rs
@@ -2,8 +2,8 @@ use crate::services::api::contract_class_errors::ContractClassError;
 use cairo_vm::felt::Felt252;
 use cairo_vm::types::{errors::program_errors::ProgramError, program::Program};
 use getset::Getters;
+use lambda_starknet_api::deprecated_contract_class::EntryPoint;
 use serde_json::Value;
-use starknet_api::deprecated_contract_class::EntryPoint;
 pub use starknet_contract_class::to_cairo_runner_program;
 use starknet_contract_class::AbiType;
 use starknet_contract_class::{ContractEntryPoint, EntryPointType};
@@ -54,11 +54,11 @@ impl ContractClass {
 // -------------------------------
 //         From traits
 // -------------------------------
-impl TryFrom<starknet_api::deprecated_contract_class::ContractClass> for ContractClass {
+impl TryFrom<lambda_starknet_api::deprecated_contract_class::ContractClass> for ContractClass {
     type Error = ProgramError;
 
     fn try_from(
-        contract_class: starknet_api::deprecated_contract_class::ContractClass,
+        contract_class: lambda_starknet_api::deprecated_contract_class::ContractClass,
     ) -> Result<Self, Self::Error> {
         let program = to_cairo_runner_program(&contract_class.program)?;
         let entry_points_by_type =
@@ -81,7 +81,7 @@ impl TryFrom<&str> for ContractClass {
     type Error = ProgramError;
 
     fn try_from(s: &str) -> Result<Self, ProgramError> {
-        let raw_contract_class: starknet_api::deprecated_contract_class::ContractClass =
+        let raw_contract_class: lambda_starknet_api::deprecated_contract_class::ContractClass =
             serde_json::from_str(s)?;
         ContractClass::try_from(raw_contract_class)
     }
@@ -101,7 +101,7 @@ impl TryFrom<&PathBuf> for ContractClass {
     fn try_from(path: &PathBuf) -> Result<Self, Self::Error> {
         let file = File::open(path)?;
         let reader = BufReader::new(file);
-        let raw_contract_class: starknet_api::deprecated_contract_class::ContractClass =
+        let raw_contract_class: lambda_starknet_api::deprecated_contract_class::ContractClass =
             serde_json::from_reader(reader)?;
         ContractClass::try_from(raw_contract_class)
     }
@@ -111,14 +111,17 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for ContractClass {
     type Error = ProgramError;
 
     fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
-        let raw_contract_class: starknet_api::deprecated_contract_class::ContractClass =
+        let raw_contract_class: lambda_starknet_api::deprecated_contract_class::ContractClass =
             serde_json::from_reader(reader)?;
         ContractClass::try_from(raw_contract_class)
     }
 }
 
 fn convert_entry_points(
-    entry_points: HashMap<starknet_api::deprecated_contract_class::EntryPointType, Vec<EntryPoint>>,
+    entry_points: HashMap<
+        lambda_starknet_api::deprecated_contract_class::EntryPointType,
+        Vec<EntryPoint>,
+    >,
 ) -> HashMap<EntryPointType, Vec<ContractEntryPoint>> {
     let mut converted_entries: HashMap<EntryPointType, Vec<ContractEntryPoint>> = HashMap::new();
     for (entry_type, vec) in entry_points {
