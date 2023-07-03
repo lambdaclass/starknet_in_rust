800ac186dacb9a79dfb90144ceeedee5382733f4
matias-gonz <maigonzalez@fi.uba.ar>
2023-07-03 15:47:36 -0300

    Rename to block_context

--- tests/internals.rs
+++ tests/internals.rs
@@ -14,6 +14,7 @@ use num_bigint::BigUint;
 use num_traits::{Num, One, ToPrimitive, Zero};
 use starknet_contract_class::EntryPointType;
 use starknet_in_rust::core::errors::state_errors::StateError;
+use starknet_in_rust::definitions::block_context;
 use starknet_in_rust::definitions::constants::{
     DEFAULT_CAIRO_RESOURCE_FEE_WEIGHTS, VALIDATE_ENTRY_POINT_SELECTOR,
 };
@@ -1128,17 +1129,17 @@ fn test_invoke_tx_state() {
 
 #[test]
 fn test_invoke_with_declarev2_tx() {
-    let (starknet_general_config, state) = &mut create_account_tx_test_state().unwrap();
+    let (block_context, state) = &mut create_account_tx_test_state().unwrap();
     let expected_initial_state = expected_state_before_tx();
     assert_eq!(state, &expected_initial_state);
 
     // Declare the fibonacci contract
     let declare_tx = declarev2_tx();
-    declare_tx.execute(state, starknet_general_config).unwrap();
+    declare_tx.execute(state, block_context).unwrap();
 
     // Deploy the fibonacci contract
     let deploy = deploy_fib_syscall();
-    deploy.execute(state, starknet_general_config).unwrap();
+    deploy.execute(state, block_context).unwrap();
 
     let Address(test_contract_address) = TEST_FIB_CONTRACT_ADDRESS.clone();
     let calldata = vec![
@@ -1153,7 +1154,7 @@ fn test_invoke_with_declarev2_tx() {
 
     let expected_gas_consumed = 4380;
     let result = invoke_tx
-        .execute(state, starknet_general_config, expected_gas_consumed)
+        .execute(state, block_context, expected_gas_consumed)
         .unwrap();
 
     let expected_execution_info = expected_fib_transaction_execution_info();
