503220973ef0f282e29ad9f70efefc12466cea10
SantiagoPittella <pittellasantiago@gmail.com>
2023-07-04 20:02:52 -0300

    add asserts

--- src/testing/erc20.rs
+++ src/testing/erc20.rs
@@ -1,17 +1,30 @@
 #![allow(unused_imports)]
-use std::{collections::HashMap, vec, io::Bytes, path::Path};
+use std::{collections::HashMap, io::Bytes, path::Path, vec};
 
+use crate::{
+    definitions::{
+        block_context::{BlockContext, StarknetChainId},
+        constants::CONSTRUCTOR_ENTRY_POINT_SELECTOR,
+    },
+    execution::{
+        execution_entry_point::ExecutionEntryPoint, CallType, TransactionExecutionContext,
+    },
+    services::api::contract_classes::deprecated_contract_class::ContractClass,
+    state::{
+        cached_state::CachedState,
+        in_memory_state_reader::InMemoryStateReader,
+        state_api::{State, StateReader},
+        ExecutionResourcesManager,
+    },
+    transaction::{error::TransactionError, DeployAccount, InvokeFunction},
+    utils::calculate_sn_keccak,
+    Felt252,
+};
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
 use cairo_vm::felt::felt_str;
 use lazy_static::lazy_static;
 use num_traits::Zero;
 use starknet_contract_class::EntryPointType;
-use crate::{
-    definitions::{block_context::{BlockContext, StarknetChainId}, constants::CONSTRUCTOR_ENTRY_POINT_SELECTOR},
-    state::{
-        cached_state::CachedState, in_memory_state_reader::InMemoryStateReader, ExecutionResourcesManager, state_api::{State, StateReader},
-    }, Felt252, transaction::{InvokeFunction, DeployAccount, error::TransactionError}, execution::{TransactionExecutionContext, execution_entry_point::ExecutionEntryPoint, CallType}, utils::calculate_sn_keccak, services::api::contract_classes::deprecated_contract_class::ContractClass,
-};
 pub const ERC20_CONTRACT_PATH: &str = "starknet_programs/cairo2/ERC20.casm";
 use crate::{
     state::state_cache::StorageEntry,
@@ -78,130 +91,113 @@ pub fn call_contract<T: State + StateReader>(
 }
 
 #[test]
-fn deploy_erc20_cairo2_without_constructor() {
-        // data to deploy
-        let erc20_class_hash: ClassHash = [2; 32];
-        let test_data = include_bytes!("../../starknet_programs/cairo2/erc20.casm");
-        let test_contract_class: CasmContractClass = serde_json::from_slice(test_data).unwrap();
-    
-        // Create the deploy contract class
-        let program_data = include_bytes!("../../starknet_programs/cairo2/deploy_erc20.casm");
-        let contract_class: CasmContractClass = serde_json::from_slice(program_data).unwrap();
-        let entrypoints = contract_class.clone().entry_points_by_type;
-        let entrypoint_selector = &entrypoints.external.get(0).unwrap().selector;
-    
-        // Create state reader with class hash data
-        let mut contract_class_cache = HashMap::new();
-    
-        let address = Address(1111.into());
-        let class_hash: ClassHash = [1; 32];
-        let nonce = Felt252::zero();
-    
-        contract_class_cache.insert(class_hash, contract_class);
-        contract_class_cache.insert(erc20_class_hash, test_contract_class.clone());
-        
-        let mut state_reader = InMemoryStateReader::default();
-        state_reader
-            .address_to_class_hash_mut()
-            .insert(address.clone(), class_hash);
-        state_reader
-            .address_to_nonce_mut()
-            .insert(address.clone(), nonce);
-    
-        // Create state from the state_reader and contract cache.
-        let mut state = CachedState::new(state_reader, None, Some(contract_class_cache));
-    
-        let name_ = Felt252::from_bytes_be(b"some-token");
-        let symbol_ = Felt252::from_bytes_be(b"my-super-awesome-token");
-        let decimals_ = Felt252::from(24);
-        let initial_supply = Felt252::from(1000);
-        let recipient = felt_str!("397149464972449753182583229366244826403270781177748543857889179957856017275");
-        let erc20_salt = felt_str!("1234");
-        // arguments of deploy contract
-        //let calldata = vec![Felt252::from_bytes_be(&erc20_class_hash), erc20_salt.clone(), name_.clone(), symbol_.clone(), decimals_.clone(), initial_supply.clone(), recipient.clone()];
-        let calldata = vec![Felt252::from_bytes_be(&erc20_class_hash), erc20_salt.clone(), recipient.clone(), name_.clone(), decimals_.clone(), initial_supply.clone(), symbol_.clone()];
-        //let calldata = vec![Felt252::from_bytes_be(&erc20_class_hash), erc20_salt.clone(), felt_str!("1")];
-        // set up remaining structures
-    
-        let caller_address = Address(0000.into());
-        let entry_point_type = EntryPointType::External;
-    
-        let exec_entry_point = ExecutionEntryPoint::new(
-            address,
-            calldata,
-            Felt252::new(entrypoint_selector.clone()),
-            caller_address,
-            entry_point_type,
-            Some(CallType::Delegate),
-            Some(class_hash),
-            100_000_000_000,
-        );
-    
-        // Execute the entrypoint
-        let block_context = BlockContext::default();
-        let mut tx_execution_context = TransactionExecutionContext::new(
-            Address(0.into()),
-            Felt252::zero(),
-            Vec::new(),
-            0,
-            10.into(),
-            block_context.invoke_tx_max_n_steps(),
-            1.into(),
-        );
-        let mut resources_manager = ExecutionResourcesManager::default();
+fn test_erc20_cairo2() {
+    // data to deploy
+    let erc20_class_hash: ClassHash = [2; 32];
+    let test_data = include_bytes!("../../starknet_programs/cairo2/erc20.casm");
+    let test_contract_class: CasmContractClass = serde_json::from_slice(test_data).unwrap();
 
-        let call_info = exec_entry_point.execute(
+    // Create the deploy contract class
+    let program_data = include_bytes!("../../starknet_programs/cairo2/deploy_erc20.casm");
+    let contract_class: CasmContractClass = serde_json::from_slice(program_data).unwrap();
+    let entrypoints = contract_class.clone().entry_points_by_type;
+    let entrypoint_selector = &entrypoints.external.get(0).unwrap().selector;
+
+    // Create state reader with class hash data
+    let mut contract_class_cache = HashMap::new();
+
+    let address = Address(1111.into());
+    let class_hash: ClassHash = [1; 32];
+    let nonce = Felt252::zero();
+
+    contract_class_cache.insert(class_hash, contract_class);
+    contract_class_cache.insert(erc20_class_hash, test_contract_class.clone());
+
+    let mut state_reader = InMemoryStateReader::default();
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(address.clone(), class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(address.clone(), nonce);
+
+    // Create state from the state_reader and contract cache.
+    let mut state = CachedState::new(state_reader, None, Some(contract_class_cache));
+
+    let name_ = Felt252::from_bytes_be(b"some-token");
+    let symbol_ = Felt252::from_bytes_be(b"my-super-awesome-token");
+    let decimals_ = Felt252::from(24);
+    let initial_supply = Felt252::from(1000);
+    let recipient =
+        felt_str!("397149464972449753182583229366244826403270781177748543857889179957856017275");
+    let erc20_salt = felt_str!("1234");
+    // arguments of deploy contract
+    //let calldata = vec![Felt252::from_bytes_be(&erc20_class_hash), erc20_salt.clone(), name_.clone(), symbol_.clone(), decimals_.clone(), initial_supply.clone(), recipient.clone()];
+    let calldata = vec![
+        Felt252::from_bytes_be(&erc20_class_hash),
+        erc20_salt.clone(),
+        recipient.clone(),
+        name_.clone(),
+        decimals_.clone(),
+        initial_supply.clone(),
+        symbol_.clone(),
+    ];
+    //let calldata = vec![Felt252::from_bytes_be(&erc20_class_hash), erc20_salt.clone(), felt_str!("1")];
+    // set up remaining structures
+
+    let caller_address = Address(0000.into());
+    let entry_point_type = EntryPointType::External;
+
+    let exec_entry_point = ExecutionEntryPoint::new(
+        address,
+        calldata,
+        Felt252::new(entrypoint_selector.clone()),
+        caller_address,
+        entry_point_type,
+        Some(CallType::Delegate),
+        Some(class_hash),
+        100_000_000_000,
+    );
+
+    // Execute the entrypoint
+    let block_context = BlockContext::default();
+    let mut tx_execution_context = TransactionExecutionContext::new(
+        Address(0.into()),
+        Felt252::zero(),
+        Vec::new(),
+        0,
+        10.into(),
+        block_context.invoke_tx_max_n_steps(),
+        1.into(),
+    );
+    let mut resources_manager = ExecutionResourcesManager::default();
+
+    let call_info = exec_entry_point
+        .execute(
             &mut state,
             &block_context,
             &mut resources_manager,
             &mut tx_execution_context,
             false,
-        ).unwrap();
-        let erc20_address = call_info.clone().retdata.get(0).unwrap().clone();
-        println!("deploy erc20 call_info: {:?}", call_info);
-        println!("message: {:?}", std::str::from_utf8(&Felt252::to_be_bytes(&erc20_address)));
-
-
-
-    // // INSERT ERC20 IN MEMORY
-    // let program_data = include_bytes!("../../starknet_programs/cairo2/erc20.casm");
-    // let contract_class: CasmContractClass = serde_json::from_slice(program_data).unwrap();
-    
-    // let mut contract_class_cache = HashMap::new();
-
-    // let address = Address(1111.into());
-    // let class_hash: ClassHash = [1; 32];
-    // let nonce = Felt252::zero();
-
-    // contract_class_cache.insert(class_hash, contract_class);
-    // let mut state_reader = InMemoryStateReader::default();
-    // state_reader
-    //     .address_to_class_hash_mut()
-    //     .insert(address.clone(), class_hash);
-    // state_reader
-    //     .address_to_nonce_mut()
-    //     .insert(address.clone(), nonce);
-
-    // let mut state = CachedState::new(state_reader, None, Some(contract_class_cache));
+        )
+        .unwrap();
+    let erc20_address = call_info.clone().retdata.get(0).unwrap().clone();
 
     // ACCOUNT 1
-    let program_data_account = include_bytes!("../../starknet_programs/cairo2/hello_world_account.casm");
-    let contract_class_account: CasmContractClass = serde_json::from_slice(program_data_account).unwrap();
+    let program_data_account =
+        include_bytes!("../../starknet_programs/cairo2/hello_world_account.casm");
+    let contract_class_account: CasmContractClass =
+        serde_json::from_slice(program_data_account).unwrap();
 
     state
-        .set_compiled_class(
-            &felt_str!("1"),
-            contract_class_account,
-        )
+        .set_compiled_class(&felt_str!("1"), contract_class_account)
         .unwrap();
 
     let contract_address_salt =
         felt_str!("2669425616857739096022668060305620640217901643963991674344872184515580705509");
 
     let internal_deploy_account = DeployAccount::new(
-        felt_str!("1")
-            .clone()
-            .to_be_bytes(),
+        felt_str!("1").clone().to_be_bytes(),
         0,
         1.into(),
         Felt252::zero(),
@@ -222,28 +218,25 @@ fn deploy_erc20_cairo2_without_constructor() {
 
     let account_address_1 = internal_deploy_account
         .execute(&mut state, &Default::default())
-        .unwrap().validate_info.unwrap().contract_address;
+        .unwrap()
+        .validate_info
+        .unwrap()
+        .contract_address;
 
-    println!("account_address_1: {:?}", account_address_1.clone());
-        
     // ACCOUNT 2
-    let program_data_account = include_bytes!("../../starknet_programs/cairo2/hello_world_account.casm");
-    let contract_class_account: CasmContractClass = serde_json::from_slice(program_data_account).unwrap();
+    let program_data_account =
+        include_bytes!("../../starknet_programs/cairo2/hello_world_account.casm");
+    let contract_class_account: CasmContractClass =
+        serde_json::from_slice(program_data_account).unwrap();
 
     state
-        .set_compiled_class(
-            &felt_str!("1"),
-            contract_class_account,
-        )
+        .set_compiled_class(&felt_str!("1"), contract_class_account)
         .unwrap();
 
-    let contract_address_salt =
-        felt_str!("123123123123123");
+    let contract_address_salt = felt_str!("123123123123123");
 
     let internal_deploy_account = DeployAccount::new(
-        felt_str!("1")
-            .clone()
-            .to_be_bytes(),
+        felt_str!("1").clone().to_be_bytes(),
         0,
         1.into(),
         Felt252::zero(),
@@ -264,39 +257,14 @@ fn deploy_erc20_cairo2_without_constructor() {
 
     let account_address_2 = internal_deploy_account
         .execute(&mut state, &Default::default())
-        .unwrap().validate_info.unwrap().contract_address;
-
-    println!("Account address 2: {:?}", account_address_2);
-
-    // CONSTRUCTOR CALLDATA FOR ERC20
-    // let name_ = Felt252::from_bytes_be(b"some-token");
-    // let symbol_ = Felt252::from_bytes_be(b"my-super-awesome-token");
-    // let decimals_ = Felt252::from(24);
-    // let initial_supply = Felt252::from(1000);
-    // let recipient = account_address_1.clone().0;
-    // let calldata = vec![name_, symbol_, decimals_, initial_supply, recipient];
-
-    // CONSTRUCTOR
-    // let entrypoint_selector = Felt252::from_bytes_be(&calculate_sn_keccak(b"deploy"));
-    // let erc20_salt = felt_str!("1234");
-    // let calldata = vec![Felt252::from_bytes_be(&class_hash), erc20_salt.clone(), name_.clone(), symbol_.clone(), decimals_.clone(), initial_supply.clone(), recipient.clone()];
-    // let erc20_account_address = call_contract(
-    //     account_address_1.clone().0,
-    //     entrypoint_selector.clone().into(),
-    //     calldata,
-    //     &mut state,
-    //     BlockContext::default(),
-    //     account_address_1.clone().into(),
-    // )
-    // .unwrap();
-    
-
-    println!("erc20 contract address: {:?}", erc20_address);
-    println!("message: {:?}", std::str::from_utf8(&Felt252::to_be_bytes(&erc20_address)));
+        .unwrap()
+        .validate_info
+        .unwrap()
+        .contract_address;
 
     // TRANSFER
     let entrypoint_selector = Felt252::from_bytes_be(&calculate_sn_keccak(b"transfer"));
-    let calldata = vec![account_address_2.clone().0, Felt252::from(100)];
+    let calldata = vec![account_address_2.clone().0, Felt252::from(123)];
 
     let _retdata = call_contract(
         erc20_address.clone(),
@@ -307,9 +275,8 @@ fn deploy_erc20_cairo2_without_constructor() {
         account_address_1.clone().into(),
     )
     .unwrap();
-    
-    println!("transfer retadata: {:?}", _retdata);
-    // println!("message: {:?}", std::str::from_utf8(&Felt252::to_be_bytes(&_retdata[0])));
+
+    assert_eq!(_retdata, []);
 
     // GET BALANCE ACCOUNT 1
     let entrypoint_selector = Felt252::from_bytes_be(&calculate_sn_keccak(b"balance_of"));
@@ -323,7 +290,8 @@ fn deploy_erc20_cairo2_without_constructor() {
     )
     .unwrap();
 
-    println!("balance account 1: {:?}", retdata);
+    assert_eq!(retdata, vec![877.into()]);
+
     // GET BALANCE ACCOUNT 2
     let entrypoint_selector = Felt252::from_bytes_be(&calculate_sn_keccak(b"balance_of"));
     let retdata = call_contract(
@@ -336,8 +304,5 @@ fn deploy_erc20_cairo2_without_constructor() {
     )
     .unwrap();
 
-    println!("balance account 2: {:?}", retdata);
-
-    println!("message: {:?}", std::str::from_utf8(&Felt252::to_be_bytes(&retdata[0])));
-    assert_eq!(retdata, vec![89.into()]);
+    assert_eq!(retdata, vec![123.into()]);
 }
