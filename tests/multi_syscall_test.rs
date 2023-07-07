4a160ed36d650f8321219e59f850faa4e9bcb2f7
fannyguthmann <fanny.guthmann@post.idc.ac.il>
2023-07-07 12:45:32 +0300

    make format changes

--- tests/multi_syscall_test.rs
+++ tests/multi_syscall_test.rs
@@ -1,26 +1,20 @@
-use std::{
-    collections::HashMap,
-    vec,
-};
-
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
-use cairo_vm::{
-    felt::Felt252,
-};
-use starknet_in_rust::EntryPointType;
+use cairo_vm::felt::Felt252;
+use std::{collections::HashMap, vec};
+
 use num_bigint::BigUint;
 use num_traits::{Num, Zero};
+use starknet_in_rust::EntryPointType;
 use starknet_in_rust::{
     definitions::{block_context::BlockContext, constants::TRANSACTION_VERSION},
     execution::{
         execution_entry_point::ExecutionEntryPoint, CallInfo, CallType, OrderedEvent,
         TransactionExecutionContext,
     },
-    state::{cached_state::CachedState},
+    state::cached_state::CachedState,
     state::{in_memory_state_reader::InMemoryStateReader, ExecutionResourcesManager},
     utils::{Address, ClassHash},
 };
-
 
 #[test]
 fn test_multiple_syscall() {
@@ -46,117 +40,179 @@ fn test_multiple_syscall() {
         .insert(address.clone(), nonce.clone());
 
     // Create state from the state_reader and contract cache.
-    let mut state = CachedState::new(state_reader.clone(), None, Some(contract_class_cache.clone()));
-
+    let mut state = CachedState::new(
+        state_reader.clone(),
+        None,
+        Some(contract_class_cache.clone()),
+    );
     // Create an execution entry point
-    let calldata= [].to_vec();
+    let calldata = [].to_vec();
     let caller_address = Address(0000.into());
     let entry_point_type = EntryPointType::External;
 
     // Block for get_caller_address.
     {
         let entrypoint_selector = &entrypoints.external.get(0).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
         assert_eq!(call_info.events, vec![])
     }
 
-    // Block for get_contract_address.
+    // Block for get_contact_address.
     {
         let entrypoint_selector = &entrypoints.external.get(1).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
-        assert_eq!(call_info.events, vec![])
-       
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
+        assert_eq!(call_info.events, vec![]);
     }
-    
     // Block for get_execution_info_syscall.
     {
         let entrypoint_selector = &entrypoints.external.get(2).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
-            assert_eq!(
-                call_info.events, vec![]);
-       
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
+        assert_eq!(call_info.events, vec![]);
     }
 
-     // Block for library_call_syscall
-     {
-         let entrypoint_selector = &entrypoints.external.get(3).unwrap().selector;
-         let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
-         assert_eq!(call_info.events, vec![])
-     }
+    // Block for library_call_syscall
+    {
+        let entrypoint_selector = &entrypoints.external.get(3).unwrap().selector;
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
+        assert_eq!(call_info.events, vec![])
+    }
 
-     // Block for call_contract_syscall
-     {
+    // Block for call_contract_syscall
+    {
         let entrypoint_selector = &entrypoints.external.get(4).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
         assert_eq!(call_info.events, vec![])
     }
 
     // Block for send_message_to_l1_syscall
     {
         let entrypoint_selector = &entrypoints.external.get(5).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
         assert_eq!(call_info.events, vec![])
     }
 
     // Block for replace_class_syscall
     {
         let entrypoint_selector = &entrypoints.external.get(6).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
         assert_eq!(call_info.events, vec![])
     }
 
     // Block for read write
     {
         let entrypoint_selector = &entrypoints.external.get(7).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
         assert_eq!(call_info.events, vec![])
     }
 
     // Block for emit
     {
         let entrypoint_selector = &entrypoints.external.get(8).unwrap().selector;
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
-    assert_eq!(
-        call_info.events,
-        vec![
-            OrderedEvent {
-                order: 0,
-                keys: vec![Felt252::from_str_radix(
-                    "1533133552972353850845856330693290141476612241335297758062928121906575244541",
-                    10
-                )
-                .unwrap()],
-                data: vec![1.into()]
-            },
-            OrderedEvent {
-                order: 1,
-                keys: vec![Felt252::from_str_radix(
-                    "1533133552972353850845856330693290141476612241335297758062928121906575244541",
-                    10
-                )
-                .unwrap()],
-                data: vec![2.into()]
-            },
-            OrderedEvent {
-                order: 2,
-                keys: vec![Felt252::from_str_radix(
-                    "1533133552972353850845856330693290141476612241335297758062928121906575244541",
-                    10
-                )
-                .unwrap()],
-                data: vec![3.into()]
-            }
-        ]
-    )
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
+        assert_eq!(
+            call_info.events,
+            vec![
+                OrderedEvent {
+                    order: 0,
+                    keys: vec![Felt252::from_str_radix(
+                        "1533133552972353850845856330693290141476612241335297758062928121906575244541",
+                        10
+                    )
+                    .unwrap()],
+                    data: vec![1.into()]
+                },
+                OrderedEvent {
+                    order: 1,
+                    keys: vec![Felt252::from_str_radix(
+                        "1533133552972353850845856330693290141476612241335297758062928121906575244541",
+                        10
+                    )
+                    .unwrap()],
+                    data: vec![2.into()]
+                },
+                OrderedEvent {
+                    order: 2,
+                    keys: vec![Felt252::from_str_radix(
+                        "1533133552972353850845856330693290141476612241335297758062928121906575244541",
+                        10
+                    )
+                    .unwrap()],
+                    data: vec![3.into()]
+                }
+            ]
+        )
     }
 
     // Block for deploy syscall
@@ -168,22 +224,30 @@ fn test_multiple_syscall() {
 
         // Create the deploy contract class
         let entrypoint_selector = &entrypoints.external.get(9).unwrap().selector;
-
         contract_class_cache.insert(class_hash, contract_class);
         contract_class_cache.insert(test_class_hash, test_contract_class.clone());
-       
-        // Create state from the state_reader and contract cache.
-
-        let call_info = test_syscall(entrypoint_selector,
-            address.clone(), calldata.clone(),  caller_address.clone(), entry_point_type, class_hash, &mut state);
+        let call_info = test_syscall(
+            entrypoint_selector,
+            address.clone(),
+            calldata.clone(),
+            caller_address.clone(),
+            entry_point_type,
+            class_hash,
+            &mut state,
+        );
         assert_eq!(call_info.events, vec![])
-
-    
     }
 }
 
-fn test_syscall(entrypoint_selector: &BigUint, address: Address, calldata: Vec<Felt252>, caller_address:Address, 
-    entry_point_type: EntryPointType,class_hash: [u8; 32], state: &mut CachedState<InMemoryStateReader>)-> CallInfo {
+fn test_syscall(
+    entrypoint_selector: &BigUint,
+    address: Address,
+    calldata: Vec<Felt252>,
+    caller_address: Address,
+    entry_point_type: EntryPointType,
+    class_hash: [u8; 32],
+    state: &mut CachedState<InMemoryStateReader>,
+) -> CallInfo {
     let exec_entry_point = ExecutionEntryPoint::new(
         address.clone(),
         calldata.clone(),
@@ -208,13 +272,13 @@ fn test_syscall(entrypoint_selector: &BigUint, address: Address, calldata: Vec<F
     );
     let mut resources_manager = ExecutionResourcesManager::default();
     let call_info = exec_entry_point
-    .execute(
-        state,
-        &block_context,
-        &mut resources_manager,
-        &mut tx_execution_context,
-        false,
-    )
-    .unwrap();
+        .execute(
+            state,
+            &block_context,
+            &mut resources_manager,
+            &mut tx_execution_context,
+            false,
+        )
+        .unwrap();
     call_info
 }
