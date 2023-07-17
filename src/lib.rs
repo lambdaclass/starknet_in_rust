ae062548631bed0c7bef5f6331462b4f884b9888
Santiago Pittella <87827390+SantiagoPittella@users.noreply.github.com>
2023-07-17 11:26:42 -0300

    Add class hash check for declarev2 (#819)
    * Add casm class hash
    
    * add casm class hash check
    
    * fix tests
    
    * Update src/lib.rs
    
    Co-authored-by: Mario Rugiero <mrugiero@gmail.com>
    
    * Update src/lib.rs
    
    Co-authored-by: Estéfano Bargas <estefano.bargas@fing.edu.uy>
    
    * Update src/lib.rs
    
    Co-authored-by: Estéfano Bargas <estefano.bargas@fing.edu.uy>
    
    * Add missed import
    
    ---------
    
    Co-authored-by: Mario Rugiero <mrugiero@gmail.com>
    Co-authored-by: Estéfano Bargas <estefano.bargas@fing.edu.uy>

--- src/lib.rs
+++ src/lib.rs
@@ -198,6 +198,7 @@ mod test {
     use std::path::PathBuf;
 
     use crate::core::contract_address::{compute_deprecated_class_hash, compute_sierra_class_hash};
+    use crate::definitions::constants::INITIAL_GAS_COST;
     use crate::definitions::{
         block_context::StarknetChainId,
         constants::{
@@ -836,7 +837,7 @@ mod test {
             tx_type: TransactionType::Declare,
             validate_entry_point_selector: VALIDATE_DECLARE_ENTRY_POINT_SELECTOR.clone(),
             version: 1.into(),
-            max_fee: 2,
+            max_fee: INITIAL_GAS_COST,
             signature: vec![],
             nonce: 0.into(),
             hash_value: 0.into(),
@@ -997,5 +998,19 @@ mod test {
             estimate_fee(&[deploy, invoke_tx], state, block_context,).unwrap(),
             [(0, 1224), (0, 0)]
         );
+    }
+
+    #[test]
+    fn test_declare_v2_with_invalid_compiled_class_hash() {
+        let (block_context, mut state) = create_account_tx_test_state().unwrap();
+        let mut declare_v2 = declarev2_tx();
+        declare_v2.compiled_class_hash = Felt252::from(1);
+        let declare_tx = Transaction::DeclareV2(Box::new(declare_v2));
+
+        let err = declare_tx
+            .execute(&mut state, &block_context, INITIAL_GAS_COST)
+            .unwrap_err();
+
+        assert_eq!(err.to_string(), "Invalid compiled class, expected class hash: \"1948962768849191111780391610229754715773924969841143100991524171924131413970\", but received: \"1\"".to_string());
     }
 }
