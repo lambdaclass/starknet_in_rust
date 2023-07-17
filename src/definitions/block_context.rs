1ba9092ddf2857313e847c6c14a42a1b16645a69
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-07-17 15:41:51 -0300

    Implement `Display` instead of `ToString`
    From the [standard library documentation](https://doc.rust-lang.org/std/string/trait.ToString.html):
    
    > This trait is automatically implemented for any type which implements the `Display` trait. As such, `ToString` shouldn't be implemented directly: `Display` should be implemented instead, and you get the `ToString` implementation for free.

--- src/definitions/block_context.rs
+++ src/definitions/block_context.rs
@@ -17,14 +17,13 @@ pub enum StarknetChainId {
     TestNet2,
 }
 
-impl ToString for StarknetChainId {
-    fn to_string(&self) -> String {
+impl core::fmt::Display for StarknetChainId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            StarknetChainId::MainNet => "SN_MAIN",
-            StarknetChainId::TestNet => "SN_GOERLI",
-            StarknetChainId::TestNet2 => "SN_GOERLI2",
+            StarknetChainId::MainNet => write!(f, "SN_MAIN"),
+            StarknetChainId::TestNet => write!(f, "SN_GOERLI"),
+            StarknetChainId::TestNet2 => write!(f, "SN_GOERLI2"),
         }
-        .to_string()
     }
 }
 
