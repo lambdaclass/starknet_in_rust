1c90f86d42498ab2fda2c55e1a1344dcf14013b5
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-07-18 11:11:47 -0300

    chore: implement `Display` instead of `ToString` (#828)
    * Implement `Display` instead of `ToString`
    
    From the [standard library documentation](https://doc.rust-lang.org/std/string/trait.ToString.html):
    
    > This trait is automatically implemented for any type which implements the `Display` trait. As such, `ToString` shouldn't be implemented directly: `Display` should be implemented instead, and you get the `ToString` implementation for free.
    
    * Fix `fmt` import

--- src/definitions/block_context.rs
+++ src/definitions/block_context.rs
@@ -1,5 +1,6 @@
 use crate::{state::BlockInfo, utils::Address};
 use cairo_vm::felt::Felt252;
+use core::fmt;
 use getset::{CopyGetters, Getters, MutGetters};
 use starknet_api::block::Block;
 use std::collections::HashMap;
@@ -17,14 +18,13 @@ pub enum StarknetChainId {
     TestNet2,
 }
 
-impl ToString for StarknetChainId {
-    fn to_string(&self) -> String {
+impl fmt::Display for StarknetChainId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            StarknetChainId::MainNet => "SN_MAIN",
-            StarknetChainId::TestNet => "SN_GOERLI",
-            StarknetChainId::TestNet2 => "SN_GOERLI2",
+            StarknetChainId::MainNet => write!(f, "SN_MAIN"),
+            StarknetChainId::TestNet => write!(f, "SN_GOERLI"),
+            StarknetChainId::TestNet2 => write!(f, "SN_GOERLI2"),
         }
-        .to_string()
     }
 }
 
