b72105a4ead44b802e2685b7d0c737c048ee8b42
matias-gonz <maigonzalez@fi.uba.ar>
2023-08-04 11:59:39 -0300

    wip

--- starknet_programs/storage.cairo
+++ starknet_programs/storage.cairo
@@ -1,12 +1,29 @@
 %lang starknet
 from starkware.cairo.common.cairo_builtins import HashBuiltin
 
+struct SomeStruct {
+    some_felt: felt,
+    another_felt: felt,
+}
+
 @storage_var
 func _counter() -> (res: felt) {
 }
+
+@storage_var
+func _struct(id: felt) -> (res: SomeStruct) {
+}
+
 
 @external
 func write_and_read{syscall_ptr: felt*, pedersen_ptr: HashBuiltin*, range_check_ptr}() -> (res:felt) {
      _counter.write(42);
      return _counter.read();
+}
+
+@external
+func write_at{syscall_ptr: felt*, pedersen_ptr: HashBuiltin*, range_check_ptr}(id: felt, some_felt: felt, another_felt: felt) {
+    tempvar some_struct: SomeStruct = SomeStruct(some_felt=some_felt, another_felt=another_felt);
+    _struct.write(id=id, value=some_struct);
+    return ();
 }
--- tests/storage.rs
+++ tests/storage.rs
@@ -1,6 +1,7 @@
 use cairo_vm::felt::Felt252;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
 use num_traits::Zero;
+use starknet_in_rust::state::state_api::State;
 use starknet_in_rust::EntryPointType;
 use starknet_in_rust::{
     definitions::{block_context::BlockContext, constants::TRANSACTION_VERSION},
@@ -28,13 +29,8 @@ fn integration_storage_test() {
     let contract_class = ContractClass::from_path(path).unwrap();
     let entry_points_by_type = contract_class.entry_points_by_type().clone();
 
-    let storage_entrypoint_selector = entry_points_by_type
-        .get(&EntryPointType::External)
-        .unwrap()
-        .get(0)
-        .unwrap()
-        .selector()
-        .clone();
+    let storage_entrypoint_selector =
+        Felt252::from_bytes_be(&calculate_sn_keccak("write_and_read".as_bytes()));
 
     //* --------------------------------------------
     //*    Create state reader with class hash data
@@ -150,4 +146,119 @@ fn integration_storage_test() {
             .cloned(),
         Some(Felt252::new(42))
     );
+}
+
+#[test]
+fn test_storage_struct() {
+    // ---------------------------------------------------------
+    //  Create program and entry point types for contract class
+    // ---------------------------------------------------------
+
+    let path = PathBuf::from("starknet_programs/storage.json");
+    let contract_class = ContractClass::from_path(path).unwrap();
+    let entry_points_by_type = contract_class.entry_points_by_type().clone();
+
+    let storage_entrypoint_selector =
+        Felt252::from_bytes_be(&calculate_sn_keccak("write_at".as_bytes()));
+
+    //* --------------------------------------------
+    //*    Create state reader with class hash data
+    //* --------------------------------------------
+
+    let mut contract_class_cache = HashMap::new();
+
+    //  ------------ contract data --------------------
+
+    let address = Address(1111.into());
+    let class_hash = [1; 32];
+    let nonce = Felt252::new(88);
+    let storage_entry = (address.clone(), [90; 32]);
+    let storage_value = Felt252::new(10902);
+
+    contract_class_cache.insert(class_hash, contract_class);
+    let mut state_reader = InMemoryStateReader::default();
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(address.clone(), class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(address.clone(), nonce);
+
+    //* ---------------------------------------
+    //*    Create state with previous data
+    //* ---------------------------------------
+
+    let mut state = CachedState::new(Arc::new(state_reader), Some(contract_class_cache), None);
+
+    //* ------------------------------------
+    //*    Create execution entry point
+    //* ------------------------------------
+
+    let calldata = [0.into(), 555.into(), 666.into()].to_vec();
+    let caller_address = Address(0000.into());
+    let entry_point_type = EntryPointType::External;
+
+    let exec_entry_point = ExecutionEntryPoint::new(
+        address.clone(),
+        calldata.clone(),
+        storage_entrypoint_selector.clone(),
+        caller_address,
+        entry_point_type,
+        Some(CallType::Delegate),
+        Some(class_hash),
+        0,
+    );
+
+    //* --------------------
+    //*   Execute contract
+    //* ---------------------
+    let block_context = BlockContext::default();
+    let mut tx_execution_context = TransactionExecutionContext::new(
+        Address(0.into()),
+        Felt252::zero(),
+        Vec::new(),
+        0,
+        10.into(),
+        block_context.invoke_tx_max_n_steps(),
+        TRANSACTION_VERSION.clone(),
+    );
+    let mut resources_manager = ExecutionResourcesManager::default();
+    exec_entry_point
+        .execute(
+            &mut state,
+            &block_context,
+            &mut resources_manager,
+            &mut tx_execution_context,
+            false,
+            block_context.invoke_tx_max_n_steps(),
+        )
+        .unwrap();
+
+    let first_key = calculate_sn_keccak("_struct".as_bytes());
+    let mut second_key = first_key.clone();
+    second_key[31] += 1;
+
+    let (n_modified_contracts, n_storage_updates) = state.count_actual_storage_changes();
+
+    assert!(!state.cache().storage_writes().is_empty());
+    dbg!(first_key.clone());
+    dbg!(state.cache().storage_writes());
+    assert_eq!(
+        state
+            .cache()
+            .storage_writes()
+            .get(&(address.clone(), first_key))
+            .cloned(),
+        Some(Felt252::new(555))
+    );
+    assert_eq!(
+        state
+            .cache()
+            .storage_writes()
+            .get(&(address, second_key))
+            .cloned(),
+        Some(Felt252::new(555))
+    );
+    assert_eq!(n_modified_contracts, 1);
+    assert_eq!(n_storage_updates, 1);
 }
