f8080891a6b737fbb2a2004822d582342268ff31
fannyguthmann <fanny.guthmann@post.idc.ac.il>
2023-08-17 13:32:25 +0300

    change Box to Arc for CompiledClass

--- tests/cairo_1_syscalls.rs
+++ tests/cairo_1_syscalls.rs
@@ -722,7 +722,7 @@ fn deploy_cairo1_from_cairo1() {
 
     let ret_class_hash = state.get_class_hash_at(&ret_address).unwrap();
     let ret_casm_class = match state.get_contract_class(&ret_class_hash).unwrap() {
-        CompiledClass::Casm(class) => *class,
+        CompiledClass::Casm(class) => class.as_ref().clone(),
         CompiledClass::Deprecated(_) => unreachable!(),
     };
 
@@ -824,7 +824,7 @@ fn deploy_cairo0_from_cairo1_without_constructor() {
 
     let ret_class_hash = state.get_class_hash_at(&ret_address).unwrap();
     let ret_casm_class = match state.get_contract_class(&ret_class_hash).unwrap() {
-        CompiledClass::Deprecated(class) => *class,
+        CompiledClass::Deprecated(class) => class.as_ref().clone(),
         CompiledClass::Casm(_) => unreachable!(),
     };
 
@@ -925,7 +925,7 @@ fn deploy_cairo0_from_cairo1_with_constructor() {
 
     let ret_class_hash = state.get_class_hash_at(&ret_address).unwrap();
     let ret_casm_class = match state.get_contract_class(&ret_class_hash).unwrap() {
-        CompiledClass::Deprecated(class) => *class,
+        CompiledClass::Deprecated(class) => class.as_ref().clone(),
         CompiledClass::Casm(_) => unreachable!(),
     };
 
@@ -1027,7 +1027,7 @@ fn deploy_cairo0_and_invoke() {
 
     let ret_class_hash = state.get_class_hash_at(&ret_address).unwrap();
     let ret_casm_class = match state.get_contract_class(&ret_class_hash).unwrap() {
-        CompiledClass::Deprecated(class) => *class,
+        CompiledClass::Deprecated(class) => class.as_ref().clone(),
         CompiledClass::Casm(_) => unreachable!(),
     };
 
@@ -1342,7 +1342,7 @@ fn replace_class_internal() {
     // Check that the class_hash_b leads to contract_class_b for soundness
     assert_eq!(
         state.get_contract_class(&class_hash_b).unwrap(),
-        CompiledClass::Casm(Box::new(contract_class_b))
+        CompiledClass::Casm(Arc::new(contract_class_b))
     );
 }
 
