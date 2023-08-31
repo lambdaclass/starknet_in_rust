e4eb32e4fbd91a0e081fe4ce3310289ee56b6b9e
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-08-31 13:52:53 -0300

    Write call_contract test skeleton

--- tests/cairo_native.rs
+++ tests/cairo_native.rs
@@ -1,5 +1,5 @@
 #![cfg(not(feature = "cairo_1_tests"))]
-#![deny(warnings)]
+// #![deny(warnings)]
 
 use cairo_vm::felt::Felt252;
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
@@ -285,4 +285,65 @@ fn integration_test_erc20() {
     assert_eq!(result.execution_resources, None);
     assert_eq!(result.class_hash, Some(class_hash));
     assert_eq!(result.gas_consumed, 0);
+}
+
+#[test]
+fn call_contract_test() {
+    let caller_contract_class: cairo_lang_starknet::contract_class::ContractClass =
+        serde_json::from_str(
+            std::fs::read_to_string("starknet_programs/cairo2/caller.sierra")
+                .unwrap()
+                .as_str(),
+        )
+        .unwrap();
+
+    let callee_contract_class: cairo_lang_starknet::contract_class::ContractClass =
+        serde_json::from_str(
+            std::fs::read_to_string("starknet_programs/cairo2/callee.sierra")
+                .unwrap()
+                .as_str(),
+        )
+        .unwrap();
+
+    // Caller contract entrypoints
+    let caller_entrypoints = caller_contract_class.clone().entry_points_by_type;
+    let call_contract_selector = &caller_entrypoints.external.get(0).unwrap().selector;
+
+    // Callee contract entrypoints
+    let callee_entrypoints = callee_contract_class.clone().entry_points_by_type;
+    let fn_contract_selector = &callee_entrypoints.external.get(0).unwrap().selector;
+
+    // Create state reader with class hash data
+    let mut sierra_contract_class_cache = HashMap::new();
+
+    // Caller contract data
+    let caller_address = Address(1111.into());
+    let caller_class_hash: ClassHash = [1; 32];
+    let caller_nonce = Felt252::zero();
+
+    // Callee contract data
+    let callee_address = Address(1112.into());
+    let callee_class_hash: ClassHash = [2; 32];
+    let callee_nonce = Felt252::zero();
+
+    sierra_contract_class_cache.insert(caller_class_hash, caller_contract_class);
+    sierra_contract_class_cache.insert(callee_class_hash, callee_contract_class);
+
+    let mut state_reader = InMemoryStateReader::default();
+
+    // Insert caller contract info into state reader
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(caller_address.clone(), caller_class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(caller_address.clone(), caller_nonce);
+
+    // Insert callee contract info into state reader
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(callee_address.clone(), callee_class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(callee_address.clone(), callee_nonce);
 }
