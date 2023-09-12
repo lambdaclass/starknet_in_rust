67bf10d645f96ccd5455cba6463027f3cca27fc2
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-09-12 13:15:55 +0200

    Fix the example.

--- rpc_state_reader/src/bin/realistic.rs
+++ rpc_state_reader/src/bin/realistic.rs
@@ -1,7 +1,15 @@
-use cairo_vm::felt::felt_str;
-use rpc_state_reader::rpc_state::{BlockValue, RpcChain, RpcState};
-use starknet_api::{block::BlockNumber, hash::StarkFelt, stark_felt, transaction::TransactionHash};
+use cairo_vm::felt::{felt_str, Felt252};
+use rpc_state_reader::rpc_state::{BlockValue, RpcBlockInfo, RpcChain, RpcState};
+use starknet_api::{
+    block::BlockNumber,
+    core::{ClassHash as SNClassHash, ContractAddress, PatriciaKey},
+    hash::{StarkFelt, StarkHash},
+    stark_felt,
+    state::StorageKey,
+    transaction::TransactionHash,
+};
 use starknet_in_rust::{
+    core::errors::state_errors::StateError,
     definitions::{
         block_context::{BlockContext, StarknetChainId, StarknetOsConfig},
         constants::{
@@ -10,10 +18,77 @@ use starknet_in_rust::{
             DEFAULT_VALIDATE_MAX_N_STEPS,
         },
     },
-    state::cached_state::CachedState,
-    utils::Address,
+    services::api::contract_classes::compiled_class::CompiledClass,
+    state::{
+        cached_state::CachedState, state_api::StateReader, state_cache::StorageEntry, BlockInfo,
+    },
+    transaction::{InvokeFunction, Transaction},
+    utils::{Address, ClassHash},
 };
 use std::{collections::HashMap, sync::Arc};
+
+pub struct RpcStateReader(RpcState);
+
+impl StateReader for RpcStateReader {
+    fn get_contract_class(&self, class_hash: &ClassHash) -> Result<CompiledClass, StateError> {
+        let hash = SNClassHash(StarkHash::new(*class_hash).unwrap());
+        Ok(CompiledClass::from(self.0.get_contract_class(&hash)))
+    }
+
+    fn get_class_hash_at(&self, contract_address: &Address) -> Result<ClassHash, StateError> {
+        let address = ContractAddress(
+            PatriciaKey::try_from(
+                StarkHash::new(contract_address.clone().0.to_be_bytes()).unwrap(),
+            )
+            .unwrap(),
+        );
+        let mut bytes = [0u8; 32];
+        bytes.copy_from_slice(self.0.get_class_hash_at(&address).0.bytes());
+        Ok(bytes)
+    }
+
+    fn get_nonce_at(&self, contract_address: &Address) -> Result<Felt252, StateError> {
+        let address = ContractAddress(
+            PatriciaKey::try_from(
+                StarkHash::new(contract_address.clone().0.to_be_bytes()).unwrap(),
+            )
+            .unwrap(),
+        );
+        let nonce = self.0.get_nonce_at(&address);
+        Ok(Felt252::from_bytes_be(nonce.bytes()))
+    }
+    fn get_storage_at(&self, storage_entry: &StorageEntry) -> Result<Felt252, StateError> {
+        let (contract_address, key) = storage_entry;
+        let address = ContractAddress(
+            PatriciaKey::try_from(
+                StarkHash::new(contract_address.clone().0.to_be_bytes()).unwrap(),
+            )
+            .unwrap(),
+        );
+        let key = StorageKey(PatriciaKey::try_from(StarkHash::new(*key).unwrap()).unwrap());
+        let value = self.0.get_storage_at(&address, &key);
+        Ok(Felt252::from_bytes_be(value.bytes()))
+    }
+    fn get_compiled_class_hash(&self, class_hash: &ClassHash) -> Result<[u8; 32], StateError> {
+        let address =
+            ContractAddress(PatriciaKey::try_from(StarkHash::new(*class_hash).unwrap()).unwrap());
+        let mut bytes = [0u8; 32];
+        bytes.copy_from_slice(self.0.get_class_hash_at(&address).0.bytes());
+        Ok(bytes)
+    }
+}
+
+impl AsRef<RpcState> for RpcStateReader {
+    fn as_ref(&self) -> &RpcState {
+        &self.0
+    }
+}
+
+impl AsMut<RpcState> for RpcStateReader {
+    fn as_mut(&mut self) -> &mut RpcState {
+        &mut self.0
+    }
+}
 
 fn main() {
     let tx_hash = TransactionHash(stark_felt!(
@@ -25,7 +100,7 @@ fn main() {
 
     // Instantiate the RPC StateReader and the CachedState
     let block = BlockValue::Number(BlockNumber(block_number));
-    let rpc_state = Arc::new(RpcState::new_infura(network, block));
+    let rpc_state = Arc::new(RpcStateReader(RpcState::new_infura(network, block)));
     let mut state = CachedState::new(rpc_state.clone(), HashMap::default());
 
     let fee_token_address = Address(felt_str!(
@@ -33,10 +108,10 @@ fn main() {
         16
     ));
 
-    let network: StarknetChainId = rpc_state.chain.into();
+    let network: StarknetChainId = StarknetChainId::MainNet;
     let starknet_os_config = StarknetOsConfig::new(network.to_felt(), fee_token_address, gas_price);
 
-    let block_info = rpc_state.get_block_info(starknet_os_config.clone());
+    let block_info = rpc_state.0.get_block_info();
 
     let block_context = BlockContext::new(
         starknet_os_config,
@@ -45,12 +120,36 @@ fn main() {
         DEFAULT_CAIRO_RESOURCE_FEE_WEIGHTS.clone(),
         DEFAULT_INVOKE_TX_MAX_N_STEPS,
         DEFAULT_VALIDATE_MAX_N_STEPS,
-        block_info,
+        {
+            let RpcBlockInfo {
+                block_number,
+                block_timestamp,
+                sequencer_address,
+                ..
+            } = block_info;
+
+            let block_number = block_number.0;
+            let block_timestamp = block_timestamp.0;
+            let sequencer_address =
+                Address(Felt252::from_bytes_be(sequencer_address.0.key().bytes()));
+
+            BlockInfo {
+                block_number,
+                block_timestamp,
+                gas_price,
+                sequencer_address,
+            }
+        },
         Default::default(),
         true,
     );
 
-    let tx = rpc_state.get_transaction(&tx_hash);
+    let tx: Transaction = match rpc_state.0.get_transaction(&tx_hash) {
+        starknet_api::transaction::Transaction::Invoke(tx) => Transaction::InvokeFunction(
+            InvokeFunction::from_invoke_transaction(tx, network).unwrap(),
+        ),
+        _ => panic!("This transaction should be an INVOKE transaction"),
+    };
 
     tx.execute(&mut state, &block_context, 0).unwrap();
 }
