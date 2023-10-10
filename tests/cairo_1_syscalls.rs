cea58413d0db17f1d7e305fe09f48de018ed6153
Federica <fedemoletta@hotmail.com>
2023-10-10 16:32:29 -0300

    Add recursive library call test

--- tests/cairo_1_syscalls.rs
+++ tests/cairo_1_syscalls.rs
@@ -3019,3 +3019,141 @@ fn keccak_syscall() {
 
     assert_eq!(retdata[0], Felt252::one());
 }
+
+#[test]
+fn library_call_recursive_50_calls() {
+    //  Create program and entry point types for contract class
+    #[cfg(not(feature = "cairo_1_tests"))]
+    let program_data = include_bytes!("../starknet_programs/cairo2/square_root_recursive.casm");
+    #[cfg(feature = "cairo_1_tests")]
+    let program_data = include_bytes!("../starknet_programs/cairo1/square_root_recursive.casm");
+
+    let contract_class: CasmContractClass = serde_json::from_slice(program_data).unwrap();
+    let entrypoints = contract_class.clone().entry_points_by_type;
+    let entrypoint_selector = &entrypoints.external.get(0).unwrap().selector;
+
+    // Create state reader with class hash data
+    let contract_class_cache = PermanentContractClassCache::default();
+
+    let address = Address(1111.into());
+    let class_hash: ClassHash = [1; 32];
+    let nonce = Felt252::zero();
+
+    contract_class_cache
+        .set_contract_class(class_hash, CompiledClass::Casm(Arc::new(contract_class)));
+    let mut state_reader = InMemoryStateReader::default();
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(address.clone(), class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(address.clone(), nonce);
+
+    // Add lib contract to the state
+
+    #[cfg(not(feature = "cairo_1_tests"))]
+    let lib_program_data = include_bytes!("../starknet_programs/cairo2/math_lib.casm");
+    #[cfg(feature = "cairo_1_tests")]
+    let lib_program_data = include_bytes!("../starknet_programs/cairo1/math_lib.casm");
+
+    let lib_contract_class: CasmContractClass = serde_json::from_slice(lib_program_data).unwrap();
+
+    let lib_address = Address(1112.into());
+    let lib_class_hash: ClassHash = [2; 32];
+    let lib_nonce = Felt252::zero();
+
+    contract_class_cache.set_contract_class(
+        lib_class_hash,
+        CompiledClass::Casm(Arc::new(lib_contract_class)),
+    );
+    state_reader
+        .address_to_class_hash_mut()
+        .insert(lib_address.clone(), lib_class_hash);
+    state_reader
+        .address_to_nonce_mut()
+        .insert(lib_address, lib_nonce);
+
+    // Create state from the state_reader and contract cache.
+    let mut state = CachedState::new(Arc::new(state_reader), Arc::new(contract_class_cache));
+
+    // Create an execution entry point
+    let calldata = [
+        felt_str!("1125899906842624"),
+        Felt252::from_bytes_be(&lib_class_hash),
+        Felt252::from(50),
+    ]
+    .to_vec();
+    let caller_address = Address(0000.into());
+    let entry_point_type = EntryPointType::External;
+
+    let exec_entry_point = ExecutionEntryPoint::new(
+        address,
+        calldata.clone(),
+        Felt252::new(entrypoint_selector.clone()),
+        caller_address,
+        entry_point_type,
+        Some(CallType::Delegate),
+        Some(class_hash),
+        u128::MAX,
+    );
+
+    // Execute the entrypoint
+    let block_context = BlockContext::default();
+    let mut tx_execution_context = TransactionExecutionContext::new(
+        Address(0.into()),
+        Felt252::zero(),
+        Vec::new(),
+        0,
+        10.into(),
+        block_context.invoke_tx_max_n_steps(),
+        TRANSACTION_VERSION.clone(),
+    );
+    let mut resources_manager = ExecutionResourcesManager::default();
+    let expected_execution_resources_internal_call = ExecutionResources {
+        #[cfg(not(feature = "cairo_1_tests"))]
+        n_steps: 84,
+        #[cfg(feature = "cairo_1_tests")]
+        n_steps: 85,
+        n_memory_holes: 5,
+        builtin_instance_counter: HashMap::from([(RANGE_CHECK_BUILTIN_NAME.to_string(), 7)]),
+    };
+
+    let call_info = exec_entry_point
+        .execute(
+            &mut state,
+            &block_context,
+            &mut resources_manager,
+            &mut tx_execution_context,
+            false,
+            block_context.invoke_tx_max_n_steps(),
+        )
+        .unwrap()
+        .call_info
+        .unwrap();
+
+    assert_eq!(call_info.internal_calls.len(), 50);
+    assert_eq!(
+        call_info.internal_calls[0],
+        CallInfo {
+            caller_address: Address(0.into()),
+            call_type: Some(CallType::Delegate),
+            contract_address: Address(1111.into()),
+            entry_point_selector: Some(
+                Felt252::from_str_radix(
+                    "544923964202674311881044083303061611121949089655923191939299897061511784662",
+                    10,
+                )
+                .unwrap(),
+            ),
+            entry_point_type: Some(EntryPointType::External),
+            calldata: vec![felt_str!("1125899906842624")],
+            retdata: [felt_str!("33554432")].to_vec(),
+            execution_resources: expected_execution_resources_internal_call,
+            class_hash: Some(lib_class_hash),
+            gas_consumed: 0,
+            ..Default::default()
+        }
+    );
+    assert_eq!(call_info.retdata, [1.into()].to_vec());
+    assert_eq!(call_info.failure_flag, false);
+}
