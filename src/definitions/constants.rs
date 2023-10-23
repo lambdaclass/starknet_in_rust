887ddc181b06e39b216086f574eda5521fcad83e
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-10-23 21:46:53 +0300

    Fix `get_onchain_data_segment_length` (#1085)
    * Fix get_onchain_data_segment_length
    
    * Update test values
    
    * Update test values
    
    * fmt
    
    ---------
    
    Co-authored-by: Juan Bono <juanbono94@gmail.com>

--- src/definitions/constants.rs
+++ src/definitions/constants.rs
@@ -7,7 +7,7 @@ use std::collections::HashMap;
 
 pub(crate) const L2_TO_L1_MSG_HEADER_SIZE: usize = 3;
 pub(crate) const L1_TO_L2_MSG_HEADER_SIZE: usize = 5;
-pub(crate) const DEPLOYMENT_INFO_SIZE: usize = 2;
+pub(crate) const DEPLOYMENT_INFO_SIZE: usize = 1;
 pub(crate) const CONSUMED_MSG_TO_L2_N_TOPICS: usize = 3;
 pub(crate) const LOG_MSG_TO_L1_N_TOPICS: usize = 2;
 pub(crate) const N_DEFAULT_TOPICS: usize = 1; // Events have one default topic.
