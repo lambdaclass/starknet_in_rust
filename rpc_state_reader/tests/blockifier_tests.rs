45840715276367b7150582b905b4bb86e0c86419
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-10-23 22:24:17 +0300

    Fix bug in `From<StarknetRsContractClass> for CompiledClass` implementation (#1090)
    * Fix bug in CompiledClass
    
    * Add tests

--- rpc_state_reader/tests/blockifier_tests.rs
+++ rpc_state_reader/tests/blockifier_tests.rs
@@ -309,6 +309,11 @@ fn blockifier_test_recent_tx() {
     885298, // real block 885299
     RpcChain::TestNet
 )]
+#[test_case(
+    "0x5a5de1f42f6005f3511ea6099daed9bcbcf9de334ee714e8563977e25f71601",
+    281513, // real block 281514
+    RpcChain::MainNet
+)]
 fn blockifier_test_case_tx(hash: &str, block_number: u64, chain: RpcChain) {
     let (tx_info, trace, receipt) = execute_tx(hash, chain, BlockNumber(block_number));
 
