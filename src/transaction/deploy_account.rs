2fed600eb5a27c4665d52edf5c762241020a4ede
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-10-23 23:01:43 +0300

    Add the ability to execute `DeployAccount` transactions using the `RpcState` (#1089)
    * Add test case
    
    * Fix get_onchain_data_segment_length
    
    * Debug setup
    
    * Add StorageChangesCount struct
    
    * Use StorageChangesCount struct in state method
    
    * Fix implicated code
    
    * Update doc
    
    * Update test values
    
    * Rename method for consistency
    
    * Add note comment
    
    * Remove hardcoded contract address
    
    * Remove txt files
    
    * Remove dbg prints
    
    * Remove dbg prints
    
    * Format
    
    * Restore blockifier version
    
    * Restore tests
    
    * Restore newlines
    
    * Restore newlines
    
    * Remove txt file
    
    * fmt
    
    * Fix bug in `From<StarknetRsContractClass> for CompiledClass` implementation (#1090)
    
    * Fix bug in CompiledClass
    
    * Add tests

--- src/transaction/deploy_account.rs
+++ src/transaction/deploy_account.rs
@@ -400,7 +400,7 @@ impl DeployAccount {
         Ok(call_info)
     }
 
-    pub(crate) fn create_for_simulation(
+    pub fn create_for_simulation(
         &self,
         skip_validate: bool,
         skip_execute: bool,
@@ -420,6 +420,42 @@ impl DeployAccount {
         };
 
         Transaction::DeployAccount(tx)
+    }
+
+    pub fn from_sn_api_transaction(
+        value: starknet_api::transaction::DeployAccountTransaction,
+        chain_id: Felt252,
+    ) -> Result<Self, SyscallHandlerError> {
+        let max_fee = value.max_fee.0;
+        let version = Felt252::from_bytes_be(value.version.0.bytes());
+        let nonce = Felt252::from_bytes_be(value.nonce.0.bytes());
+        let class_hash: [u8; 32] = value.class_hash.0.bytes().try_into().unwrap();
+        let contract_address_salt = Felt252::from_bytes_be(value.contract_address_salt.0.bytes());
+
+        let signature = value
+            .signature
+            .0
+            .iter()
+            .map(|f| Felt252::from_bytes_be(f.bytes()))
+            .collect();
+        let constructor_calldata = value
+            .constructor_calldata
+            .0
+            .as_ref()
+            .iter()
+            .map(|f| Felt252::from_bytes_be(f.bytes()))
+            .collect();
+
+        DeployAccount::new(
+            class_hash,
+            max_fee,
+            version,
+            nonce,
+            constructor_calldata,
+            signature,
+            contract_address_salt,
+            chain_id,
+        )
     }
 }
 
