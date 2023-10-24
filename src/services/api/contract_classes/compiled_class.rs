8baecd992a03380770243055be680987fc0988f0
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-10-24 21:19:16 +0300

    Fix/Refactor State::count actual storage changes + Support `DeployAccount` in the RpcStateReader (#1096)
    * Fix get_onchain_data_segment_length
    
    * Update test values
    
    * Update test values
    
    * fmt
    
    * Fix/Refactor `State::count actual storage changes` (#1086)
    
    * Add StorageChangesCount struct
    
    * Use StorageChangesCount struct in state method
    
    * Fix implicated code
    
    * Update doc
    
    * Update test values
    
    * Rename method for consistency
    
    * Add the ability to execute `DeployAccount` transactions using the `RpcState` (#1089)
    
    * Add test case
    
    * Fix get_onchain_data_segment_length
    
    * Debug setup
    
    * Add StorageChangesCount struct
    
    * Use StorageChangesCount struct in state method
    
    * Fix implicated code
    
    * Update doc
    
    * Update test values
    
    * Rename method for consistency
    
    * Add note comment
    
    * Remove hardcoded contract address
    
    * Remove txt files
    
    * Remove dbg prints
    
    * Remove dbg prints
    
    * Format
    
    * Restore blockifier version
    
    * Restore tests
    
    * Restore newlines
    
    * Restore newlines
    
    * Remove txt file
    
    * fmt
    
    * Fix bug in `From<StarknetRsContractClass> for CompiledClass` implementation (#1090)
    
    * Fix bug in CompiledClass
    
    * Add tests
    
    ---------
    
    Co-authored-by: Juan Bono <juanbono94@gmail.com>

--- src/services/api/contract_classes/compiled_class.rs
+++ src/services/api/contract_classes/compiled_class.rs
@@ -130,7 +130,7 @@ impl From<StarknetRsContractClass> for CompiledClass {
                         )
                     })
                     .collect::<Vec<ContractEntryPoint>>();
-                entry_points_by_type.insert(EntryPointType::Constructor, l1_handler_entries);
+                entry_points_by_type.insert(EntryPointType::L1Handler, l1_handler_entries);
 
                 let v = serde_json::to_value(&_deprecated_contract_class.abi).unwrap();
                 let abi: Option<AbiType> = serde_json::from_value(v).unwrap();
