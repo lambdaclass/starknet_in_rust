1c559347e733dce7fc28ee7d34a956931d16864c
Federica <fedemoletta@hotmail.com>
2023-11-06 12:06:16 -0300

    wip

---
+++ examples/transaction_execution/src/main.rs
@@ -0,0 +1,8 @@
+use cairo_vm::felt::felt_str;
+
+fn main() {
+    let tx_hash = felt_str!("0x49ea9b7e4cdd2c3d535673c961f240090172225769b6d971bdc42037fde5b15");
+
+}
+
+fn execute_tx_from_hash(tx_hash)
