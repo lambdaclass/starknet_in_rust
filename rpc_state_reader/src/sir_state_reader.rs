12b5e43209d321473908bf70c90bce419e118c85
juanbono <juan.bono@lambdaclass.com>
2023-11-07 19:20:05 -0300

    restore code in execute_tx_configurable

--- rpc_state_reader/src/sir_state_reader.rs
+++ rpc_state_reader/src/sir_state_reader.rs
@@ -7,10 +7,10 @@ use starknet_api::{
     hash::{StarkFelt, StarkHash},
     stark_felt,
     state::StorageKey,
-    transaction::{Transaction as SNTransaction, TransactionHash},
+    transaction::{Transaction as SNTransaction, TransactionHash, TransactionVersion},
 };
 use starknet_in_rust::{
-    core::errors::state_errors::StateError,
+    core::{contract_address::compute_casm_class_hash, errors::state_errors::StateError},
     definitions::{
         block_context::{BlockContext, StarknetChainId, StarknetOsConfig},
         constants::{
@@ -27,7 +27,7 @@ use starknet_in_rust::{
         state_cache::StorageEntry,
         BlockInfo,
     },
-    transaction::InvokeFunction,
+    transaction::{Declare, DeclareV2, DeployAccount, InvokeFunction, L1Handler},
     utils::{Address, ClassHash},
 };
 
@@ -96,7 +96,6 @@ impl StateReader for RpcStateReader {
     }
 }
 
-#[allow(unused)]
 pub fn execute_tx_configurable(
     tx_hash: &str,
     network: RpcChain,
@@ -118,9 +117,8 @@ pub fn execute_tx_configurable(
 
     let tx_hash = tx_hash.strip_prefix("0x").unwrap();
 
-    let rpc_state = RpcState::new_infura(network, block_number.into())?;
     // Instantiate the RPC StateReader and the CachedState
-    let rpc_reader = RpcStateReader(rpc_state);
+    let rpc_reader = RpcStateReader(RpcState::new_infura(network, block_number.into()).unwrap());
     let gas_price = rpc_reader.0.get_gas_price(block_number.0).unwrap();
 
     // Get values for block context before giving ownership of the reader
@@ -137,7 +135,7 @@ pub fn execute_tx_configurable(
             block_timestamp,
             sequencer_address,
             ..
-        } = rpc_reader.0.get_block_info()?;
+        } = rpc_reader.0.get_block_info().unwrap();
 
         let block_number = block_number.0;
         let block_timestamp = block_timestamp.0;
@@ -153,15 +151,86 @@ pub fn execute_tx_configurable(
 
     // Get transaction before giving ownership of the reader
     let tx_hash = TransactionHash(stark_felt!(tx_hash));
-    let tx = match rpc_reader.0.get_transaction(&tx_hash)? {
+    let tx = match rpc_reader.0.get_transaction(&tx_hash).unwrap() {
         SNTransaction::Invoke(tx) => InvokeFunction::from_invoke_transaction(tx, chain_id)
             .unwrap()
             .create_for_simulation(skip_validate, false, false, false, skip_nonce_check),
-        _ => unimplemented!(),
+        SNTransaction::DeployAccount(tx) => {
+            DeployAccount::from_sn_api_transaction(tx, chain_id.to_felt())
+                .unwrap()
+                .create_for_simulation(skip_validate, false, false, false)
+        }
+        SNTransaction::Declare(tx) => {
+            // Fetch the contract_class from the next block (as we don't have it in the previous one)
+            let next_block_state_reader = RpcStateReader(
+                RpcState::new_infura(network, (block_number.next()).into()).unwrap(),
+            );
+            let contract_class = next_block_state_reader
+                .get_contract_class(tx.class_hash().0.bytes().try_into().unwrap())
+                .unwrap();
+
+            if tx.version() != TransactionVersion(2_u8.into()) {
+                let contract_class = match contract_class {
+                    CompiledClass::Deprecated(cc) => cc.as_ref().clone(),
+                    _ => unreachable!(),
+                };
+
+                let declare = Declare::new_with_tx_and_class_hash(
+                    contract_class,
+                    Address(Felt252::from_bytes_be(tx.sender_address().0.key().bytes())),
+                    tx.max_fee().0,
+                    Felt252::from_bytes_be(tx.version().0.bytes()),
+                    tx.signature()
+                        .0
+                        .iter()
+                        .map(|f| Felt252::from_bytes_be(f.bytes()))
+                        .collect(),
+                    Felt252::from_bytes_be(tx.nonce().0.bytes()),
+                    Felt252::from_bytes_be(tx_hash.0.bytes()),
+                    tx.class_hash().0.bytes().try_into().unwrap(),
+                )
+                .unwrap();
+                declare.create_for_simulation(skip_validate, false, false, false)
+            } else {
+                let contract_class = match contract_class {
+                    CompiledClass::Casm(cc) => cc.as_ref().clone(),
+                    _ => unreachable!(),
+                };
+
+                let compiled_class_hash = compute_casm_class_hash(&contract_class).unwrap();
+
+                let declare = DeclareV2::new_with_sierra_class_hash_and_tx_hash(
+                    None,
+                    Felt252::from_bytes_be(tx.class_hash().0.bytes()),
+                    Some(contract_class),
+                    compiled_class_hash,
+                    Address(Felt252::from_bytes_be(tx.sender_address().0.key().bytes())),
+                    tx.max_fee().0,
+                    Felt252::from_bytes_be(tx.version().0.bytes()),
+                    tx.signature()
+                        .0
+                        .iter()
+                        .map(|f| Felt252::from_bytes_be(f.bytes()))
+                        .collect(),
+                    Felt252::from_bytes_be(tx.nonce().0.bytes()),
+                    Felt252::from_bytes_be(tx_hash.0.bytes()),
+                )
+                .unwrap();
+                declare.create_for_simulation(skip_validate, false, false, false)
+            }
+        }
+        SNTransaction::L1Handler(tx) => L1Handler::from_sn_api_tx(
+            tx,
+            Felt252::from_bytes_be(tx_hash.0.bytes()),
+            Some(Felt252::from(u128::MAX)),
+        )
+        .unwrap()
+        .create_for_simulation(skip_validate, false),
+        SNTransaction::Deploy(_) => unimplemented!(),
     };
 
-    let trace = rpc_reader.0.get_transaction_trace(&tx_hash)?;
-    let receipt = rpc_reader.0.get_transaction_receipt(&tx_hash)?;
+    let trace = rpc_reader.0.get_transaction_trace(&tx_hash).unwrap();
+    let receipt = rpc_reader.0.get_transaction_receipt(&tx_hash).unwrap();
 
     let class_cache = ContractClassCache::default();
     let mut state = CachedState::new(Arc::new(rpc_reader), class_cache);
