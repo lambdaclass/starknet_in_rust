cc5662e961d5a77978bd4392dbc7a80906256ab5
juanbono <juanbono94@gmail.com>
2023-11-10 10:32:41 -0300

    fixes

--- replay/src/main.rs
+++ replay/src/main.rs
@@ -1,7 +1,6 @@
 use clap::{Parser, Subcommand};
 use rpc_state_reader::{
-    execute_tx,
-    rpc_state::{RpcChain, RpcTransactionReceipt, TransactionTrace},
+    rpc_state::{RpcChain, RpcTransactionReceipt, TransactionTrace}, execute_tx_configurable, execute_tx,
 };
 use starknet_api::block::BlockNumber;
 use starknet_in_rust::execution::TransactionExecutionInfo;
@@ -20,14 +19,16 @@ enum ReplayExecute {
         tx_hash: String,
         chain: String,
         block_number: u64,
+        silent: Option<bool>,
     },
     #[clap(about = "Execute all the invoke transactions in a given block.")]
-    Block { block_number: u64, chain: String },
+    Block { block_number: u64, chain: String, silent: Option<bool> },
     #[clap(about = "Execute all the invoke transactions in a given range of blocks.")]
     BlockRange {
         block_start: u64,
         block_end: u64,
         chain: String,
+        silent: Option<bool>,
     },
 }
 
@@ -39,22 +40,14 @@ fn main() {
             tx_hash,
             chain,
             block_number,
+            silent
         } => {
-            let rpc_chain: RpcChain = match chain.as_str() {
-                "mainnet" => RpcChain::MainNet,
-                "testnet" => RpcChain::TestNet,
-                "testnet2" => RpcChain::TestNet2,
-                _ => panic!("Invalid chain name"),
-            };
-            let block_number = BlockNumber(block_number);
+            let rpc_chain = parse_network(&chain);
+            let block_number = BlockNumber(block_number-1);
 
-            let (tx_info, trace, receipt) = execute_tx(&tx_hash, rpc_chain, block_number).unwrap();
+            let (tx_info, )trace, receipt) = execute_tx(&tx_hash, rpc_chain, block_number).unwrap();
 
-            // TODO: Show all the data correctly
             let TransactionExecutionInfo {
-                call_info,
-                validate_info,
-                fee_transfer_info,
                 revert_error,
                 actual_fee,
                 ..
@@ -62,12 +55,12 @@ fn main() {
             
             let sir_actual_fee = actual_fee;
 
-            let TransactionTrace {
-                validate_invocation,
-                function_invocation,
-                fee_transfer_invocation,
-                ..
-            } = trace;
+            // let TransactionTrace {
+            //     validate_invocation,
+            //     function_invocation,
+            //     fee_transfer_invocation,
+            //     ..
+            // } = trace;
 
             let RpcTransactionReceipt {
                 actual_fee,
@@ -75,23 +68,42 @@ fn main() {
                 ..
             } = receipt;
 
-            println!("Transaction hash: {}", tx_hash);
-            println!("Block number: {}", block_number.0);
-            println!("Chain: {}", chain);
-            println!("[RPC] Execution status: {:?}", execution_status);
-            println!("[RPC] Actual fee: {}", actual_fee);
-            println!("[SIR] Actual fee: {:?}", sir_actual_fee);
+            if let Some(true) = silent {
+                println!("Transaction hash: {}", tx_hash);
+                println!("Block number: {}", block_number.0);
+                println!("Chain: {}", chain);
+                println!("[RPC] Execution status: {:?}", execution_status);
+                if let Some(revert_error) = revert_error {
+                    println!("[SIR] Revert error: {}", revert_error);
+                }
+                println!("[RPC] Actual fee: {} wei", actual_fee);
+                println!("[SIR] Actual fee: {} wei", sir_actual_fee);
+            }
         }
         ReplayExecute::Block {
             block_number,
             chain,
-        } => {}
+            silent
+        } => {
+
+        }
         ReplayExecute::BlockRange {
             block_start,
             block_end,
             chain,
+            silent
         } => {
 
         }
+    }
+}
+
+
+fn parse_network(network: &str) -> RpcChain {
+    match network.to_lowercase().as_str() {
+        "mainnet" => RpcChain::MainNet,
+        "testnet" => RpcChain::TestNet,
+        "testnet2" => RpcChain::TestNet2,
+        _ => panic!("Invalid network name, it should be one of: mainnet, testnet, testnet2"),
     }
 }
