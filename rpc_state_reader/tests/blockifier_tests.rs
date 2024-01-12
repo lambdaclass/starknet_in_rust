bd76db502123859c3b8b0462e9dab4cbde464e7f
Federica <fedemoletta@hotmail.com>
2024-01-12 15:35:20 -0300

    Update Blockifier version

--- rpc_state_reader/tests/blockifier_tests.rs
+++ rpc_state_reader/tests/blockifier_tests.rs
@@ -1,6 +1,6 @@
 use blockifier::{
-    block_context::BlockContext,
-    execution::{contract_class::ContractClass, entry_point::CallInfo},
+    block_context::{BlockContext, FeeTokenAddresses, GasPrices},
+    execution::{call_info::CallInfo, contract_class::ContractClass},
     state::{
         cached_state::{CachedState, GlobalContractCache},
         errors::StateError,
@@ -171,9 +171,17 @@ pub fn execute_tx(
         block_number,
         block_timestamp,
         sequencer_address,
-        fee_token_address,
+        // TODO: Add strk token address when updated
+        fee_token_addresses: FeeTokenAddresses {
+            strk_fee_token_address: fee_token_address,
+            eth_fee_token_address: fee_token_address,
+        },
         vm_resource_fee_cost,
-        gas_price,
+        // TODO: Add strk l1 gas price when updated
+        gas_prices: GasPrices {
+            eth_l1_gas_price: gas_price,
+            strk_l1_gas_price: gas_price,
+        },
         invoke_tx_max_n_steps: 1_000_000,
         validate_max_n_steps: 1_000_000,
         max_recursion_depth: 500,
@@ -182,18 +190,23 @@ pub fn execute_tx(
     // Map starknet_api transaction to blockifier's
     let blockifier_tx = match sn_api_tx.unwrap() {
         SNTransaction::Invoke(tx) => {
-            let invoke = InvokeTransaction { tx, tx_hash };
+            let invoke = InvokeTransaction {
+                tx,
+                tx_hash,
+                only_query: false,
+            };
             AccountTransaction::Invoke(invoke)
         }
         SNTransaction::DeployAccount(tx) => {
             let contract_address = calculate_contract_address(
-                tx.contract_address_salt,
-                tx.class_hash,
-                &tx.constructor_calldata,
+                tx.contract_address_salt(),
+                tx.class_hash(),
+                &tx.constructor_calldata(),
                 ContractAddress::default(),
             )
             .unwrap();
             AccountTransaction::DeployAccount(DeployAccountTransaction {
+                only_query: false,
                 tx,
                 tx_hash,
                 contract_address,
