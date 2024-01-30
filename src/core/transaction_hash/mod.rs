b8db5759d10f5f47331259e2761ac64f04aab6b7
Federica <fedemoletta@hotmail.com>
2024-01-30 12:36:12 -0300

    Use constants for hash prefixes

--- src/core/transaction_hash/mod.rs
+++ src/core/transaction_hash/mod.rs
@@ -1,6 +1,7 @@
 pub mod current;
 pub mod deprecated;
 use cairo_vm::Felt252;
+use lazy_static::lazy_static;
 
 #[derive(Debug)]
 /// Enum representing the different types of transaction hash prefixes.
@@ -12,19 +13,26 @@ pub enum TransactionHashPrefix {
     L1Handler,
 }
 
+lazy_static! {
+    static ref DECLARE_TX_HASH_PREFIX: Felt252 =
+        Felt252::from_dec_str("28258975365558885").unwrap();
+    static ref DEPLOY_TX_HASH_PREFIX: Felt252 = Felt252::from_dec_str("110386840629113").unwrap();
+    static ref DEPLOY_ACCOUNT_TX_HASH_PREFIX: Felt252 =
+        Felt252::from_dec_str("2036277798190617858034555652763252").unwrap();
+    static ref INVOKE_TX_HASH_PREFIX: Felt252 = Felt252::from_dec_str("115923154332517").unwrap();
+    static ref L1_HANDLER_TX_HASH_PREFIX: Felt252 =
+        Felt252::from_dec_str("510926345461491391292786").unwrap();
+}
+
 // Returns the associated prefix value for a given transaction type.
 impl TransactionHashPrefix {
     fn get_prefix(&self) -> Felt252 {
         match self {
-            TransactionHashPrefix::Declare => Felt252::from_dec_str("28258975365558885").unwrap(),
-            TransactionHashPrefix::Deploy => Felt252::from_dec_str("110386840629113").unwrap(),
-            TransactionHashPrefix::DeployAccount => {
-                Felt252::from_dec_str("2036277798190617858034555652763252").unwrap()
-            }
-            TransactionHashPrefix::Invoke => Felt252::from_dec_str("115923154332517").unwrap(),
-            TransactionHashPrefix::L1Handler => {
-                Felt252::from_dec_str("510926345461491391292786").unwrap()
-            }
+            TransactionHashPrefix::Declare => *DECLARE_TX_HASH_PREFIX,
+            TransactionHashPrefix::Deploy => *DEPLOY_TX_HASH_PREFIX,
+            TransactionHashPrefix::DeployAccount => *DEPLOY_ACCOUNT_TX_HASH_PREFIX,
+            TransactionHashPrefix::Invoke => *INVOKE_TX_HASH_PREFIX,
+            TransactionHashPrefix::L1Handler => *L1_HANDLER_TX_HASH_PREFIX,
         }
     }
 }
